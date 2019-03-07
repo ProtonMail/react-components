8512c63ba00ad0026c396bf014e6d8f08c7e8455
dhoko <aurelien@protonmail.com>
2019-03-07 17:33:00 +0100

    Add logic for Toggle component

--- components/toggle/Toggle.js
+++ components/toggle/Toggle.js
@@ -1,15 +1,22 @@
-import React from 'react';
+import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 import { t } from 'ttag';
 
 import Input from '../input/Input';
 import Label from '../label/Label';
 
-const Toggle = ({ id, checked }) => {
+const Toggle = ({ id, checked, onChange }) => {
+    const [value, setValue] = useState(checked);
     return (
         <>
-            <Input type="checkbox" id={id} checked={checked} className="pm-toggle-checkbox" />
-            <Label htmlFor={id} className="pm-toggle-label">
+            <Input
+                type="checkbox"
+                id={id}
+                checked={value}
+                className="pm-toggle-checkbox"
+                onChange={() => onChange(value)}
+            />
+            <Label htmlFor={id} className="pm-toggle-label" onClick={() => setValue(!value)}>
                 <span className="pm-toggle-label-text">{t`On`}</span>
                 <span className="pm-toggle-label-text">{t`Off`}</span>
             </Label>
@@ -19,7 +26,8 @@ const Toggle = ({ id, checked }) => {
 
 Toggle.propTypes = {
     id: PropTypes.string.isRequired,
-    checked: PropTypes.bool.isRequired
+    checked: PropTypes.bool.isRequired,
+    onChange: PropTypes.func.isRequired
 };
 
 Toggle.defaultProps = {
