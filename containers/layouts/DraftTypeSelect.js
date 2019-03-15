10898881559c6b65aa364dd44b1a21c492efebe4
edu <econdepe@protonmail.com>
2019-03-15 21:32:43 +0100

    proposal for DraftTypeSelect

--- containers/layouts/DraftTypeSelect.js
+++ containers/layouts/DraftTypeSelect.js
@@ -1,20 +1,35 @@
 import React from 'react';
 import { c } from 'ttag';
-import { Select } from 'react-components';
+import { Select, useApiWithoutResult, useEventManager } from 'react-components';
 import { updateDraftType } from 'proton-shared/lib/api/mailSettings';
 
-import useApi from '../../hooks/useApi';
+// const { NORMAL, PLAIN_TEXT } = DRAFT_TYPE;
 
 const DraftTypeSelect = () => {
-    const api = useApi();
-    const handleChange = (event) => api(updateDraftType(event.target.value));
+    // const { DraftMIMEType } = useMailSettings();
+    const { call } = useEventManager();
+    const { request, loading } = useApiWithoutResult(updateDraftType);
+
+    const handleChange = (event) => async () => {
+        await request(event.target.value);
+        call();
+    };
+
+    // const api = useApi();
+    // const handleChange = (event) => api(updateDraftType(event.target.value));
 
     const options = [
         { text: c('Option').t`Normal`, value: 'text/html' },
         { text: c('Option').t`Plain Text`, value: 'text/plain' }
     ];
 
-    return <Select options={options} onChange={handleChange} />;
+    return (
+        <>
+            <Select options={options} diabled={loading} onChange={handleChange} />
+        </>
+    );
+
+    // return <Select options={options} onChange={handleChange} />;
 };
 
 export default DraftTypeSelect;
