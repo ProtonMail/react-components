1bb4ecc7c6f6ecd3f425c715aa4d15536ba30f4e
edu <econdepe@protonmail.com>
2019-03-15 15:44:18 +0100

    edit ViewModeRadios

--- containers/layouts/ViewModeRadios.js
+++ containers/layouts/ViewModeRadios.js
@@ -1,24 +1,45 @@
 import React from 'react';
 import { c } from 'ttag';
-import { Label, Radio } from 'react-components';
+import { RadioCard, useApiWithoutResult, useMailSettings, useEventManager } from 'react-components';
 import { updateViewMode } from 'proton-shared/lib/api/mailSettings';
+import { VIEW_MODE } from 'proton-shared/lib/constants';
 
-import useApi from '../../hooks/useApi';
+const { GROUP, SINGLE } = VIEW_MODE;
 
 const ViewModeRadios = () => {
-    const api = useApi();
-    const handleChange = (mode) => () => api(updateViewMode(mode));
+    const { ViewMode } = useMailSettings();
+    const { call } = useEventManager();
+    const { request, loading } = useApiWithoutResult(updateViewMode);
+
+    const handleChange = (mode) => async () => {
+        await request(mode);
+        call();
+    };
 
     return (
         <>
-            <Label htmlFor="conversationRadio">
-                <Radio id="conversationRadio" name="viewMode" onChange={handleChange(0)} />
-                {c('Label').t`Conversation Group`}
-            </Label>
-            <Label htmlFor="messageRadio">
-                <Radio id="messageRadio" name="viewMode" onChange={handleChange(1)} />
-                {c('Label').t`Single Messages`}
-            </Label>
+            <RadioCard
+                value={GROUP}
+                checked={ViewMode === GROUP}
+                id="groupRadio"
+                disabled={loading}
+                name="viewMode"
+                label={c('Label to change view mode').t`Conversation group`}
+                onChange={handleChange(GROUP)}
+            >
+                <img alt="Popup" src="/assets/img/design-system-website/popup.png" />
+            </RadioCard>
+            <RadioCard
+                value={SINGLE}
+                checked={ViewMode === SINGLE}
+                id="singleRadio"
+                disabled={loading}
+                name="ViewMode"
+                label={c('Label to change view mode').t`Single messages`}
+                onChange={handleChange(SINGLE)}
+            >
+                <img alt="Popup" src="/assets/img/design-system-website/popup.png" />
+            </RadioCard>
         </>
     );
 };
