868c83eddfd8ca3771b9cfee38757adde8dc1622
Richard <richard@protonmail.com>
2019-03-21 16:54:11 +0100

    Review Dropdown

--- components/dropdown/Dropdown.test.js
+++ components/dropdown/Dropdown.test.js
@@ -7,10 +7,15 @@ describe('Dropdown component', () => {
     const getContent = (container) => container.firstChild.querySelector('.dropDown-content');
 
     it('should display children when clicking on button', async () => {
-        const { container, getByText } = render(<Dropdown content="clickOnMe">Boo</Dropdown>);
+        const { container, getByText } = render(
+            <div>
+                <Dropdown content="clickOnMe">Boo</Dropdown>
+            </div>
+        );
+        const buttonNode = getByText('clickOnMe');
 
         expect(getContent(container)).toBe(null);
-        fireEvent.click(getByText('clickOnMe'));
+        fireEvent.click(buttonNode);
         expect(getContent(container)).toContainHTML('<div class="dropDown-content">Boo</div>');
     });
 });
