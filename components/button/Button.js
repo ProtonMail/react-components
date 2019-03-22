7d21ec52a9a1517b6e9ee420b24ed703d6745fa3
dhoko <aurelien@protonmail.com>
2019-03-22 17:02:43 +0100

    Remove useless default classname prop

--- components/button/Button.js
+++ components/button/Button.js
@@ -62,7 +62,7 @@ const Button = (props) => {
         <button
             role={role}
             disabled={loading ? true : disabled}
-            className={'pm-button mr1 '.concat(className || '')}
+            className={'pm-button '.concat(className || '')}
             type={type}
             tabIndex={disabled ? '-1' : tabIndex}
             title={title}
@@ -98,7 +98,6 @@ Button.propTypes = {
 };
 
 Button.defaultProps = {
-    className: '',
     role: 'button',
     type: 'button',
     disabled: false,
