1e093a0817a6111ecf4af069d8f56e5284ec3186
dhoko <aurelien@protonmail.com>
2019-04-01 16:28:53 +0200

    Remove loading state as we have useLoading hook

--- components/modal/useModal.js
+++ components/modal/useModal.js
@@ -2,23 +2,12 @@ import { useState } from 'react';
 
 const useModal = (initialState = false) => {
     const [state, setModalState] = useState({ show: initialState });
-    const [isLoading, setLoading] = useState(false);
-    const open = (data) =>
-        setModalState({
-            show: true,
-            data
-        });
-    const close = () => (setLoading(false), setModalState({ ...state, show: false }));
-    const toggle = () => {
-        const show = !state.show;
-        setModalState({ ...state, show });
-        !show && setLoading(false);
-    };
+    const open = (data) => setModalState({ show: true, data });
+    const close = () => setModalState({ ...state, show: false });
+    const toggle = () => setModalState({ ...state, show: !state.show });
 
     return {
         state,
-        isLoading,
-        setLoading,
         open,
         toggle,
         close
