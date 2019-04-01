20eaded2d19f51e384875c9c1ac4129576d923b7
dhoko <aurelien@protonmail.com>
2019-04-01 15:31:06 +0200

    Keep state for the modal

--- components/modal/useModal.js
+++ components/modal/useModal.js
@@ -1,13 +1,24 @@
 import { useState } from 'react';
 
 const useModal = (initialState = false) => {
-    const [isOpen, setModalState] = useState(initialState);
-    const open = () => setModalState(true);
-    const close = () => setModalState(false);
-    const toggle = () => setModalState(!isOpen);
+    const [state, setModalState] = useState({ show: initialState });
+    const [isLoading, setLoading] = useState(false);
+    const open = (data) =>
+        setModalState({
+            show: true,
+            data
+        });
+    const close = () => (setLoading(false), setModalState({ ...state, show: false }));
+    const toggle = () => {
+        const show = !state.show;
+        setModalState({ ...state, show });
+        !show && setLoading(false);
+    };
 
     return {
-        isOpen,
+        state,
+        isLoading,
+        setLoading,
         open,
         toggle,
         close
