6cf669506600ac94f288e257e8e59ade21fab57d
Richard <richard@protonmail.com>
2019-04-02 17:51:03 +0200

    Review again for Button

--- components/sidebar/SubSidebar.test.js
+++ components/sidebar/SubSidebar.test.js
@@ -5,12 +5,13 @@ import Button from '../button/Button';
 import SubSidebar from './SubSidebar';
 
 describe('SubSidebar component', () => {
+    const text = 'chocolat';
     const list = [{ text: 'panda', id: 'panda' }, { text: 'tiger', id: 'tiger' }, { text: 'turtle', id: 'turtle' }];
 
     it('should render the sub sidebar properly', () => {
         const { container } = render(
             <SubSidebar list={list}>
-                <Button>test</Button>
+                <Button>{text}</Button>
             </SubSidebar>
         );
 
@@ -19,6 +20,7 @@ describe('SubSidebar component', () => {
 
         expect(anchors.length).toBe(3);
         expect(buttonNode).not.toBe(null);
+        expect(buttonNode.textContent).toBe(text);
 
         anchors.forEach((anchor, index) => {
             expect(anchor.getAttribute('href')).toBe(`#${list[index].id}`);
