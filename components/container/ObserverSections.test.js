6157625fb61ff9af9949993815ceaec13f853556
edu <econdepe@protonmail.com>
2019-04-09 17:58:02 +0200

    update test

--- components/container/ObserverSections.test.js
+++ components/container/ObserverSections.test.js
@@ -1,27 +1,36 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import { render } from 'react-testing-library';
-
-// import ObserverSections from './ObserverSections';
 import ObserverSections from './ObserverSections';
 
-describe('ObserverSections component', () => {
-    const dummyText =
-        'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed pretium enim nec massa fringilla, ac ultrices tortor posuere. Fusce sed quam vitae arcu pharetra congue. Quisque in elementum nibh.';
-    const repeatText = (text, n) => {
-        return Array(n)
-            .fill(text)
-            .reduce((acc, cur) => cur + acc, '');
-    };
-    const list = [
-        { id: 'section1', granularity: 20, children: <div>{repeatText(dummyText, 1)}</div> },
-        { id: 'section2', granularity: 20, children: <div>{repeatText(dummyText, 2)}</div> },
-        { id: 'section3', granularity: 20, children: <div>{repeatText(dummyText, 3)}</div> },
-        { id: 'section4', granularity: 20, children: <div>{repeatText(dummyText, 4)}</div> },
-        { id: 'section5', granularity: 20, children: <div>{repeatText(dummyText, 5)}</div> }
-    ];
+const dummyText =
+    'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed pretium enim nec massa fringilla, ac ultrices tortor posuere. Fusce sed quam vitae arcu pharetra congue. Quisque in elementum nibh.';
+const repeatText = (text, n) => {
+    return Array(n)
+        .fill(text)
+        .reduce((acc, cur) => cur + acc, '');
+};
 
+const MyDummyComponent = ({ index, text }) => {
+    return <div>{repeatText(text, index)}</div>;
+};
+MyDummyComponent.propTypes = {
+    index: PropTypes.number,
+    text: PropTypes.string,
+    id: PropTypes.string
+};
+
+describe('ObserverSections component', () => {
     it('should render the observer sections properly', () => {
-        const { container } = render(<ObserverSections list={list} />);
+        const { container } = render(
+            <ObserverSections granularity={20} wait={500}>
+                <MyDummyComponent index={1} text={dummyText} id="section1" />
+                <MyDummyComponent index={2} text={dummyText} id="section2" />
+                <MyDummyComponent index={3} text={dummyText} id="section3" />
+                <MyDummyComponent index={4} text={dummyText} id="section4" />
+                <MyDummyComponent index={5} text={dummyText} id="section5" />
+            </ObserverSections>
+        );
 
         const sections = [].slice.call(container.querySelectorAll('section'));
 
