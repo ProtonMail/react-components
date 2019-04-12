5a64f9901a0f37e07fd9b18dca43d2dd36af2d38
EpokK <richard.d@protonmail.com>
2019-04-12 11:46:38 +0200

    Add test for upload

--- components/input/FileInput.test.js
+++ components/input/FileInput.test.js
@@ -1,10 +1,11 @@
 import React from 'react';
-import { render } from 'react-testing-library';
+import { render, fireEvent } from 'react-testing-library';
 
 import FileInput from './FileInput';
 
 describe('FileInput component', () => {
     const value = 'panda';
+    const file = new File(['(⌐□_□)'], 'chucknorris.png', { type: 'image/png' });
 
     it('should render a file input and wrap children', () => {
         const { container } = render(<FileInput>{value}</FileInput>);
@@ -13,5 +14,16 @@ describe('FileInput component', () => {
         expect(inputNode).not.toBe(null);
         expect(inputNode.getAttribute('type')).toBe('file');
         expect(container.textContent).toBe(value);
+    });
+
+    it('should upload a file', () => {
+        const mockOnChange = jest.fn(({ target }) => [...target.files]);
+        const { container } = render(<FileInput onChange={mockOnChange}>{value}</FileInput>);
+        const inputNode = container.querySelector('input');
+
+        Object.defineProperty(inputNode, 'files', { value: [file] });
+        fireEvent.change(inputNode);
+        expect(mockOnChange).toHaveBeenCalledTimes(1);
+        expect(mockOnChange.mock.results[0].value[0]).toBe(file);
     });
 });
