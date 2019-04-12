1e04f5f1e480946c02271a6f6b09729a789a2189
Richard <richard@protonmail.com>
2019-04-12 08:33:29 +0200

    Review modal with Mat

--- containers/support/BugModal.js
+++ containers/support/BugModal.js
@@ -9,6 +9,8 @@ import {
     Alert,
     Row,
     Input,
+    Button,
+    useToggle,
     Info,
     TextArea,
     Label,
@@ -27,7 +29,6 @@ import AttachScreenshot from './AttachScreenshot';
 const BugModal = ({ show, onClose }) => {
     const [{ Name = '' }] = useUser();
     const { createNotification } = useNotifications();
-
     const [addresses = []] = useAddresses();
     const [{ Email = '' } = {}] = addresses;
     const os = getOS();
@@ -49,6 +50,7 @@ const BugModal = ({ show, onClose }) => {
         Username: Name,
         Email
     });
+    const { state: showDetails, toggle: toggleDetails } = useToggle(false);
     const { request, loading } = useApiWithoutResult(reportBug);
     const link = (
         <Href url="https://protonmail.com/support/knowledge-base/how-to-clean-cache-and-cookies/">{c('Link')
@@ -93,42 +95,6 @@ const BugModal = ({ show, onClose }) => {
                 <Alert type="warning">{c('Warning')
                     .t`Bug reports are not end-to-end encrypted, please do not send any sensitive information.`}</Alert>
                 <Row>
-                    <Label htmlFor="OS">{c('Label').t`Operating system`}</Label>
-                    <Input
-                        id="OS"
-                        value={model.OS}
-                        onChange={handleChange('OS')}
-                        placeholder={c('Placeholder').t`OS name`}
-                    />
-                </Row>
-                <Row>
-                    <Label htmlFor="OSVersion">{c('Label').t`Operating system version`}</Label>
-                    <Input
-                        id="OSVersion"
-                        value={model.OSVersion}
-                        onChange={handleChange('OSVersion')}
-                        placeholder={c('Placeholder').t`OS version`}
-                    />
-                </Row>
-                <Row>
-                    <Label htmlFor="Browser">{c('Label').t`Browser`}</Label>
-                    <Input
-                        id="Browser"
-                        value={model.Browser}
-                        onChange={handleChange('Browser')}
-                        placeholder={c('Placeholder').t`Browser name`}
-                    />
-                </Row>
-                <Row>
-                    <Label htmlFor="BrowserVersion">{c('Label').t`Browser version`}</Label>
-                    <Input
-                        id="BrowserVersion"
-                        value={model.BrowserVersion}
-                        onChange={handleChange('BrowserVersion')}
-                        placeholder={c('Placeholder').t`Browser version`}
-                    />
-                </Row>
-                <Row>
                     <Label htmlFor="Email">{c('Label').t`Email address`}</Label>
                     <EmailInput
                         id="Email"
@@ -155,6 +121,52 @@ const BugModal = ({ show, onClose }) => {
                     </Label>
                     <AttachScreenshot id="Attachments" onUpload={handleUpload} onReset={handleReset} />
                 </Row>
+                <Row>
+                    <Label>{c('Label').t`System information`}</Label>
+                    <Button onClick={toggleDetails}>
+                        {showDetails ? c('Action').t`Hide info` : c('Action').t`Show info`}
+                    </Button>
+                </Row>
+                {showDetails ? (
+                    <>
+                        <Row>
+                            <Label htmlFor="OS">{c('Label').t`Operating system`}</Label>
+                            <Input
+                                id="OS"
+                                value={model.OS}
+                                onChange={handleChange('OS')}
+                                placeholder={c('Placeholder').t`OS name`}
+                            />
+                        </Row>
+                        <Row>
+                            <Label htmlFor="OSVersion">{c('Label').t`Operating system version`}</Label>
+                            <Input
+                                id="OSVersion"
+                                value={model.OSVersion}
+                                onChange={handleChange('OSVersion')}
+                                placeholder={c('Placeholder').t`OS version`}
+                            />
+                        </Row>
+                        <Row>
+                            <Label htmlFor="Browser">{c('Label').t`Browser`}</Label>
+                            <Input
+                                id="Browser"
+                                value={model.Browser}
+                                onChange={handleChange('Browser')}
+                                placeholder={c('Placeholder').t`Browser name`}
+                            />
+                        </Row>
+                        <Row>
+                            <Label htmlFor="BrowserVersion">{c('Label').t`Browser version`}</Label>
+                            <Input
+                                id="BrowserVersion"
+                                value={model.BrowserVersion}
+                                onChange={handleChange('BrowserVersion')}
+                                placeholder={c('Placeholder').t`Browser version`}
+                            />
+                        </Row>
+                    </>
+                ) : null}
                 <Alert>{c('Info').t`Contact us at security@protonmail.com for critical security issues.`}</Alert>
                 <FooterModal>
                     <ResetButton>{c('Action').t`Cancel`}</ResetButton>
