6bd1784861f194ee2a3f595bf4051d8703312e27
edu <econdepe@protonmail.com>
2019-04-23 13:17:32 +0200

    improve code: * split state object variable into two * move CSS to aux file * fix issue with opacity in Chrome Picker

--- components/input/ColorPicker.js
+++ components/input/ColorPicker.js
@@ -2,60 +2,29 @@ import React, { useState, useEffect } from 'react';
 import PropTypes from 'prop-types';
 import { Button } from 'react-components';
 import { ChromePicker } from 'react-color';
+import './ColorPickerAux.css';
 
-const ColorPicker = ({ text, initialColor, onColorChange, ...rest }) => {
-    const [pickerState, setPickerState] = useState({
-        display: false,
-        color: initialColor
-    });
+const ColorPicker = ({ text, initialRgbaColor, onColorChange, ...rest }) => {
+    const [display, setDisplay] = useState(false);
+    const [rgbaColor, setRgbaColor] = useState(initialRgbaColor);
 
-    const rgbaColor = (color) => {
-        return `rgba(${color.rgb.r}, ${color.rgb.g}, ${color.rgb.b}, ${color.rgb.a})`;
+    const rgbaColorString = (rgbaColor) => {
+        return `rgba(${rgbaColor.r}, ${rgbaColor.g}, ${rgbaColor.b}, ${rgbaColor.a})`;
     };
-
     const handleClick = () => {
-        setPickerState((prevState) => {
-            return {
-                ...prevState,
-                display: !pickerState.display
-            };
-        });
+        setDisplay(!display);
     };
-
     const handleClose = () => {
-        setPickerState((prevState) => {
-            return {
-                ...prevState,
-                display: false
-            };
-        });
+        setDisplay(false);
     };
-
     const handleChange = (color) => {
-        setPickerState((prevState) => {
-            return {
-                ...prevState,
-                color: color
-            };
-        });
-    };
-
-    const popover = {
-        position: 'absolute',
-        zIndex: '2'
-    };
-    const cover = {
-        position: 'fixed',
-        top: '0px',
-        right: '0px',
-        bottom: '0px',
-        left: '0px'
+        setRgbaColor(color.rgb);
     };
 
     const picker = (
-        <div style={popover}>
-            <div style={cover} onClick={handleClose} />
-            <ChromePicker color={pickerState.color} onChange={handleChange} />
+        <div className="popover">
+            <div className="cover" onClick={handleClose} />
+            <ChromePicker color={rgbaColor} onChange={handleChange} />
         </div>
     );
 
@@ -63,34 +32,27 @@ const ColorPicker = ({ text, initialColor, onColorChange, ...rest }) => {
         if (onColorChange) {
             onColorChange();
         }
-    }, [pickerState.color]);
+    }, [rgbaColor]);
 
     return (
-        <div style={{ position: 'relative' }}>
-            <Button onClick={handleClick} style={{ backgroundColor: rgbaColor(pickerState.color) }} {...rest}>
+        <div className="relative">
+            <Button onClick={handleClick} style={{ backgroundColor: rgbaColorString(rgbaColor) }} {...rest}>
                 {text}
             </Button>
-            {pickerState.display ? picker : null}
+            {display ? picker : null}
         </div>
     );
 };
 
 ColorPicker.propTypes = {
     text: PropTypes.string,
-    initialColor: PropTypes.object,
+    initialRgbaColor: PropTypes.object,
     onColorChange: PropTypes.func
 };
 
 ColorPicker.defaultProps = {
     text: '',
-    initialColor: {
-        // white
-        hex: '#ffffff',
-        rgb: { r: 255, g: 255, b: 255, a: 1 },
-        hsl: { h: 0, s: 0, l: 1, a: 1 },
-        hsv: { h: 0, s: 0, v: 1, a: 1 },
-        oldHue: 0
-    }
+    initialRgbaColor: { r: 255, g: 255, b: 255, a: 1 } // white
 };
 
 export default ColorPicker;
