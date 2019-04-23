083426681a3f209c687b1feb8c5aa1447223d085
edu <econdepe@protonmail.com>
2019-04-23 14:46:21 +0200

    fix typo in css import

--- components/input/ColorPicker.js
+++ components/input/ColorPicker.js
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from 'react';
 import PropTypes from 'prop-types';
 import { Button } from 'react-components';
 import { ChromePicker } from 'react-color';
-import './ColorPickerAux.css';
+import './ColorPicker.css';
 
 const ColorPicker = ({ text, initialRgbaColor, onColorChange, ...rest }) => {
     const [display, setDisplay] = useState(false);
