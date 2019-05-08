27d3f5fe117f0ed6618bf8a05b0a5a047013e538
edu <econdepe@protonmail.com>
2019-05-08 22:09:13 +0200

    update CustomThemeModal with InnerModal

--- containers/themes/CustomThemeModal.js
+++ containers/themes/CustomThemeModal.js
@@ -1,7 +1,17 @@
 import React, { useState } from 'react';
 import { c } from 'ttag';
 import PropTypes from 'prop-types';
-import { Modal, FooterModal, Label, ContentModal, Alert, TextArea, ResetButton, PrimaryButton } from 'react-components';
+import {
+    Modal,
+    FooterModal,
+    Label,
+    ContentModal,
+    InnerModal,
+    Alert,
+    TextArea,
+    ResetButton,
+    PrimaryButton
+} from 'react-components';
 
 const CustomThemeModal = ({ onClose, onSave, theme: initialTheme = '' }) => {
     const [theme, setTheme] = useState(initialTheme);
@@ -10,16 +20,18 @@ const CustomThemeModal = ({ onClose, onSave, theme: initialTheme = '' }) => {
     return (
         <Modal onClose={onClose} title={c('Title').t`Custom Theme`} type="small">
             <ContentModal onSubmit={handleSubmit} onReset={onClose}>
-                <Alert type="warning">{c('Warning')
-                    .t`Custom themes from third parties can potentially betray your privacy. Only use themes from trusted sources`}</Alert>
-                <Label className="mb1" htmlFor="themeTextarea">{c('Label').t`CSS code`}</Label>
-                <TextArea
-                    className="mb1"
-                    id="themeTextarea"
-                    value={theme}
-                    placeholder={c('Action').t`Insert CSS code here`}
-                    onChange={handleChange}
-                />
+                <InnerModal>
+                    <Alert type="warning">{c('Warning')
+                        .t`Custom themes from third parties can potentially betray your privacy. Only use themes from trusted sources`}</Alert>
+                    <Label className="mb1" htmlFor="themeTextarea">{c('Label').t`CSS code`}</Label>
+                    <TextArea
+                        className="mb1"
+                        id="themeTextarea"
+                        value={theme}
+                        placeholder={c('Action').t`Insert CSS code here`}
+                        onChange={handleChange}
+                    />
+                </InnerModal>
                 <FooterModal>
                     <ResetButton>{c('Action').t`Cancel`}</ResetButton>
                     <PrimaryButton type="submit">{c('Action').t`Save`}</PrimaryButton>
