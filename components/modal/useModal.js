5cb6541ac400d9583ebf066fc55364e6646f189e
Richard <richard@protonmail.com>
2019-05-08 21:35:31 +0200

    Handle close effect modal
    When closing the modal, the class pm-modalOverlay--fadeOut is added on pm-modalOverlay, let this animation execute itself before removing the code in the DOM (500ms duration).

--- components/modal/useModal.js
+++ components/modal/useModal.js
@@ -1,10 +1,22 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 
 const useModal = (initialState = false) => {
     const [isOpen, setModalState] = useState(initialState);
-    const open = () => setModalState(true);
-    const close = () => setModalState(false);
     const toggle = () => setModalState(!isOpen);
+    const open = () => setModalState(true);
+    let id;
+
+    const close = () => {
+        const modal = document.querySelector('.pm-modalOverlay');
+        modal && modal.classList.add('pm-modalOverlay--fadeOut');
+        id = setTimeout(() => setModalState(false), 500);
+    };
+
+    useEffect(() => {
+        return () => {
+            clearTimeout(id);
+        };
+    });
 
     return {
         isOpen,
