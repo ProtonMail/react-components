d0f8040ce76029892d43045e7e0fb54c9403f168
vincaslt <vincaslt@gmail.com>
2019-05-08 16:22:19 +0300

    react components for auto-reply

---
+++ containers/autoReply/AutoReplyModal.js
@@ -0,0 +1,48 @@
+import React from 'react';
+import {
+    Modal,
+    ContentModal,
+    FooterModal,
+    Button,
+    PrimaryButton,
+    useMailSettings,
+    useApiWithoutResult,
+    useEventManager
+} from 'react-components';
+import { updateAutoresponder } from 'proton-shared/lib/api/mailSettings';
+import PropTypes from 'prop-types';
+import { c } from 'ttag';
+import AutoReplyForm from './AutoReplyForm/AutoReplyForm';
+import useAutoReplyForm from './AutoReplyForm/useAutoReplyForm';
+
+const AutoReplyModal = ({ onClose }) => {
+    const [{ AutoResponder }] = useMailSettings();
+    const { model, updateModel, toAutoResponder } = useAutoReplyForm(AutoResponder);
+    const { request } = useApiWithoutResult(updateAutoresponder);
+    const { call } = useEventManager();
+
+    const handleSubmit = async () => {
+        await request(toAutoResponder(model));
+        call();
+        onClose();
+    };
+
+    return (
+        <Modal title={c('Title').t`Create auto-reply`} type="small" onClose={onClose}>
+            <ContentModal>
+                <AutoReplyForm model={model} updateModel={updateModel} />
+
+                <FooterModal>
+                    <Button onClick={onClose}>{c('Action').t`Cancel`}</Button>
+                    <PrimaryButton onClick={handleSubmit}>{c('Action').t`Update`}</PrimaryButton>
+                </FooterModal>
+            </ContentModal>
+        </Modal>
+    );
+};
+
+AutoReplyModal.propTypes = {
+    onClose: PropTypes.func.isRequired
+};
+
+export default AutoReplyModal;
