62a3d09788866e308835915eeacfff619da88a14
vincaslt <vincaslt@gmail.com>
2019-05-21 17:01:50 +0300

    Add ability to bind onChange to autocomplete hook

--- components/autocomplete/useAutocomplete.js
+++ components/autocomplete/useAutocomplete.js
@@ -1,15 +1,28 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
+import { noop } from 'proton-shared/lib/helpers/function';
 
-const defaultItemToLabel = (label) => label;
+const defaultLabelToItem = (label) => label;
 
 const useAutocomplete = ({
     multiple = true,
     initialSelectedItems = [],
     initialInputValue = '',
-    labelToItem = defaultItemToLabel
+    labelToItem = defaultLabelToItem,
+    onChange = noop
 } = {}) => {
+    const [initialized, setInitialized] = useState(false);
     const [selectedItems, setSelectedItems] = useState(initialSelectedItems);
     const [inputValue, changeInputValue] = useState(initialInputValue);
+
+    // We want to emit onChange when items are selected or removed,
+    // but not with the initial values on mount
+    useEffect(() => {
+        if (initialized) {
+            onChange(selectedItems);
+        } else {
+            setInitialized(true);
+        }
+    }, [selectedItems]);
 
     const select = (item, label) => {
         changeInputValue(multiple ? '' : label);
