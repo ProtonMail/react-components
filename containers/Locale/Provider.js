d2776282d1488dab677281e65ea43f3a02bc05c9
dhoko <dhoko@pm.me>
2019-05-29 19:03:54 +0200

    The simplest solution is always the best

--- containers/Locale/Provider.js
+++ containers/Locale/Provider.js
@@ -1,5 +1,5 @@
 import { c } from 'ttag';
-import React, { createContext, useContext, useState } from 'react';
+import React, { createContext, useContext, useState, useEffect } from 'react';
 import PropTypes from 'prop-types';
 import { useUserSettings, useConfig, useNotifications } from 'react-components';
 import { DEFAULT_TRANSLATION } from 'proton-shared/lib/constants';
@@ -7,21 +7,24 @@ import { loadLocale } from 'proton-shared/lib/i18n';
 
 export const LocaleContext = createContext();
 
-const appState = {};
 export const LocaleProvider = ({ children }) => {
     const config = useConfig();
     const { createNotification } = useNotifications();
     const [{ Locale = DEFAULT_TRANSLATION } = { Locale }] = useUserSettings();
     const [state, setState] = useState(Locale);
 
-    // Only when the locale changes via event or onLoadApp
-    if (state !== Locale || !appState.isLoaded) {
-        appState.isLoaded = true;
-
+    // Load the translations when we load the app
+    useEffect(() => {
         loadLocale(config, Locale).then(() => {
-            appState.isTranslated && createNotification({ text: c('Success').t`Locale updated` });
             setState(Locale); // force refresh children
-            appState.isTranslated = true;
+        });
+    }, []);
+
+    // Only when the locale changes via event
+    if (state !== Locale) {
+        loadLocale(config, Locale).then(() => {
+            setState(Locale); // force refresh children
+            createNotification({ text: c('Success').t`Locale updated` });
         });
     }
 
