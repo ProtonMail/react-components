b927e965bfb5d9974d55cd87aca91c029eb3777e
mmso <mmso@mmso.se>
2019-07-12 10:05:05 +0200

    Improve addresses dropdown style

--- containers/members/MemberAddresses.js
+++ containers/members/MemberAddresses.js
@@ -5,7 +5,11 @@ import { msgid, c } from 'ttag';
 import { Dropdown, DropdownMenu } from 'react-components';
 
 const MemberAddresses = ({ addresses }) => {
-    const list = addresses.map(({ Email }) => Email);
+    const list = addresses.map(({ ID, Email }) => (
+        <div key={ID} className="inbl w100 pt0-5 pb0-5 ellipsis">
+            {Email}
+        </div>
+    ));
     const title = list.join(', ');
     const n = list.length;
 
@@ -17,9 +21,9 @@ const MemberAddresses = ({ addresses }) => {
                 className="pm-button pm-button--link"
                 content={c('Info').ngettext(msgid`${n} address`, `${n} addresses`, n)}
             >
-                <DropdownMenu>{list}</DropdownMenu>
-                <div className="alignright">
-                    <Link className="pm-button pm-button--small" to="/settings/addresses">{c('Link').t`Manage`}</Link>
+                <DropdownMenu className="p0-5">{list}</DropdownMenu>
+                <div className="alignright p1">
+                    <Link className="pm-button" to="/settings/addresses">{c('Link').t`Manage`}</Link>
                 </div>
             </Dropdown>
         </>
