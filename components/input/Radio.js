10704e345701a83d294ae6b06dd2aa54997e4210
RandomAdversary <RandomAdversary@gmail.com>
2019-07-18 16:20:14 +0200

    Update radio component + selectors.

--- components/input/Radio.js
+++ components/input/Radio.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-const Radio = ({ id, children, className, ...rest }) => {
+const Radio = ({ id, children, className, cypressTag, ...rest }) => {
     return (
         <label htmlFor={id} className={className}>
             <input id={id} type="radio" className="pm-radio" {...rest} />
-            <span className="pm-radio-fakeradio" />
+            <span className="pm-radio-fakeradio" {...cypressTag} />
             {children}
         </label>
     );
