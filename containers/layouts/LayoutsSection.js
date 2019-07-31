72f01293e75580dbb50dac8c432e214461b2605b
Yiin <stanislovas.janonis@gmail.com>
2019-07-31 15:12:03 +0300

    Use constant for sticky label status

--- containers/layouts/LayoutsSection.js
+++ containers/layouts/LayoutsSection.js
@@ -21,7 +21,7 @@ import {
     updateDraftType,
     updateRightToLeft
 } from 'proton-shared/lib/api/mailSettings';
-import { VIEW_MODE } from 'proton-shared/lib/constants';
+import { VIEW_MODE, STICKY_LABELS } from 'proton-shared/lib/constants';
 
 import DraftTypeSelect from './DraftTypeSelect';
 import TextDirectionSelect from './TextDirectionSelect';
@@ -56,7 +56,7 @@ const LayoutsSection = () => {
 
     const handleChangeViewMode = async (mode) => {
         if (mode !== VIEW_MODE.GROUP) {
-            await api(updateStickyLabels(0));
+            await api(updateStickyLabels(STICKY_LABELS.OFF));
         }
         await api(updateViewMode(mode));
         await call();
