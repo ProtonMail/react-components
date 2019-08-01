1a7d0773a682d8688cfc7f34efa56eb508d160c7
vincaslt <vincaslt@gmail.com>
2019-08-01 17:05:36 +0300

    Use react dropdown styles

--- components/dropdown/DropdownCaret.js
+++ components/dropdown/DropdownCaret.js
@@ -4,7 +4,7 @@ import { classnames } from '../../helpers/component';
 import Icon from '../icon/Icon';
 
 const DropdownCaret = ({ isOpen }) => {
-    return <Icon className={classnames(['expand-caret', isOpen && 'rotateX-180'])} size={12} name="caret" />;
+    return <Icon className={classnames(['fill-currentColor', isOpen && 'rotateX-180'])} size={12} name="caret" />;
 };
 
 DropdownCaret.propTypes = {
