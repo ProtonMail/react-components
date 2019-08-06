2b20ac97d95991493489a2dd47c01fa9263099e6
Yiin <stanislovas.janonis@gmail.com>
2019-08-06 18:57:23 +0300

    Change Link to a

--- containers/heading/UpgradeButton.js
+++ containers/heading/UpgradeButton.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
 import PropTypes from 'prop-types';
 import { c } from 'ttag';
 
@@ -7,10 +6,10 @@ import { Icon } from 'react-components';
 
 const UpgradeButton = ({ className }) => {
     return (
-        <Link to="/settings/subscription" className={className}>
+        <a to="/settings/subscription" className={className}>
             <Icon name="upgrade-to-paid" className="topnav-icon mr0-5 flex-item-centered-vert fill-white" />
             {c('Link').t`Upgrade`}
-        </Link>
+        </a>
     );
 };
 
