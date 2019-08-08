1ba97388d28f85ed93cd5f221f13ca2ebf83a1b9
vincaslt <vincaslt@gmail.com>
2019-08-08 11:42:24 +0300

    idiot

--- components/popper/index.js
+++ components/popper/index.js
@@ -1,3 +1,3 @@
 export { default as usePopper } from './usePopper';
 export { default as usePopperAnchor } from './usePopperAnchor';
-export { default as Popper } from './popper';
+export { default as Popper } from './Popper';
