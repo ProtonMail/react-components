47b9a91dd2a0c327b4a0c78889ccc93908b5d12f
vincaslt <vincaslt@gmail.com>
2019-08-08 16:57:25 +0300

    fix ci

--- containers/vpn/OpenVPNConfigurationSection/Country.js
+++ containers/vpn/OpenVPNConfigurationSection/Country.js
@@ -8,14 +8,13 @@ const getFlagSvg = (abbreviation) => flags(`./${abbreviation.toLowerCase()}.svg`
 
 const Country = ({ server: { EntryCountry, ExitCountry } }) => {
     const isRouted = EntryCountry && EntryCountry !== ExitCountry;
+    const exitCountryName = getCountryByAbbr(EntryCountry);
 
     return (
         <div className="inline-flex-vcenter">
             <img width={20} className="mr0-5" src={getFlagSvg(ExitCountry)} alt={`flag-${ExitCountry}`} />
             {getCountryByAbbr(ExitCountry)}
-            {isRouted && (
-                <span className="ml0-25 opacity-50">{c('CountryInfo').t`(via ${getCountryByAbbr(EntryCountry)})`}</span>
-            )}
+            {isRouted && <span className="ml0-25 opacity-50">{c('CountryInfo').t`(via ${exitCountryName})`}</span>}
         </div>
     );
 };
