7fb43398b34d967af49c813512b976c35d6925f9
Yiin <stanislovas.janonis@gmail.com>
2019-08-13 13:02:34 +0300

    Autogrow textarea (#174)

--- components/input/TextArea.js
+++ components/input/TextArea.js
@@ -1,14 +1,54 @@
-import React, { useState } from 'react';
+import React, { useState, useCallback } from 'react';
 import PropTypes from 'prop-types';
 
 import { generateUID } from '../../helpers/component';
 import useInput from './useInput';
 import ErrorZone from '../text/ErrorZone';
 
+const useAutoGrow = ({ maxRows = 5, minRows = 1, autoGrow = false }, handlers) => {
+    const [rows, setRows] = useState(minRows);
+
+    const updateTextArea = useCallback(
+        (event) => {
+            const textAreaLineHeight = +getComputedStyle(event.target).lineHeight.replace('px', '');
+
+            const previousRows = event.target.rows;
+
+            // Reset rows so we can calculate calculate currentRows correctly
+            event.target.rows = minRows;
+
+            const currentRows = Math.min(
+                maxRows,
+                Math.max(minRows, ~~(event.target.scrollHeight / textAreaLineHeight))
+            );
+
+            // Set rows attribute directly because React won't update it as it stayed the same
+            if (currentRows === previousRows) {
+                event.target.rows = currentRows;
+            }
+
+            setRows(currentRows);
+
+            handlers.onChange(event);
+        },
+        [minRows, maxRows, handlers]
+    );
+
+    if (!autoGrow) {
+        return {
+            rows: maxRows,
+            updateTextArea: handlers.onChange
+        };
+    }
+    return { rows, updateTextArea };
+};
+
 const TextArea = (props) => {
-    const { className = '', rows = 5, error, ...rest } = props;
+    const { className = '', error, ...rest } = props;
     const { handlers, statusClasses, status } = useInput(props);
     const [uid] = useState(generateUID('textarea'));
+    const { rows, updateTextArea } = useAutoGrow(props, handlers);
+
     return (
         <>
             <textarea
@@ -18,6 +58,7 @@ const TextArea = (props) => {
                 aria-describedby={uid}
                 {...rest}
                 {...handlers}
+                onChange={updateTextArea}
             />
             <ErrorZone id={uid}>{error && status.isDirty ? error : ''}</ErrorZone>
         </>
@@ -26,6 +67,7 @@ const TextArea = (props) => {
 
 TextArea.propTypes = {
     error: PropTypes.string,
+    autoGrow: PropTypes.bool,
     className: PropTypes.string,
     disabled: PropTypes.bool,
     id: PropTypes.string,
