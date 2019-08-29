f549fe3ed5995a0e191a8af5a4b9436640e734b0
mmso <mmso@mmso.se>
2019-08-29 16:09:46 +0200

    Add abuse modal on login

---
+++ containers/login/AbuseModal.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import { FormModal, Alert } from 'react-components';
+import { c } from 'ttag';
+import { Href } from '../../index';
+
+const AbuseModal = (props) => {
+    const title = c('Title').t`Account disabled`;
+
+    const abuseOrFraudLink = (
+        <Href url="https://protonmail.com/support/knowledge-base/account-disabled/" key={0}>
+            {c('Info').t`abuse or fraud`}
+        </Href>
+    );
+
+    const contactLink = (
+        <Href url="https://protonmail.com/abuse" key={1}>
+            {c('Info').t`here`}
+        </Href>
+    );
+
+    return (
+        <FormModal hasClose={false} hasSubmit={false} title={title} {...props}>
+            <Alert type="warning">
+                <div className="mb1">{c('Info').jt`Account disabled due to ${abuseOrFraudLink}.`}</div>
+                <div>{c('Info').jt`You can find more information and contact us ${contactLink}.`}</div>
+            </Alert>
+        </FormModal>
+    );
+};
+
+export default AbuseModal;
