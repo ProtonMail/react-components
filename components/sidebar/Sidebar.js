8e84ac4fcf091e9942a5d94d642e319bba09005f
Nicolas Hoffmann <dante3333@gmail.com>
2019-09-02 17:07:36 +0200

    Add logo only on mobile in sidebar container

--- components/sidebar/Sidebar.js
+++ components/sidebar/Sidebar.js
@@ -2,10 +2,12 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 import NavMenu from './NavMenu';
+import MainLogo from '../logo/MainLogo';
 
 const Sidebar = ({ list = [] }) => {
     return (
         <div className="sidebar flex flex-column noprint">
+            <MainLogo url="/account" className="nodesktop notablet" />
             <nav className="navigation mw100 flex-item-fluid scroll-if-needed mb1">
                 <NavMenu list={list} />
             </nav>
