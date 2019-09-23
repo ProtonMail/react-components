80cb7554da5d406340004025959bbc362baca37f
Richard <richard@protonmail.com>
2019-09-23 14:52:13 +0200

    Review

--- containers/payments/PaymentVerificationModal.js
+++ containers/payments/PaymentVerificationModal.js
@@ -29,10 +29,9 @@ const PROCESSING_DELAY = 5000;
 const PaymentVerificationModal = ({ params, token, approvalURL, onSubmit, ...rest }) => {
     const TITLES = {
         [STEPS.REDIRECT]: c('Title').t`Card verification`,
-        ...
-        redirecting: c('Title').t`Processing...`,
-        redirected: c('Title').t`Payment verification in progress`,
-        fail: c('Title').t`3-D secure payment verification failed`
+        [STEPS.REDIRECTING]: c('Title').t`Processing...`,
+        [STEPS.REDIRECTED]: c('Title').t`Payment verification in progress`,
+        [STEPS.FAILS]: c('Title').t`3-D secure payment verification failed`
     };
     const [step, setStep] = useState(STEPS.REDIRECT);
     const [error, setError] = useState();
@@ -84,73 +83,57 @@ const PaymentVerificationModal = ({ params, token, approvalURL, onSubmit, ...res
             }
             {...rest}
         >
-            {{
-                [STEPS.REDIRECT]: (
-                    <>
-                        <p className="aligncenter">{c('Info')
-                            .t`Your bank requires 3-D Secure verification for security purposes.`}</p>
-                        <p className="aligncenter">
-                            <img src={tabSvg} alt={c('Title').t`New tab`} />
-                        </p>
-                        <Alert>{c('Info')
-                            .t`Verification will open a new tab. Please disable any popup blockers. <b>You will not be charged</b>. Any amount used to verify your card will be refunded immediately.`}</Alert>
-                    </>
-                ),
-                [STEPS.REDIRECTING]: (
-                    <>
-                        <p className="aligncenter">{c('Info')
-                            .t`You will be soon redirected to your bank to verify your payment.`}</p>
-                        <Loader />
-                        <Alert>{c('Info').t`Verification will open a new tab, please disable any popup blockers.`}</Alert>
-                    </>
-                )
-                ,
-                ...
-            }[step]}
-                <>
-                    <p className="aligncenter">{c('Info')
-                        .t`Your bank requires 3-D Secure verification for security purposes.`}</p>
-                    <p className="aligncenter">
-                        <img src={tabSvg} alt={c('Title').t`New tab`} />
-                    </p>
-                    <Alert>{c('Info')
-                        .t`Verification will open a new tab. Please disable any popup blockers. <b>You will not be charged</b>. Any amount used to verify your card will be refunded immediately.`}</Alert>
-                </>
-            ) : null}
-            {step === STEPS.REDIRECTING ? (
-                <>
-                    <p className="aligncenter">{c('Info')
-                        .t`You will be soon redirected to your bank to verify your payment.`}</p>
-                    <Loader />
-                    <Alert>{c('Info').t`Verification will open a new tab, please disable any popup blockers.`}</Alert>
-                </>
-            ) : null}
-            {step === STEPS.REDIRECTED ? (
-                <>
-                    <p className="aligncenter">{c('Info').t`Please verify payment at the new tab which was opened.`}</p>
-                    <Loader />
-                    <p className="aligncenter">
-                        <Button onClick={handleCancel}>{c('Action').t`Cancel`}</Button>
-                    </p>
-                    <Alert>{c('Info').t`Payment can take a few minutes to fully verify.`}</Alert>
-                </>
-            ) : null}
-            {step === STEPS.FAIL ? (
-                <div className="aligncenter">
-                    <p>{c('Info')
-                        .t`Please try again, use a different payment method, or call your bank for assistance.`}</p>
-                    <img src={errorSvg} alt={c('Title').t`Error`} />
-                    {error.tryAgain ? (
-                        <p>
-                            <Button type="submit">{c('Action').t`Try again`}</Button>
-                        </p>
-                    ) : null}
-                    <p>
-                        <PrimaryButton onClick={handleCancel}>{c('Action')
-                            .t`Use a different payment method`}</PrimaryButton>
-                    </p>
-                </div>
-            ) : null}
+            {
+                {
+                    [STEPS.REDIRECT]: (
+                        <>
+                            <p className="aligncenter">{c('Info')
+                                .t`Your bank requires 3-D Secure verification for security purposes.`}</p>
+                            <p className="aligncenter">
+                                <img src={tabSvg} alt={c('Title').t`New tab`} />
+                            </p>
+                            <Alert>{c('Info')
+                                .t`Verification will open a new tab. Please disable any popup blockers. <b>You will not be charged</b>. Any amount used to verify your card will be refunded immediately.`}</Alert>
+                        </>
+                    ),
+                    [STEPS.REDIRECTING]: (
+                        <>
+                            <p className="aligncenter">{c('Info')
+                                .t`You will be soon redirected to your bank to verify your payment.`}</p>
+                            <Loader />
+                            <Alert>{c('Info')
+                                .t`Verification will open a new tab, please disable any popup blockers.`}</Alert>
+                        </>
+                    ),
+                    [STEPS.REDIRECTED]: (
+                        <>
+                            <p className="aligncenter">{c('Info')
+                                .t`Please verify payment at the new tab which was opened.`}</p>
+                            <Loader />
+                            <p className="aligncenter">
+                                <Button onClick={handleCancel}>{c('Action').t`Cancel`}</Button>
+                            </p>
+                            <Alert>{c('Info').t`Payment can take a few minutes to fully verify.`}</Alert>
+                        </>
+                    ),
+                    [STEPS.FAIL]: (
+                        <div className="aligncenter">
+                            <p>{c('Info')
+                                .t`Please try again, use a different payment method, or call your bank for assistance.`}</p>
+                            <img src={errorSvg} alt={c('Title').t`Error`} />
+                            {error.tryAgain ? (
+                                <p>
+                                    <Button type="submit">{c('Action').t`Try again`}</Button>
+                                </p>
+                            ) : null}
+                            <p>
+                                <PrimaryButton onClick={handleCancel}>{c('Action')
+                                    .t`Use a different payment method`}</PrimaryButton>
+                            </p>
+                        </div>
+                    )
+                }[step]
+            }
         </FormModal>
     );
 };
