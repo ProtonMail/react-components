534dd23c15fbd868544f82d7d55790020a30d0e2
Yiin <stanislovas.janonis@gmail.com>
2019-09-30 07:32:29 +0300

    Focus dropdown

--- components/popper/Popper.js
+++ components/popper/Popper.js
@@ -1,11 +1,29 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import PropTypes from 'prop-types';
 import Portal from '../portal/Portal';
 
 const Popper = React.forwardRef(({ children, position, isOpen, role = 'tooltip', ...rest }, ref) => {
+    useEffect(() => {
+        if (isOpen) {
+            ref.current.focus();
+        }
+    }, [isOpen]);
+
     return (
         <Portal>
-            <div {...rest} ref={ref} style={position} role={role} hidden={!isOpen} aria-hidden={!isOpen}>
+            <div
+                {...rest}
+                ref={ref}
+                style={{
+                    outline: 'none',
+                    ...position
+                }}
+                role={role}
+                hidden={!isOpen}
+                aria-hidden={!isOpen}
+                aria-modal="true"
+                tabIndex="-1"
+            >
                 {children}
             </div>
         </Portal>
