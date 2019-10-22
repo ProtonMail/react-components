1fe8c026646c265e65feebbac6a90b6a56957107
Richard <richard@protonmail.com>
2019-10-22 12:16:33 +0200

    Review Sidebar for responsive

--- components/sidebar/Sidebar.js
+++ components/sidebar/Sidebar.js
@@ -3,13 +3,16 @@ import PropTypes from 'prop-types';
 
 import NavMenu from './NavMenu';
 import MainLogo from '../logo/MainLogo';
+import Hamburger from './Hamburger';
 
-const Sidebar = ({ expanded = false, list = [], url = '/account' }) => {
+const Sidebar = ({ expanded = false, onToggleExpand, list = [], url = '/account', children }) => {
     return (
         <div className="sidebar flex flex-column noprint" data-expanded={expanded}>
-            <div className="nodesktop notablet">
+            <div className="nodesktop notablet flex flex-spacebetween flex-items-center">
                 <MainLogo url={url} />
+                <Hamburger expanded={expanded} onToggle={onToggleExpand} />
             </div>
+            {children}
             <nav className="navigation mw100 flex-item-fluid scroll-if-needed mb1">
                 <NavMenu list={list} />
             </nav>
@@ -20,7 +23,9 @@ const Sidebar = ({ expanded = false, list = [], url = '/account' }) => {
 Sidebar.propTypes = {
     list: PropTypes.arrayOf(PropTypes.object),
     url: PropTypes.string,
-    expanded: PropTypes.bool
+    expanded: PropTypes.bool,
+    onToggleExpand: PropTypes.func,
+    children: PropTypes.node
 };
 
 export default Sidebar;
