9ceae104d6a37870391ab0429e78d892aa05a16c
Richard <EpokK@users.noreply.github.com>
2019-10-22 16:21:59 +0200

    Feat/mobile nav services (#374)
    * Add mobile navigation between services in sidebar
    
    * Continue
    
    * Review

--- components/sidebar/Sidebar.js
+++ components/sidebar/Sidebar.js
@@ -2,13 +2,15 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 import NavMenu from './NavMenu';
+import MobileNavServices from './MobileNavServices';
+import MobileNavLink from './MobileNavLink';
 import MainLogo from '../logo/MainLogo';
 import Hamburger from './Hamburger';
 
-const Sidebar = ({ expanded = false, onToggleExpand, list = [], url = '/account', children }) => {
+const Sidebar = ({ expanded = false, onToggleExpand, list = [], url = '', mobileLinks = [], children }) => {
     return (
         <div className="sidebar flex flex-column noprint" data-expanded={expanded}>
-            <div className="nodesktop notablet">
+            <div className="nodesktop notablet flex-item-noshrink">
                 <div className="flex flex-spacebetween flex-items-center">
                     <MainLogo url={url} />
                     <Hamburger expanded={expanded} onToggle={onToggleExpand} />
@@ -18,14 +20,22 @@ const Sidebar = ({ expanded = false, onToggleExpand, list = [], url = '/account'
             <nav className="navigation mw100 flex-item-fluid scroll-if-needed mb1">
                 <NavMenu list={list} />
             </nav>
+            {mobileLinks.length ? (
+                <MobileNavServices>
+                    {mobileLinks.map(({ to, icon, external, current }) => {
+                        return <MobileNavLink key={icon} to={to} icon={icon} external={external} current={current} />;
+                    })}
+                </MobileNavServices>
+            ) : null}
         </div>
     );
 };
 
 Sidebar.propTypes = {
     list: PropTypes.arrayOf(PropTypes.object),
-    url: PropTypes.string,
-    expanded: PropTypes.bool,
+    mobileLinks: PropTypes.arrayOf(PropTypes.object),
+    url: PropTypes.string.isRequired,
+    expanded: PropTypes.bool
     onToggleExpand: PropTypes.func,
     children: PropTypes.node
 };
