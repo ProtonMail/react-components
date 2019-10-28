3699e5eee80082dad02c872874a1ca6b43cf5f8a
edu <econdepe@protonmail.com>
2019-10-28 14:49:40 +0100

    add prop to force image display

--- components/image/RemoteImage.js
+++ components/image/RemoteImage.js
@@ -6,20 +6,21 @@ import { isURL } from 'proton-shared/lib/helpers/validators';
 import Button from '../button/Button';
 import useMailSettings from '../../hooks/useMailSettings';
 
-const RemoteImage = (props) => {
+const RemoteImage = ({ src, show = false, ...rest }) => {
     const [{ ShowImages }, loading] = useMailSettings();
-    const [showAnyways, setShowAnyways] = useState(!isURL(props.src));
+    const [showAnyways, setShowAnyways] = useState(show || !isURL(src));
 
     const handleClick = () => setShowAnyways(true);
 
     if ((!loading && ShowImages & SHOW_IMAGES.REMOTE) || showAnyways) {
-        return <img {...props} />;
+        return <img src={src} {...rest} />;
     }
     return <Button onClick={handleClick}>{c('Action').t`Load image`}</Button>;
 };
 
 RemoteImage.propTypes = {
-    src: PropTypes.string.isRequired
+    src: PropTypes.string.isRequired,
+    show: PropTypes.bool
 };
 
 export default RemoteImage;
