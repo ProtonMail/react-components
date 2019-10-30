f357617b430b22d85a071b8a2e784ec72c6d5dfa
Swiip <matthieu.lux@gmail.com>
2019-10-30 17:45:53 +0100

    Fix settings dashboard anchor links

--- containers/overview/LinkItem.js
+++ containers/overview/LinkItem.js
@@ -1,26 +1,41 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { Link } from 'react-router-dom';
+import { withRouter } from 'react-router-dom';
 import { Tooltip, Icon } from 'react-components';
 import { c } from 'ttag';
 
-const LinkItem = ({ route, text, permission }) => {
+const LinkItem = ({ route, text, permission, history }) => {
+    const handleClick = async (event) => {
+        event.preventDefault();
+
+        history.push(route.pathname);
+
+        // Let the router do the navigation
+        await null;
+
+        const el = document.querySelector(`#${route.hash}`);
+        if (el) {
+            el.scrollIntoView({ behavior: 'smooth', block: 'start' });
+        }
+    };
+
     return (
-        <Link to={route}>
+        <a onClick={handleClick} href={`${route.pathname}#${route.hash}`}>
             <span className="mr0-5">{text}</span>
             {permission ? null : (
                 <Tooltip title={c('Tag').t`Premium feature`}>
                     <Icon name="starfull" fill="attention" />
                 </Tooltip>
             )}
-        </Link>
+        </a>
     );
 };
 
 LinkItem.propTypes = {
-    route: PropTypes.string,
+    route: PropTypes.object,
     permission: PropTypes.bool,
-    text: PropTypes.string
+    text: PropTypes.string,
+    history: PropTypes.object
 };
 
-export default LinkItem;
+export default withRouter(LinkItem);
