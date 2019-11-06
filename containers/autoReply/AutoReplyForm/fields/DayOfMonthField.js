0a1847c6cf4aad4130cdd7f6eaca194e86b9af62
mmso <mmso@mmso.se>
2019-11-06 09:33:43 +0100

    Update day of month field

--- containers/autoReply/AutoReplyForm/fields/DayOfMonthField.js
+++ containers/autoReply/AutoReplyForm/fields/DayOfMonthField.js
@@ -1,22 +1,17 @@
-import React, { useMemo } from 'react';
+import React from 'react';
 import { Row, Label, Field, Select } from 'react-components';
 import PropTypes from 'prop-types';
-import { dateLocale } from 'proton-shared/lib/i18n';
 
 import { getDaysOfMonthOptions } from '../../utils';
 
 const DayOfMonthField = ({ id, label, value, onChange }) => {
     const handleChange = ({ target }) => onChange(+target.value);
 
-    const options = useMemo(() => {
-        return getDaysOfMonthOptions({ locale: dateLocale });
-    }, [dateLocale]);
-
     return (
         <Row>
             <Label htmlFor={id}>{label}</Label>
             <Field>
-                <Select id={id} options={options} value={value} onChange={handleChange} />
+                <Select id={id} options={getDaysOfMonthOptions()} value={value} onChange={handleChange} />
             </Field>
         </Row>
     );
