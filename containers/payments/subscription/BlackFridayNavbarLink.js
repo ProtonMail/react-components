b152150e0bf6adbf3dc75b97f6f386138a62ab79
Richard <richard@protonmail.com>
2019-11-18 18:50:13 +0100

    Review with @mmso

--- containers/payments/subscription/BlackFridayNavbarLink.js
+++ containers/payments/subscription/BlackFridayNavbarLink.js
@@ -1,10 +1,7 @@
 import React, { useEffect, useState, useRef } from 'react';
-import differenceInDays from 'date-fns/differenceInDays';
 import PropTypes from 'prop-types';
 import { useUser, useApi, useLoading, useBlackFriday, TopNavbarLink } from 'react-components';
-import { getLastCancelledSubscription } from 'proton-shared/lib/api/payments';
-
-const ONE_MONTH = 30;
+import { checkLastCancelledSubscription } from './helpers';
 
 const BlackFridayNavbarLink = ({ to }) => {
     const api = useApi();
@@ -14,17 +11,9 @@ const BlackFridayNavbarLink = ({ to }) => {
     const [isEligible, setEligibility] = useState(false);
     const checked = useRef(false);
 
-    const check = async () => {
-        // Return the latest subscription cancellation time, return null if the user never had any subscription, 0 if the user currently has an active subscription
-        const { LastSubscriptionEnd = 0 } = (await api(getLastCancelledSubscription())) || {};
-        const now = new Date();
-
-        setEligibility(LastSubscriptionEnd ? differenceInDays(now, new Date(LastSubscriptionEnd)) >= ONE_MONTH : false); // TODO change LastSubscriptionEnd < 30th September
-    };
-
     useEffect(() => {
         if (!checked.current && user.isFree && isBlackFriday) {
-            withLoading(check());
+            withLoading(checkLastCancelledSubscription(api).then(setEligibility));
             checked.current = true;
         }
     }, [isBlackFriday]);
