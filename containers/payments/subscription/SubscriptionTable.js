9df833f03cf019097a60747d8163fad1d3afd034
EpokK <richard.d@protonmail.com>
2019-11-24 16:43:36 +0100

    Starting

---
+++ containers/payments/subscription/SubscriptionTable.js
@@ -0,0 +1,70 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { useToggle, Button, classnames, LinkButton } from 'react-components';
+import { c } from 'ttag';
+
+const SubscriptionTable = ({ plans, onSelect, currentPlanIndex = 0, mostPopularIndex = 0 }) => {
+    const { state: showAllFeatures, toggle } = useToggle();
+
+    return (
+        <div className="bordered-container">
+            <div className="flex flex-nowrap">
+                {plans.map(({ planName, price, imageSrc, description, features = [] }, index) => {
+                    return (
+                        <div
+                            key={planName}
+                            className="pt2 pb2 pl1 pr1 border-right"
+                            data-current-plan={index === currentPlanIndex}
+                            data-most-popular={index === mostPopularIndex}
+                        >
+                            {index === mostPopularIndex ? (
+                                <div className="mb0-5">{c('Title for subscription plan').t`Most popular`}</div>
+                            ) : null}
+                            <div className="bold aligncenter mb0-5">{planName}</div>
+                            <div className="aligncenter mb0-5">{price}</div>
+                            <div className="aligncenter">
+                                <img src={imageSrc} alt={planName} />
+                            </div>
+                            <p className="aligncenter mb1">{description}</p>
+                            <ul className="small mb1">
+                                {features.map((feature, index) => {
+                                    return <li key={index}>{feature}</li>;
+                                })}
+                            </ul>
+                            <div className="aligncenter">
+                                <Button
+                                    className={classnames([index !== currentPlanIndex && 'pm-button--primary'])}
+                                    onClick={() => onSelect(index)}
+                                >
+                                    {index === currentPlanIndex ? c('Action').t`Customize` : c('Action').t`Select`}
+                                </Button>
+                            </div>
+                        </div>
+                    );
+                })}
+            </div>
+            {showAllFeatures ? (
+                <div className="aligncenter p1 border-top">
+                    <LinkButton onClick={toggle}>{c('Action').t`Compare all features`}</LinkButton>
+                </div>
+            ) : null}
+        </div>
+    );
+};
+
+SubscriptionTable.propTypes = {
+    plans: PropTypes.arrayOf(
+        PropTypes.shape({
+            planName: PropTypes.string,
+            price: PropTypes.node,
+            imageSrc: PropTypes.string,
+            description: PropTypes.node,
+            features: PropTypes.arrayOf(PropTypes.node)
+        })
+    ),
+    onSelect: PropTypes.func.isRequired,
+    currentPlanIndex: PropTypes.number,
+    mostPopularIndex: PropTypes.number
+};
+
+export default SubscriptionTable;
