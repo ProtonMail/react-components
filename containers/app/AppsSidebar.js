0a33048872a91ab54a0a8477c7e68052a285d65c
vincaslt <vincaslt@gmail.com>
2019-11-25 11:10:36 +0200

    Drive link in apps sidebar

--- containers/app/AppsSidebar.js
+++ containers/app/AppsSidebar.js
@@ -3,23 +3,26 @@ import PropTypes from 'prop-types';
 import { Icon, useConfig, Tooltip } from 'react-components';
 import { APPS } from 'proton-shared/lib/constants';
 
-const { PROTONMAIL, PROTONCONTACTS, PROTONMAIL_SETTINGS } = APPS;
+const { PROTONMAIL, PROTONCONTACTS, PROTONMAIL_SETTINGS, PROTONDRIVE } = APPS;
 
+// TODO: show drive only for invited users, also add the correct icon
 const AppsSidebar = ({ items = [] }) => {
     const { APP_NAME } = useConfig();
     const apps = [
         { appNames: [PROTONMAIL, PROTONMAIL_SETTINGS], icon: 'protonmail', title: 'ProtonMail', link: '/inbox' },
-        { appNames: [PROTONCONTACTS], icon: 'protoncontacts', title: 'ProtonContacts', link: '/contacts' }
+        { appNames: [PROTONCONTACTS], icon: 'protoncontacts', title: 'ProtonContacts', link: '/contacts' },
+        { appNames: [PROTONDRIVE], icon: 'user-storage', title: 'ProtonDrive', link: '/drive', highlight: true }
     ].filter(Boolean);
 
     return (
         <aside className="aside noprint nomobile" id="aside-bar">
             <ul className="unstyled m0 aligncenter flex flex-column h100">
-                {apps.map(({ appNames = [], icon, title, link, target }, index) => {
+                {apps.map(({ appNames = [], icon, title, link, target, highlight }, index) => {
                     const isCurrent = appNames.includes(APP_NAME);
                     const key = `${index}`;
                     return (
-                        <li key={key} className="mb0-5">
+                        <li key={key} className="mb0-5 relative">
+                            {highlight && <span className="aside-link-highlight" />}
                             <Tooltip title={title} originalPlacement="right">
                                 <a
                                     href={link}
