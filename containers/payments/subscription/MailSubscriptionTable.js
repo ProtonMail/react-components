21e7706f715c4b6bf12de09ecab5dcc3832c5849
EpokK <richard.d@protonmail.com>
2019-11-27 16:09:22 +0100

    Continue

--- containers/payments/subscription/MailSubscriptionTable.js
+++ containers/payments/subscription/MailSubscriptionTable.js
@@ -35,13 +35,31 @@ const MailSubscriptionTable = ({ subscription = {}, plans: apiPlans = [], cycle,
             canCustomize: true,
             price: <SubscriptionPrices cycle={cycle} currency={currency} plan={FREE_PLAN} />,
             imageSrc: freePlanSvg,
-            description: c('Description').t`The basics for private and secure communications`,
+            description: c('Description').t`Basic private and secure comminications`,
             features: [
-                { feature: c('Feature').t`1 user` },
-                { feature: c('Feature').t`500 MB storage` },
-                { feature: c('Feature').t`1 address` },
-                { feature: c('Feature').t`No domain support` },
-                { feature: c('Feature').t`ProtonVPN (optional) *` }
+                c('Feature').t`1 user`,
+                c('Feature').t`500 MB storage`,
+                c('Feature').t`1 address`,
+                c('Feature').t`No domain support`,
+                c('Feature').t`150 messages/day`,
+                c('Feature').t`ProtonVPN (optional)`
+            ],
+            allFeatures: [
+                c('Feature').t`1 user`,
+                c('Feature').t`500 MB storage`,
+                c('Feature').t`1 address`,
+                c('Feature').t`No domain support`,
+                c('Feature').t`150 messages per day`,
+                c('Feature').t`3 folders/labels`,
+                <del key="encrypted">{c('Feature').t`Encrypted contacts`}</del>,
+                <del key="address">{c('Feature').t`Address verification`}</del>,
+                <del key="filters">{c('Feature').t`Filters`}</del>,
+                <del key="imap">{c('Feature').t`IMAP/SMTP support`}</del>,
+                <del key="auto">{c('Feature').t`Auto-responder`}</del>,
+                <del key="me">{c('Feature').t`@pm.me short email`}</del>,
+                <del key="catch">{c('Feature').t`Catch-all email`}</del>,
+                <del key="multi">{c('Feature').t`Multi-user support`}</del>,
+                c('Feature').t`Limited support`
             ]
         },
         plusPlan && {
@@ -51,12 +69,29 @@ const MailSubscriptionTable = ({ subscription = {}, plans: apiPlans = [], cycle,
             imageSrc: plusPlanSvg,
             description: c('Description').t`Full-featured mailbox with advanced protection`,
             features: [
-                { feature: c('Feature').t`1 user` },
-                { feature: c('Feature').t`5 GB storage *` },
-                { feature: c('Feature').t`5 addresses *` },
-                { feature: c('Feature').t`Supports 1 domain *` },
-                { feature: c('Feature').t`Supports folder, labels, filters, auto-reply, IMAP/SMTP and more` },
-                { feature: c('Feature').t`ProtonVPN (optional) *` }
+                c('Feature').t`1 user`,
+                c('Feature').t`5 GB storage *`,
+                c('Feature').t`5 addresses *`,
+                c('Feature').t`Supports 1 domain *`,
+                c('Feature').t`Folder, labels, filters, auto-reply, IMAP/SMTP and more`,
+                c('Feature').t`ProtonVPN (optional)`
+            ],
+            allFeatures: [
+                c('Feature').t`1 user`,
+                c('Feature').t`5 GB storage *`,
+                c('Feature').t`5 addresses *`,
+                c('Feature').t`1 custom domain *`,
+                c('Feature').t`Unlimited messages **`,
+                c('Feature').t`Unlimited folders/labels`,
+                c('Feature').t`Encrypted contacts`,
+                c('Feature').t`Address verification`,
+                c('Feature').t`Filters`,
+                c('Feature').t`IMAP/SMTP support`,
+                c('Feature').t`Auto-responder`,
+                c('Feature').t`@pm.me short email`,
+                <del key="catch">{c('Feature').t`Catch-all email`}</del>,
+                <del key="multi">{c('Feature').t`Multi-user support`}</del>,
+                c('Feature').t`Normal support`
             ]
         },
         professionalPlan && {
@@ -66,12 +101,29 @@ const MailSubscriptionTable = ({ subscription = {}, plans: apiPlans = [], cycle,
             imageSrc: professionalPlanSvg,
             description: c('Description').t`ProtonMail for professionals and businesses`,
             features: [
-                { feature: c('Feature').t`1 - 5000 user *` },
-                { feature: c('Feature').t`5 GB storage per user *` },
-                { feature: c('Feature').t`5 addresses per user *` },
-                { feature: c('Feature').t`Supports 2 domains *` },
-                { feature: c('Feature').t`Catch all email, multi user management, priority support and more` },
-                { feature: c('Feature').t`ProtonVPN (optional) *` }
+                c('Feature').t`1 - 5000 user *`,
+                c('Feature').t`5 GB storage per user *`,
+                c('Feature').t`5 addresses per user *`,
+                c('Feature').t`Supports 2 domains *`,
+                c('Feature').t`Catch-all email, multi-user management`,
+                c('Feature').t`Priority support`
+            ],
+            allFeatures: [
+                c('Feature').t`1-5000 user`,
+                c('Feature').t`5 GB per user *`,
+                c('Feature').t`5 addresses per user *`,
+                c('Feature').t`2 custom domains *`,
+                c('Feature').t`Unlimited messages **`,
+                c('Feature').t`Unlimited folders/labels`,
+                c('Feature').t`Encrypted contacts`,
+                c('Feature').t`Address verification`,
+                c('Feature').t`Filters`,
+                c('Feature').t`IMAP/SMTP support`,
+                c('Feature').t`Auto-responder`,
+                c('Feature').t`@pm.me short email`,
+                c('Feature').t`Catch-all email`,
+                c('Feature').t`Multi-user support`,
+                c('Feature').t`Priority support`
             ]
         },
         visionaryPlan && {
@@ -81,12 +133,29 @@ const MailSubscriptionTable = ({ subscription = {}, plans: apiPlans = [], cycle,
             imageSrc: visionaryPlanSvg,
             description: c('Description').t`ProtonMail for families and small businesses`,
             features: [
-                { feature: c('Feature').t`6 users` },
-                { feature: c('Feature').t`20 GB storage` },
-                { feature: c('Feature').t`Supports 10 domains` },
-                { feature: c('Feature').t`Includes all features` },
-                { feature: c('Feature').t`Priority support` },
-                { feature: c('Feature').t`Includes ProtonVPN` }
+                c('Feature').t`6 users`,
+                c('Feature').t`20 GB storage`,
+                c('Feature').t`50 addresses`,
+                c('Feature').t`Supports 10 domains`,
+                c('Feature').t`Includes all features`,
+                c('Feature').t`Includes ProtonVPN`
+            ],
+            allFeatures: [
+                c('Feature').t`6 users`,
+                c('Feature').t`20 GB storage`,
+                c('Feature').t`50 addresses`,
+                c('Feature').t`10 custom domains *`,
+                c('Feature').t`Unlimited messages **`,
+                c('Feature').t`Unlimited folders/labels`,
+                c('Feature').t`Encrypted contacts`,
+                c('Feature').t`Address verification`,
+                c('Feature').t`Filters`,
+                c('Feature').t`IMAP/SMTP support`,
+                c('Feature').t`Auto-responder`,
+                c('Feature').t`@pm.me short email`,
+                c('Feature').t`Catch-all email`,
+                c('Feature').t`Multi-user support`,
+                c('Feature').t`Priority support`
             ]
         }
     ];
--- containers/payments/subscription/VpnSubscriptionTable.js
+++ containers/payments/subscription/VpnSubscriptionTable.js
@@ -4,10 +4,10 @@ import PropTypes from 'prop-types';
 import { PLAN_NAMES, PLANS, CYCLE } from 'proton-shared/lib/constants';
 import { getPlan } from 'proton-shared/lib/helpers/subscription';
 import { c } from 'ttag';
-import freePlanSvg from 'design-system/assets/img/pm-images/free-plan.svg';
-import plusPlanSvg from 'design-system/assets/img/pm-images/plus-plan.svg';
-import professionalPlanSvg from 'design-system/assets/img/pm-images/professional-plan.svg';
-import visionaryPlanSvg from 'design-system/assets/img/pm-images/visionary-plan.svg';
+import freePlanSvg from 'design-system/assets/img/pv-images/plans/free-plan.svg';
+import plusPlanSvg from 'design-system/assets/img/pv-images/plans/vpnbasic-plan.svg';
+import professionalPlanSvg from 'design-system/assets/img/pv-images/plans/vpnplus-plan.svg';
+import visionaryPlanSvg from 'design-system/assets/img/pv-images/plans/visionary-plan.svg';
 
 import SubscriptionPrices from './SubscriptionPrices';
 
@@ -34,29 +34,60 @@ const VpnSubscriptionTable = ({ subscription = {}, plans: apiPlans = [], cycle, 
             planName: 'Free',
             price: <SubscriptionPrices cycle={cycle} currency={currency} plan={FREE_PLAN} />,
             imageSrc: freePlanSvg,
-            description: c('Description').t`The basics for private and secure communications`,
-            features: []
+            description: c('Description').t`Privacy and security for everyone`,
+            features: [
+                c('Feature').t`1 VPN connection`,
+                c('Feature').t`Servers in 3 countries`,
+                c('Feature').t`Medium speed`,
+                c('Feature').t`No logs/No ads`,
+                <del key="filesharing">{c('Feature').t`Filesharing/bitorrent support`}</del>,
+                <del key="secure">{c('Feature').t`Secure Core and Tor VPN`}</del>,
+                <del key="advanced">{c('Feature').t`Advanced privacy features`}</del>,
+                <del key="access">{c('Feature').t`Access blocked content`}</del>
+            ]
         },
         vpnBasicPlan && {
             planName: PLAN_NAMES[PLANS.VPNBASIC],
             price: <SubscriptionPrices cycle={cycle} currency={currency} plan={vpnBasicPlan} />,
             imageSrc: plusPlanSvg,
-            description: c('Description').t`Full-featured mailbox with advanced protection`,
-            features: []
+            description: c('Description').t`Basic privacy features`,
+            features: [
+                c('Feature').t`2 VPN connection`,
+                c('Feature').t`Servers in XX countries`, // TODO
+                c('Feature').t`High speed`,
+                c('Feature').t`No logs/No ads`,
+                <del key="filesharing">{c('Feature').t`Filesharing/bitorrent support`}</del>,
+                <del key="secure">{c('Feature').t`Secure Core and Tor VPN`}</del>,
+                <del key="advanced">{c('Feature').t`Advanced privacy features`}</del>,
+                <del key="access">{c('Feature').t`Access blocked content`}</del>
+            ]
         },
         vpnPlusPlan && {
             planName: PLAN_NAMES[PLANS.VPNPLUS],
             price: <SubscriptionPrices cycle={cycle} currency={currency} plan={vpnPlusPlan} />,
             imageSrc: professionalPlanSvg,
-            description: c('Description').t`ProtonMail for professionals and businesses`,
-            features: []
+            description: c('Description').t`Advanced security features`,
+            features: [
+                c('Feature').t`5 VPN connection`,
+                c('Feature').t`Servers in XX countries`, // TODO
+                c('Feature').t`Highest speed (10 Gbps)`,
+                c('Feature').t`No logs/No ads`,
+                c('Feature').t`Filesharing/bitorrent support`,
+                c('Feature').t`Secure Core and Tor VPN`,
+                c('Feature').t`Advanced privacy features`,
+                c('Feature').t`Access blocked content`
+            ]
         },
         visionaryPlan && {
             planName: PLAN_NAMES[PLANS.VISIONARY],
             price: <SubscriptionPrices cycle={cycle} currency={currency} plan={visionaryPlan} />,
             imageSrc: visionaryPlanSvg,
-            description: c('Description').t`ProtonMail for families and small businesses`,
-            features: []
+            description: c('Description').t`The complete privacy suite`,
+            features: [
+                c('Feature').t`All Plus plan features`,
+                c('Feature').t`10 simultaneous VPN connections`,
+                c('Feature').t`ProtonMail Visionary account`
+            ]
         }
     ];
     const { Name } = getPlan(subscription);
@@ -64,7 +95,7 @@ const VpnSubscriptionTable = ({ subscription = {}, plans: apiPlans = [], cycle, 
     return (
         <SubscriptionTable
             currentPlanIndex={INDEXES[Name] || 0}
-            mostPopularIndex={1}
+            mostPopularIndex={2}
             plans={plans}
             onSelect={onSelect}
         />
