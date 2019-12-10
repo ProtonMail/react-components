06fe1c27387f54c77894eef224735f2461be92a9
Richard <richard@protonmail.com>
2019-12-10 12:16:35 +0100

    Continue

--- containers/payments/subscription/NewSubscriptionModalFooter.js
+++ containers/payments/subscription/NewSubscriptionModalFooter.js
@@ -1,8 +1,10 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { useAddresses, Icon, Button, PrimaryButton, Loader } from 'react-components';
+import { useAddresses, Button, PrimaryButton, Loader } from 'react-components';
 import { c } from 'ttag';
 import { CYCLE, COUPON_CODES } from 'proton-shared/lib/constants';
+import checkmarkSvg from 'design-system/assets/img/shared/checkmark-icon.svg';
+import percentageSvg from 'design-system/assets/img/shared/percentage-icon.svg';
 
 const STEPS = {
     CUSTOMIZATION: 0,
@@ -10,6 +12,9 @@ const STEPS = {
     UPGRADE: 2,
     THANKS: 3
 };
+
+const CheckmarkIcon = () => <img className="mr0-5" src={checkmarkSvg} alt="checkmark" />;
+const PercentageIcon = () => <img className="mr0-5" src={percentageSvg} alt="percentage" />;
 
 const NewSubscriptionModalFooter = ({ submit, step, model, loading }) => {
     const [addresses, loadingAddresses] = useAddresses();
@@ -27,31 +32,31 @@ const NewSubscriptionModalFooter = ({ submit, step, model, loading }) => {
     const upsells = [
         model.cycle === CYCLE.MONTHLY && (
             <div className="nomobile">
-                <Icon className="mr0-5" fill="warning" name="off" />
+                <PercentageIcon />
                 {c('Info').t`Save 20% by switching to annual billing`}
             </div>
         ),
         model.cycle === CYCLE.YEARLY && (
             <div className="nomobile">
-                <Icon className="mr0-5" fill="success" name="on" />
+                <CheckmarkIcon />
                 {c('Info').t`You are saving 20% with annual billing`}
             </div>
         ),
         model.cycle === CYCLE.TWO_YEARS && (
             <div className="nomobile">
-                <Icon className="mr0-5" fill="success" name="on" />
+                <CheckmarkIcon />
                 {c('Info').t`You are saving 33% with 2-year billing`}
             </div>
         ),
         hasAddresses && model.coupon !== COUPON_CODES.BUNDLE && (
             <div className="nomobile">
-                <Icon className="mr0-5" fill="warning" name="off" />
+                <PercentageIcon />
                 {c('Info').t`Save an extra 20% by combining Mail and VPN`}
             </div>
         ),
         hasAddresses && model.coupon === COUPON_CODES.BUNDLE && (
             <div className="nomobile">
-                <Icon className="mr0-5" fill="success" name="on" />
+                <CheckmarkIcon />
                 {c('Info').t`You are saving an extra 20% with the bundle discount`}
             </div>
         )
