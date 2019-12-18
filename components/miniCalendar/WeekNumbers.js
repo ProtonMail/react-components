f5b784af7a83ecbe0f8eeab428831b1c7d02d525
mmso <mmso@mmso.se>
2019-12-18 13:18:17 +0100

    Calculate the week number in mini calendar with iso week numbering instead

--- components/miniCalendar/WeekNumbers.js
+++ components/miniCalendar/WeekNumbers.js
@@ -1,18 +1,21 @@
 import React, { useMemo } from 'react';
 import PropTypes from 'prop-types';
 import { c } from 'ttag';
-import { getWeek } from './helper';
+import { getISOWeek } from 'date-fns';
 
-const WeekNumbers = ({ days, numberOfWeeks }) => {
+const WeekNumbers = ({ days, numberOfWeeks, weekStartsOn }) => {
     const style = {
         '--minicalendar-weeknumbers-numberOfWeeks': numberOfWeeks + 1
     };
 
     const weekNumberLabels = useMemo(() => {
         return Array.from({ length: numberOfWeeks }, (a, i) => {
-            const weekNumber = getWeek(days[i + i * 7]);
+            // Have to calculate the iso week from monday.
+            const mondayOffset = 1 - weekStartsOn;
+            const day = days[i * 7 + (mondayOffset < 0 ? +mondayOffset + 7 : mondayOffset)];
+            const weekNumber = getISOWeek(day);
             return (
-                <span className="italic flex-item-fluid flex minicalendar-weeknumbers-number" key={weekNumber}>
+                <span className="italic flex-item-fluid flex minicalendar-weeknumbers-number" key={+day}>
                     <span className="mauto">{weekNumber}</span>
                 </span>
             );
@@ -31,7 +34,8 @@ const WeekNumbers = ({ days, numberOfWeeks }) => {
 
 WeekNumbers.propTypes = {
     days: PropTypes.array.isRequired,
-    numberOfWeeks: PropTypes.number.isRequired
+    numberOfWeeks: PropTypes.number.isRequired,
+    weekStartsOn: PropTypes.number.isRequired
 };
 
 export default WeekNumbers;
