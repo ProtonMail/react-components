f15a02a2c34c7dddfd25b77fba2d7ff4993466bf
econdepe <econdepe@protonmail.com>
2020-01-06 11:37:01 +0100

    use isSameDay to make day check

--- components/miniCalendar/helper.js
+++ components/miniCalendar/helper.js
@@ -1,4 +1,4 @@
-import { addWeeks, subWeeks, eachDayOfInterval, endOfWeek, startOfMonth, startOfWeek, getDay } from 'date-fns';
+import { addWeeks, subWeeks, eachDayOfInterval, endOfWeek, startOfMonth, startOfWeek, isSameDay } from 'date-fns';
 
 /**
  * Get all days to display in the mini calendar for a given date.
@@ -12,7 +12,7 @@ export const getDaysInMonth = (currentDate, { weekStartsOn, weeks }) => {
     const startOfWeekDate = startOfWeek(startOfMonthDate, { weekStartsOn });
     // in case of displaying 6 weeks for a month starting on weekStartsOn day,
     // display last week of previous month so that trailing days are displayed both at the beginning and the end
-    const adjust = getDay(startOfMonthDate) === weekStartsOn && weeks === 5;
+    const adjust = isSameDay(startOfMonthDate, startOfWeekDate) && weeks === 5;
 
     const start = adjust ? subWeeks(startOfWeekDate, 1) : startOfWeekDate;
     const end = endOfWeek(addWeeks(start, weeks), { weekStartsOn });
