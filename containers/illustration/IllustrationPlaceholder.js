9d3b3b5d390ac5d00409a8888034b271d15f4685
Nicolas Hoffmann <dante3333@gmail.com>
2020-01-24 15:01:26 +0100

    Feat - url for dark mode version of placeholder images

--- containers/illustration/IllustrationPlaceholder.js
+++ containers/illustration/IllustrationPlaceholder.js
@@ -2,12 +2,14 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { classnames } from '../../helpers/component';
 
-const IllustrationPlaceholder = ({ className, title, text, url, uppercase, children }) => {
+const IllustrationPlaceholder = ({ className, title, text, url, urlDarkVersion = '', uppercase, children }) => {
     const info = typeof text === 'string' ? <p>{text}</p> : text;
+    const darkImg = urlDarkVersion !== '' ? <img src={urlDarkVersion} alt={title} className="p1 mb1 display-on-darkmode" /> : null;
 
     return (
         <div className={classnames(['flex flex-column flex-items-center w100', className])}>
-            <img src={url} alt={title} className="p1 mb1" />
+            <img src={url} alt={title} className={classnames(['p1 mb1', darkImg && 'hide-on-darkmode'])} />
+            {darkImg}
             <h2 className={classnames(['bold', uppercase && 'uppercase'])}>{title}</h2>
             {info}
             {children}
@@ -20,6 +22,7 @@ IllustrationPlaceholder.propTypes = {
     title: PropTypes.string.isRequired,
     text: PropTypes.oneOfType([PropTypes.string, PropTypes.node]),
     url: PropTypes.string.isRequired,
+    urlDarkVersion: PropTypes.string,
     uppercase: PropTypes.bool,
     children: PropTypes.node
 };
