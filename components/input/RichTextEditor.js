11db2fc240c34db95b4d3f1bf5376b40d822d84a
Nicolas Hoffmann <dante3333@gmail.com>
2020-02-05 11:54:50 +0100

    Fix - update Quill styles

--- components/input/RichTextEditor.js
+++ components/input/RichTextEditor.js
@@ -2,13 +2,15 @@ import React from 'react';
 import ReactQuill from 'react-quill';
 import Quill from 'quill';
 
-// This style will be lazy loaded by LazyRichTextEditor
-import 'react-quill/dist/quill.snow.css';
-
 const Block = Quill.import('blots/block');
 Block.tagName = 'div';
 Quill.register(Block);
 
-const RichTextEditor = ({ ...rest }) => <ReactQuill {...rest} />;
+const RichTextEditor = ({ ...rest }) => {
+    // This style will be lazy loaded by LazyRichTextEditor
+    import('design-system/_sass/react-styles/quill/_snow.scss');
+
+    return <ReactQuill {...rest} />;
+};
 
 export default RichTextEditor;
