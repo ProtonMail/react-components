f6c926a4d0565a48d59aa4a6654c89b6f0c85855
Nicolas Hoffmann <dante3333@gmail.com>
2020-02-05 14:40:36 +0100

    Review

--- components/input/RichTextEditor.js
+++ components/input/RichTextEditor.js
@@ -2,15 +2,13 @@ import React from 'react';
 import ReactQuill from 'react-quill';
 import Quill from 'quill';
 
+// This style will be lazy loaded by LazyRichTextEditor
+import('design-system/_sass/react-styles/quill/_snow.scss');
+
 const Block = Quill.import('blots/block');
 Block.tagName = 'div';
 Quill.register(Block);
 
-const RichTextEditor = ({ ...rest }) => {
-    // This style will be lazy loaded by LazyRichTextEditor
-    import('design-system/_sass/react-styles/quill/_snow.scss');
-
-    return <ReactQuill {...rest} />;
-};
+const RichTextEditor = ({ ...rest }) => <ReactQuill {...rest} />;
 
 export default RichTextEditor;
