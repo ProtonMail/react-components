19656e3118287536e5251914013b4e69ea4db08d
Richard <richard@protonmail.com>
2020-02-19 20:09:56 +0100

    Review

--- containers/api/HumanVerificationModal.js
+++ containers/api/HumanVerificationModal.js
@@ -8,6 +8,15 @@ import HumanVerificationLabel from './HumanVerificationLabel';
 import CodeVerification from './CodeVerification';
 import RequestInvite from './RequestInvite';
 
+const getLabel = (method) =>
+    ({
+        captcha: c('Human verification method').t`Captcha`,
+        payment: c('Human verification method').t`Donation`,
+        sms: c('Human verification method').t`Verify with SMS`,
+        email: c('Human verification method').t`Verify with email`,
+        invite: c('Human verification method').t`Manual verification`
+    }[method]);
+
 const HumanVerificationModal = ({ token, methods = [], onSuccess, ...rest }) => {
     const title = c('Title').t`Human verification`;
     const [method, setMethod] = useState();
@@ -26,55 +35,28 @@ const HumanVerificationModal = ({ token, methods = [], onSuccess, ...rest }) => 
     return (
         <FormModal hasClose={false} hasSubmit={false} title={title} {...rest}>
             <Alert type="warning">{c('Info').t`For security reasons, please verify that you are not a robot.`}</Alert>
-            {methods.includes('captcha') ? (
+            {methods.length ? (
                 <Row>
                     <Label>
-                        {methods.includes('captcha') ? (
-                            <HumanVerificationLabel
-                                value="captcha"
-                                methods={methods}
-                                method={method}
-                                onChange={setMethod}
-                            >{c('Label').t`Captcha`}</HumanVerificationLabel>
-                        ) : null}
-                        {methods.includes('email') ? (
-                            <HumanVerificationLabel
-                                value="email"
-                                methods={methods}
-                                method={method}
-                                onChange={setMethod}
-                            >{c('Label').t`Verify with email`}</HumanVerificationLabel>
-                        ) : null}
-                        {methods.includes('sms') ? (
-                            <HumanVerificationLabel
-                                value="sms"
-                                methods={methods}
-                                method={method}
-                                onChange={setMethod}
-                            >{c('Label').t`Verify with SMS`}</HumanVerificationLabel>
-                        ) : null}
-                        {methods.includes('payment') ? (
-                            <HumanVerificationLabel
-                                value="payment"
-                                methods={methods}
-                                method={method}
-                                onChange={setMethod}
-                            >{c('Label').t`Donation`}</HumanVerificationLabel>
-                        ) : null}
-                        {methods.includes('invite') ? (
-                            <HumanVerificationLabel
-                                value="invite"
-                                methods={methods}
-                                method={method}
-                                onChange={setMethod}
-                            >{c('Label').t`Manual verification`}</HumanVerificationLabel>
-                        ) : null}
+                        {methods.map((m) => {
+                            return (
+                                <HumanVerificationLabel
+                                    value={m}
+                                    key={m}
+                                    methods={methods}
+                                    method={method}
+                                    onChange={setMethod}
+                                >
+                                    {getLabel(m)}
+                                </HumanVerificationLabel>
+                            );
+                        })}
                     </Label>
                     <div className="w100">
                         {method === 'captcha' ? <Captcha token={token} onSubmit={handleSubmit} /> : null}
                         {method === 'email' ? <CodeVerification onSubmit={handleSubmit} method="email" /> : null}
                         {method === 'sms' ? <CodeVerification onSubmit={handleSubmit} method="sms" /> : null}
-                        {methods === 'payment' ? null : null}
+                        {methods === 'payment' ? 'TODO' : null}
                         {method === 'invite' ? <RequestInvite /> : null}
                     </div>
                 </Row>
