e8c002c38c16a67383bc1f016f7cd7358510d68f
Richard <richard@protonmail.com>
2020-03-20 08:32:29 +0100

    Fix MAILWEB-91 - Active premium domain
    Fix https://jira.protontech.ch/browse/MAILWEB-91

--- containers/addresses/PmMeButton.tsx
+++ containers/addresses/PmMeButton.tsx
@@ -1,18 +1,65 @@
 import React from 'react';
 import { c } from 'ttag';
-import { AddressModal, PrimaryButton, useMembers, useModals } from '../../index';
+import { setupAddress } from 'proton-shared/lib/api/addresses';
+import {
+    UnlockModal,
+    PrimaryButton,
+    useApi,
+    useMembers,
+    useLoading,
+    useEventManager,
+    useOrganization,
+    useOrganizationKey,
+    useModals,
+    useNotifications,
+    usePremiumDomains
+} from 'react-components';
+import CreateMissingKeysAddressModal from './missingKeys/CreateMissingKeysAddressModal';
 
 const PmMeButton = () => {
-    const [members = [], loading] = useMembers();
+    const [loading, withLoading] = useLoading();
+    const { createNotification } = useNotifications();
     const { createModal } = useModals();
+    const api = useApi();
+    const { call } = useEventManager();
+    const [members, loadingMembers] = useMembers();
+    const [premiumDomains, loadingPremiumDomains] = usePremiumDomains();
+    const [organization, loadingOrganization] = useOrganization();
+    const [organizationKey, loadingOrganizationKey] = useOrganizationKey(organization);
+    const isLoadingDependencies =
+        loadingMembers || loadingPremiumDomains || loadingOrganization || loadingOrganizationKey;
     const member = members.find(({ Self }) => Self);
 
-    if (!member) {
+    if (loadingMembers || !member) {
         return null;
     }
 
+    const createPremiumAddress = async () => {
+        const [Domain = ''] = premiumDomains || [];
+        const [{ DisplayName = '', Signature = '' } = {}] = member.addresses;
+        await new Promise((resolve, reject) => {
+            createModal(<UnlockModal onClose={() => reject()} onSuccess={resolve} />);
+        });
+        const { Address } = await api(
+            setupAddress({
+                Domain,
+                DisplayName: DisplayName ? DisplayName : '', // DisplayName can be null
+                Signature: Signature ? Signature : '' // Signature can be null
+            })
+        );
+        await call();
+        createNotification({ text: c('Success').t`Premium address created` });
+        createModal(
+            <CreateMissingKeysAddressModal organizationKey={organizationKey} member={member} addresses={[Address]} />
+        );
+    };
+
     return (
-        <PrimaryButton disabled={loading} onClick={() => createModal(<AddressModal member={member} />)}>
+        <PrimaryButton
+            disabled={isLoadingDependencies}
+            loading={loading}
+            onClick={() => withLoading(createPremiumAddress())}
+        >
             {c('Action').t`Activate`}
         </PrimaryButton>
     );
