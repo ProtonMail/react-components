a7cf08bab6da47600f1f9e3653bf8d3f327518d5
mmso <mmso@mmso.se>
2020-03-26 17:54:35 +0100

    Add refresh button

--- containers/app/StandardLoadError.tsx
+++ containers/app/StandardLoadError.tsx
@@ -1,13 +1,14 @@
 import React, { useEffect } from 'react';
-import GenericError from '../error/GenericError';
 import { c } from 'ttag';
+import GenericError from '../error/GenericError';
+import { PrimaryButton } from '../../index';
 
 const StandardLoadError = () => {
     useEffect(() => {
         const wasOffline = !navigator.onLine;
 
         const handleOnline = () => {
-            // If the user was offline and it comes back online, automatically refresh the page to retry the operation.
+            // If the user was offline and comes back online, automatically refresh the page to retry the operation.
             // This is intended to handle the case where one of the dependencies fails to load due to a connection issue.
             if (wasOffline && navigator.onLine) {
                 return window.location.reload();
@@ -24,6 +25,9 @@ const StandardLoadError = () => {
         <GenericError>
             <span>{c('Error message').t`There was a problem connecting to Proton.`}</span>
             <span>{c('Error message').t`Please refresh the page or check your connection.`}</span>
+            <PrimaryButton className="mt1" onClick={() => window.location.reload()}>
+                {c('Action').t`Refresh`}
+            </PrimaryButton>
         </GenericError>
     );
 };
