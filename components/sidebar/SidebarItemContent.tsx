fa999b091a696fdaffdbfd0dacb1e9113fb207bd
Nicolas Hoffmann <dante3333@gmail.com>
2020-04-14 16:12:46 +0200

    Fix - hide text

--- components/sidebar/SidebarItemContent.tsx
+++ components/sidebar/SidebarItemContent.tsx
@@ -2,6 +2,9 @@ import React, { ReactNode } from 'react';
 
 import Icon from '../icon/Icon';
 
+import { classnames } from '../../helpers/component';
+import { useActiveBreakpoint } from 'react-components';
+
 interface Props {
     icon?: string;
     iconColor?: string;
@@ -10,13 +13,36 @@ interface Props {
 }
 
 const SidebarItemContent = ({ icon, iconColor, text, aside }: Props) => {
+    const { isNarrow } = useActiveBreakpoint();
     return (
         <span className="flex flex-nowrap w100 flex-items-center">
             {icon && (
-                <Icon color={iconColor} name={icon} className="flex-item-noshrink mr0-5 flex-item-centered-vert" />
+                <Icon
+                    color={iconColor}
+                    name={icon}
+                    className="navigation__icon flex-item-noshrink mr0-5 flex-item-centered-vert"
+                />
             )}
-            <span className="flex-item-fluid ellipsis mw100">{text}</span>
-            {aside && <span className="flex flex-items-center">{aside}</span>}
+            <span
+                className={classnames([
+                    'flex-item-fluid ellipsis mw100',
+                    !isNarrow && 'is-hidden-when-sidebar-is-collapsed'
+                    // if we put this, it does not work for folders in collapsed view (but is okay with Inbox/labels)
+                    // if we don't, it does not work for Inbox/Labels/etc.
+                ])}
+            >
+                {text}
+            </span>
+            {aside && (
+                <span
+                    className={classnames([
+                        'flex flex-items-center',
+                        !isNarrow && 'is-hidden-when-sidebar-is-collapsed'
+                    ])}
+                >
+                    {aside}
+                </span>
+            )}
         </span>
     );
 };
