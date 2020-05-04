670df45712f7ecfe78c2c56e4138c3f520cb14a8
Richard <richard@protonmail.com>
2020-05-04 15:47:07 +0200

    Continue

--- containers/signup/SignupLayout.tsx
+++ containers/signup/SignupLayout.tsx
@@ -1,55 +1,41 @@
 import React, { ReactNode } from 'react';
-import { Button, SupportDropdown } from 'react-components';
+import { LinkButton, SupportDropdown, classnames } from 'react-components';
 import { c } from 'ttag';
 
 import { SIGNUP_STEPS } from './constants';
 import { SignupModel } from './interfaces';
-import LayoutAside from './LayoutAside';
 
 interface Props {
     model: SignupModel;
     children: ReactNode;
-    aside: ReactNode;
     onBack: () => void;
 }
 
 const { PLANS, PAYMENT } = SIGNUP_STEPS;
 
-const SignupHeader = ({ onBack }: { onBack: () => void }) => {
+const SignupLayout = ({ children, onBack, model }: Props) => {
+    const larger = [PLANS, PAYMENT].includes(model.step);
+
     return (
-        <div className="flex flex-nowrap flex-justify-center flex-items-center mb2">
-            <div className="flex-item-fluid">
-                <Button onClick={onBack}>{c('Action').t`Back`}</Button>
-            </div>
-            <div className="flex-item-fluid h3 bold mb0 aligncenter">{c('Page title').t`Signup`}</div>
-            <div className="flex-item-fluid alignright">
-                <SupportDropdown className="pm-button" content={c('Action').t`Need help`} />
+        <div className="pt1 pb1 pl2 pr2 scroll-if-needed h100v">
+            <div className="flex-item-fluid flex-item-noshrink flex flex-column flex-nowrap">
+                <div className="flex flex-column flex-nowrap flex-item-noshrink">
+                    <div
+                        className={classnames([
+                            'center bg-white color-global-grey mt2 w100 p2 bordered-container flex-item-noshrink',
+                            larger ? '' : 'mw40e'
+                        ])}
+                    >
+                        <div className="flex flex-items-center flex-nowrap mb1">
+                            <LinkButton onClick={onBack}>{c('Action').t`Back`}</LinkButton>
+                            <div className="flex-item-fluid aligncenter h3 mb0">{c('Title').t`Signup`}</div>
+                            <SupportDropdown className="link" content={c('Action').t`Need help`} />
+                        </div>
+                        {children}
+                    </div>
+                </div>
             </div>
         </div>
-    );
-};
-
-const SignupLayout = ({ children, aside, onBack, model }: Props) => {
-    const noAside = [PLANS, PAYMENT].includes(model.step);
-
-    if (noAside) {
-        return (
-            <div className="flex flex-nowrap h100v">
-                <main className="bg-white color-global-grey p2">
-                    <SignupHeader onBack={onBack} />
-                    {children}
-                </main>
-            </div>
-        );
-    }
-
-    return (
-        <LayoutAside aside={aside}>
-            <div style={{ maxWidth: '500px' }}>
-                <SignupHeader onBack={onBack} />
-                {children}
-            </div>
-        </LayoutAside>
     );
 };
 
