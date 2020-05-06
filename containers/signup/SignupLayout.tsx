9ab081cec6e64e135f370823dbd252ce2a8ed390
Nicolas Hoffmann <dante3333@gmail.com>
2020-05-06 18:37:22 +0200

    Feat - layout

--- containers/signup/SignupLayout.tsx
+++ containers/signup/SignupLayout.tsx
@@ -22,14 +22,18 @@ const SignupLayout = ({ children, onBack, model }: Props) => {
                 <div className="flex flex-column flex-nowrap flex-item-noshrink">
                     <div
                         className={classnames([
-                            'center bg-white color-global-grey mt2 w100 p2 bordered-container flex-item-noshrink',
+                            'center bg-white-dm color-global-grey-dm mt2 mb2 onmobile-mt1 onmobile-pb1 w100 mw100 p2 bordered-container flex-item-noshrink',
                             larger ? '' : 'mw40e'
                         ])}
                     >
                         <div className="flex flex-items-center flex-nowrap mb1">
-                            <LinkButton onClick={onBack}>{c('Action').t`Back`}</LinkButton>
+                            <span className="flex-item-fluid">
+                                <LinkButton onClick={onBack}>{c('Action').t`Back`}</LinkButton>
+                            </span>
                             <div className="flex-item-fluid aligncenter h3 mb0">{c('Title').t`Signup`}</div>
-                            <SupportDropdown className="link" content={c('Action').t`Need help`} />
+                            <span className="flex-item-fluid alignright">
+                                <SupportDropdown className="link" content={c('Action').t`Need help`} />
+                            </span>
                         </div>
                         {children}
                     </div>
