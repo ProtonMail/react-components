cf967469702557bacf4ed935432ccde1e2218f0c
Richard <richard@protonmail.com>
2020-05-14 11:34:50 +0200

    Continue

--- containers/signup/SignLayout.tsx
+++ containers/signup/SignLayout.tsx
@@ -1,42 +1,56 @@
 import React, { ReactNode } from 'react';
-import { LinkButton, SupportDropdown, classnames } from 'react-components';
 import { c } from 'ttag';
+import { classnames, Icon } from 'react-components';
+import { Locales } from 'proton-shared/lib/interfaces/Locales';
 
-import { SIGNUP_STEPS } from './constants';
-import { SignupModel } from './interfaces';
+import './SignLayout.scss';
+import LanguageSelect from './LanguageSelect';
 
 interface Props {
-    model: SignupModel;
-    title: string;
     children: ReactNode;
+    aside?: ReactNode;
+    help?: ReactNode;
     onBack?: () => void;
+    locales: Locales;
+    larger?: boolean;
 }
 
-const { PLANS, PAYMENT } = SIGNUP_STEPS;
-
-const SignLayout = ({ children, title, onBack, model }: Props) => {
-    const larger = [PLANS, PAYMENT].includes(model.step);
-
+const SignLayout = ({ children, aside, larger, help, onBack, locales }: Props) => {
     return (
         <div className="pt1 pb1 pl2 pr2 scroll-if-needed h100v">
             <div className="flex-item-fluid flex-item-noshrink flex flex-column flex-nowrap">
                 <div className="flex flex-column flex-nowrap flex-item-noshrink">
                     <div
                         className={classnames([
-                            'center bg-white-dm color-global-grey-dm mt2 mb2 onmobile-mt1 onmobile-pb1 w100 mw100 bordered-container flex-item-noshrink',
-                            larger ? '' : 'mw40e'
+                            'center bg-white-dm color-global-grey-dm mt2 mb2 onmobile-mt1 onmobile-pb1 w100 mw100 bordered-container flex-item-noshrink flex flex-nowrap',
+                            larger ? '' : aside ? 'mw50e' : 'mw40e'
                         ])}
                     >
-                        <div className="flex flex-items-center flex-nowrap p2">
-                            <span className="flex-item-fluid">
-                                {onBack ? <LinkButton onClick={onBack}>{c('Action').t`Back`}</LinkButton> : null}
-                            </span>
-                            <div className="flex-item-fluid aligncenter h3 mb0">{title}</div>
-                            <span className="flex-item-fluid alignright">
-                                <SupportDropdown className="link" content={c('Action').t`Need help`} />
-                            </span>
+                        <div>
+                            <div className="flex flex-items-center flex-nowrap p2">
+                                <span className="flex-item-fluid">
+                                    {onBack ? (
+                                        <button onClick={onBack} type="button" title={c('Action').t`Back`}>
+                                            <Icon name="arrow-left" />
+                                        </button>
+                                    ) : null}
+                                </span>
+                                <div className="flex-item-fluid aligncenter">TODO:ProtonLogo</div>
+                                <span className="flex-item-fluid alignright" />
+                            </div>
+                            {children}
+                            <div className="flex flex-items-center flex-nowrap p2">
+                                <span className="flex-item-fluid">
+                                    <LanguageSelect locales={locales} className="noborder" />
+                                </span>
+                                <span className="flex-item-fluid alignright">{help}</span>
+                            </div>
                         </div>
-                        {children}
+                        {aside ? (
+                            <aside className="nomobile bg-global-light p2 flex flex-items-center flex-justify-center small m0">
+                                {aside}
+                            </aside>
+                        ) : null}
                     </div>
                 </div>
             </div>
