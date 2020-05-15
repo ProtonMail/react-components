a633f58caeba159ab1cd59e1d588a73f53567de3
mmso <mmso@mmso.se>
2020-05-15 11:21:21 +0200

    Add new challenge iframe

---
+++ components/frame/Challenge.tsx
@@ -0,0 +1,55 @@
+import React, { useState } from 'react';
+import { c } from 'ttag';
+import ChallengeFrame, { Props as ChallengeProps } from './ChallengeFrame';
+import { Alert, Loader } from '../../index';
+
+const Challenge = ({ children, style, ...rest }: Omit<ChallengeProps, 'src'>) => {
+    const [isLoading, setIsLoading] = useState(true);
+    const [hasError, setHasError] = useState(false);
+
+    const supportTeam = (
+        <a
+            target="_blank"
+            rel="noreferrer"
+            href="https://protonmail.com/support-form"
+            title="Contact the ProtonMail support team."
+        >
+            {c('Info').t`support team`}
+        </a>
+    );
+
+    return (
+        <div style={style}>
+            {isLoading ? <Loader /> : null}
+
+            {hasError ? (
+                <>
+                    <Alert type="error">
+                        {c('Error')
+                            .jt`Something went wrong, please refresh the page in order to proceed. If you still see this error message please contact our ${supportTeam}.`}
+                    </Alert>
+                </>
+            ) : (
+                <ChallengeFrame
+                    src="https://secure.protonmail.com/challenge/challenge.html"
+                    className={isLoading || hasError ? 'hidden' : 'w100'}
+                    innerClassName="flex-item-fluid-auto"
+                    bodyClassName="color-black bg-white"
+                    style={style}
+                    onLoaded={() => {
+                        setIsLoading(false);
+                    }}
+                    onError={() => {
+                        setHasError(true);
+                        setIsLoading(false);
+                    }}
+                    {...rest}
+                >
+                    {children}
+                </ChallengeFrame>
+            )}
+        </div>
+    );
+};
+
+export default Challenge;
