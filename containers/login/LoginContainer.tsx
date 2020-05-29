e29c9d6693b10eccc7b3cef77ad1488e3f51f4a0
Nicolas Hoffmann <dante3333@gmail.com>
2020-05-29 10:47:59 +0200

    Add illustration to component and reuse it for login

--- containers/login/LoginContainer.tsx
+++ containers/login/LoginContainer.tsx
@@ -5,6 +5,7 @@ import { Link } from 'react-router-dom';
 import LoginForm from './LoginForm';
 import SignLayout from '../signup/SignLayout';
 import ProtonLogo from '../../components/logo/ProtonLogo';
+import OneAccountIllustration from '../illustration/OneAccountIllustration';
 
 interface Props {
     onLogin: () => void;
@@ -13,17 +14,7 @@ interface Props {
 const LoginContainer = ({ onLogin }: Props) => {
     const signupLink = <Link key="signupLink" to="/signup">{c('Link').t`Create an account`}</Link>;
     return (
-        <SignLayout
-            title={c('Title').t`Sign in`}
-            center={<ProtonLogo />}
-            aside={
-                <div className="aligncenter">
-                    TODO:images
-                    <br />
-                    {c('Info').t`One account for all Proton services`}
-                </div>
-            }
-        >
+        <SignLayout title={c('Title').t`Sign in`} center={<ProtonLogo />} aside={<OneAccountIllustration />}>
             <LoginForm onLogin={onLogin} />
             <div className="mb2 alignright">{c('Info').jt`New to Proton? ${signupLink}`}</div>
         </SignLayout>
