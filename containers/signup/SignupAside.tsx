0fc29144fc7064e0b2fdef5de3f8c6343d198304
Nicolas Hoffmann <dante3333@gmail.com>
2020-05-29 10:47:59 +0200

    Add illustration to component and reuse it for login

--- containers/signup/SignupAside.tsx
+++ containers/signup/SignupAside.tsx
@@ -1,9 +1,8 @@
 import React from 'react';
-import { c } from 'ttag';
-import { Icon } from 'react-components';
 
 import { SignupModel } from './interfaces';
 import { SIGNUP_STEPS } from './constants';
+import OneAccountIllustration from '../illustration/OneAccountIllustration';
 
 interface Props {
     model: SignupModel;
@@ -13,35 +12,7 @@ const { ACCOUNT_CREATION_USERNAME, ACCOUNT_CREATION_EMAIL } = SIGNUP_STEPS;
 
 const SignupAside = ({ model }: Props) => {
     if ([ACCOUNT_CREATION_USERNAME, ACCOUNT_CREATION_EMAIL].includes(model.step)) {
-        return (
-            <div className="center flex flex-column flex-items-center">
-                <div className="inline-flex">
-                    <span className="bg-global-altgrey icon-28p rounded50 flex">
-                        <Icon name="protondrive" className="color-global-light mauto" alt="ProtonDrive"></Icon>
-                    </span>
-                    <span className="w40p"></span>
-                    <span className="bg-global-altgrey icon-20p rounded50 flex mt2">
-                        <Icon name="protoncontacts" className="color-global-light mauto" size="12"></Icon>
-                    </span>
-                </div>
-
-                <span className="bg-global-altgrey icon-42p rounded50 inline-flex mt0-25 mb0-5">
-                    <Icon name="protonmail" className="color-global-light mauto" size="24"></Icon>
-                </span>
-
-                <div className="inline-flex">
-                    <span className="bg-global-altgrey icon-28p rounded50 flex mt0-5">
-                        <Icon name="protonvpn" className="color-global-light mauto" size="20"></Icon>
-                    </span>
-                    <span className="w40p mr0-5r"></span>
-                    <span className="bg-global-altgrey icon-24p rounded50 flex ">
-                        <Icon name="protoncalendar" className="color-global-light mauto"></Icon>
-                    </span>
-                </div>
-
-                <div className="mw60 mt1 aligncenter">{c('Info').t`One account for all Proton services`}</div>
-            </div>
-        );
+        return <OneAccountIllustration />;
     }
 
     return null;
