005efab16d11a10cb5fdb5597a4441a6cf0cebeb
Matthieu Bué <matthieu.bue@gmail.com>
2020-06-02 11:32:25 +0200

    Transfer attributes

--- components/progress/Progress.tsx
+++ components/progress/Progress.tsx
@@ -9,13 +9,14 @@ interface Props {
     className?: string;
 }
 
-const Progress = ({ value = 50, max = 100, id, className }: Props) => {
+const Progress = ({ value = 50, max = 100, id, className, ...rest }: Props) => {
     return (
         <progress
             aria-describedby={id}
             className={classnames(['progressbar w100', className])}
             value={value}
             max={max}
+            {...rest}
         />
     );
 };
