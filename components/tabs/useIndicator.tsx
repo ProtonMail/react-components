4c2b6fd55bd5b7a25a63a1a409f2abdeb4da50ca
Ed Telezhnikov <edtelezhnikov@protonmail.com>
2020-06-04 18:20:19 +0300

    reset indicator on incorrect state

--- components/tabs/useIndicator.tsx
+++ components/tabs/useIndicator.tsx
@@ -9,25 +9,30 @@ const getComputedMargin = (element: HTMLLIElement) => {
     const value = window.getComputedStyle(element)?.marginRight;
     if (value.includes('px')) return Number(value.slice(0, -2));
     return 0;
-}
+};
+
+const INITIAL_STATE = {
+    scale: 0,
+    translate: '0px'
+};
 
 export const useIndicator = (tabs: { ref: RefObject<HTMLLIElement> }[], currentTabIndex: number) => {
     const tabsRef = createRef<HTMLUListElement>();
 
-    const [{ scale, translate }, setIndicatorParams] = React.useState({
-        scale: 0,
-        translate: '0px',
-    });
+    const [{ scale, translate }, setIndicatorParams] = React.useState(INITIAL_STATE);
 
     // on each route change, recalculate indicator offset and scale
     React.useEffect(() => {
-        const tabsWidthsAndRefs = tabs.map(({ ref }) => ({ref, width: getWidth(ref)}));
-        const totalTabWidth = tabsWidthsAndRefs.reduce((acc, {width}) => acc + width, 0)
-        if (totalTabWidth > getWidth(tabsRef)) return;
+        const tabProperties = tabs.map(({ ref }) => ({ ref, width: getWidth(ref), margin: ref.current ? getComputedMargin(ref.current) : 0 }));
+        const totalTabWidth = tabProperties.reduce((acc, { width, margin }) => acc + width + margin, 0);
 
-        const { offset, width } = tabsWidthsAndRefs.reduce(
-            (acc, {width, ref}, index) => {
-                const margin = ref.current ? getComputedMargin(ref.current) : 0;
+        if (totalTabWidth > getWidth(tabsRef)) {
+            setIndicatorParams(INITIAL_STATE);
+            return;
+        }
+
+        const { offset, width } = tabProperties.reduce(
+            (acc, { width, margin }, index) => {
                 // for each tab before current, add its width and margin to offset
                 if (index < currentTabIndex) {
                     acc.offset += width + margin;
@@ -36,17 +41,17 @@ export const useIndicator = (tabs: { ref: RefObject<HTMLLIElement> }[], currentT
                 if (index === currentTabIndex) acc.width = width;
                 return acc;
             },
-            { offset: 0, width: 0 },
+            { offset: 0, width: 0 }
         );
         // indicator scale is proportion to whole container width
         setIndicatorParams({
             scale: width / getWidth(tabsRef),
-            translate: `${offset}px`,
+            translate: `${offset}px`
         });
     }, [tabs, currentTabIndex]);
     return {
         scale,
         translate,
-        ref: tabsRef,
+        ref: tabsRef
     };
 };
