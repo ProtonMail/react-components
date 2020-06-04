b305f910ffcf7e43073df6cd613d15da4b611357
Jeremy Benaim <jeremybenaim@gmail.com>
2020-06-04 18:46:27 +0100

    PGP_SCHEME_TEXT enum

--- components/contacts/ContactSchemeSelect.tsx
+++ components/contacts/ContactSchemeSelect.tsx
@@ -3,8 +3,10 @@ import { c } from 'ttag';
 
 import { Select } from 'react-components';
 import { PACKAGE_TYPE, PGP_SCHEMES } from 'proton-shared/lib/constants';
-import { PGP_INLINE_TEXT, PGP_MIME_TEXT } from 'proton-shared/lib/contacts/constants';
+import { PGP_SCHEME_TEXT } from 'proton-shared/lib/contacts/constants';
 import { MailSettings } from 'proton-shared/lib/interfaces';
+
+const { INLINE, MIME } = PGP_SCHEME_TEXT;
 
 const { PGP_MIME, PGP_INLINE } = PGP_SCHEMES;
 
@@ -16,12 +18,12 @@ interface Props {
 
 const ContactSchemeSelect = ({ value, mailSettings, onChange }: Props) => {
     const { PGPScheme } = mailSettings;
-    const defaultValueText = PGPScheme === PACKAGE_TYPE.SEND_PGP_INLINE ? PGP_INLINE_TEXT : PGP_MIME_TEXT;
+    const defaultValueText = PGPScheme === PACKAGE_TYPE.SEND_PGP_INLINE ? INLINE : MIME;
 
     const options = [
         { value: '', text: c('Default encryption scheme').t`Use global default (${defaultValueText})` },
-        { value: PGP_MIME, text: PGP_MIME_TEXT },
-        { value: PGP_INLINE, text: PGP_INLINE_TEXT }
+        { value: PGP_MIME, text: MIME },
+        { value: PGP_INLINE, text: INLINE }
     ];
 
     const handleChange = ({ target }: ChangeEvent<HTMLSelectElement>) => onChange(target.value);
