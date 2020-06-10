1d014cfe6f461acd7fe8b9f8dd70a2a64c9b1630
mmso <mmso@mmso.se>
2020-06-10 19:24:42 +0200

    Move row mode

--- components/layout/PrivateMainArea.tsx
+++ components/layout/PrivateMainArea.tsx
@@ -5,14 +5,16 @@ interface Props extends React.HTMLAttributes<HTMLDivElement> {
     className?: string;
     children: React.ReactNode;
     withToolbar?: boolean;
+    rowMode?: boolean;
 }
 const PrivateMainArea = React.forwardRef(
-    ({ className, withToolbar = false, children, ...rest }: Props, ref: Ref<HTMLDivElement>) => {
+    ({ className, withToolbar = false, children, rowMode = false, ...rest }: Props, ref: Ref<HTMLDivElement>) => {
         return (
             <main
                 className={classnames([
                     'flex-item-fluid',
                     withToolbar ? 'main-area--withToolbar' : 'main-area',
+                    rowMode ? 'main-area--rowMode' : '',
                     className
                 ])}
                 ref={ref}
