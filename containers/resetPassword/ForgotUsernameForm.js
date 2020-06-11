c9463fea697c97b3e1c28bfe458afd2ee2bcebd0
Richard <richard@protonmail.com>
2020-06-11 18:54:04 +0200

    Review

--- containers/resetPassword/ForgotUsernameForm.js
+++ containers/resetPassword/ForgotUsernameForm.js
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 import { c } from 'ttag';
-import { EmailInput, Alert, PrimaryButton, Label } from 'react-components';
+import { EmailInput, PrimaryButton, Label } from 'react-components';
 
 const ForgotUsernameForm = ({ onSubmit, loading }) => {
     const [email, updateEmail] = useState('');
@@ -14,8 +14,8 @@ const ForgotUsernameForm = ({ onSubmit, loading }) => {
                 onSubmit(email);
             }}
         >
-            <Alert>{c('Info')
-                .t`Enter your recovery email address or recovery phone number and we will send you your username or email address.`}</Alert>
+            <p>{c('Info')
+                .t`Enter your recovery email address or recovery phone number and we will send you your username or email address.`}</p>
             <div className="flex onmobile-flex-column signup-label-field-container mb2">
                 <Label htmlFor="email">{c('Label').t`Recovery email`}</Label>
                 <div className="flex-item-fluid">
