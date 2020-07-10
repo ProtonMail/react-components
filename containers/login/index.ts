a04194851ba16ccf35d61ef49934f9ea01680260
mmso <mmso@mmso.se>
2020-07-10 11:10:24 +0200

    Continue

--- containers/login/index.ts
+++ containers/login/index.ts
@@ -1,5 +1,5 @@
 export { default as AbuseModal } from './AbuseModal';
-export { default as MinimalForgotUsernameContainer } from './MinimalForgotUsernameContainer';
+export { default as MinimalForgotUsernameContainer } from '../forgotUsername/MinimalForgotUsernameContainer';
 export { default as MinimalLoginContainer } from './MinimalLoginContainer';
 export { default as FooterDetails } from './FooterDetails';
 
