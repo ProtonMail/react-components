431581811056d635e3f5d4ce49b2e56ebaf6e553
Mantvidas M <mantvidasm@Mantvidass-MacBook-Pro.local>
2020-07-14 15:46:56 +0300

    Acnchor ref passed as param to ContextMenu.

--- components/contextMenu/ContextMenu.tsx
+++ components/contextMenu/ContextMenu.tsx
@@ -4,6 +4,7 @@ import { generateUID, Dropdown, DropdownMenu, DropdownMenuButton, Icon } from '.
 import { CORNERS_ONLY_PLACEMENTS } from '../popper/utils';
 
 interface Props {
+    anchorRef: React.RefObject<HTMLElement>;
     isOpen: boolean;
     position?: {
         top: number;
@@ -18,53 +19,51 @@ interface Props {
     }[];
 }
 
-const ContextMenu = React.forwardRef<HTMLElement, Props>(
-    ({ isOpen, position, close, autoClose = true, menuItems }, ref) => {
-        const [uid] = useState(generateUID('context-menu'));
+const ContextMenu = ({ anchorRef, isOpen, position, close, autoClose = true, menuItems }: Props) => {
+    const [uid] = useState(generateUID('context-menu'));
 
-        useEffect(() => {
-            if (!isOpen) {
-                return;
+    useEffect(() => {
+        if (!isOpen) {
+            return;
+        }
+
+        const handleContextMenu = () => {
+            if (autoClose) {
+                close();
             }
+        };
 
-            const handleContextMenu = () => {
-                if (autoClose) {
-                    close();
-                }
-            };
+        document.addEventListener('contextmenu', handleContextMenu);
 
-            document.addEventListener('contextmenu', handleContextMenu);
+        return () => {
+            document.removeEventListener('contextmenu', handleContextMenu);
+        };
+    }, [isOpen, autoClose, close]);
 
-            return () => {
-                document.removeEventListener('contextmenu', handleContextMenu);
-            };
-        }, [isOpen, autoClose, close]);
+    const dropdownMenuButtons = menuItems.map((item) => (
+        <DropdownMenuButton key={item.name} className="flex flex-nowrap alignleft" onClick={item.onClick}>
+            <Icon className="mt0-25 mr0-5" name={item.icon} />
+            {item.name}
+        </DropdownMenuButton>
+    ));
 
-        const dropdownMenuButtons = menuItems.map((item) => (
-            <DropdownMenuButton key={item.name} className="flex flex-nowrap alignleft" onClick={item.onClick}>
-                <Icon className="mt0-25 mr0-5" name={item.icon} />
-                {item.name}
-            </DropdownMenuButton>
-        ));
+    const drowpdown = menuItems.length ? (
+        <Dropdown
+            id={uid}
+            isOpen={isOpen}
+            originalPosition={position}
+            availablePlacements={CORNERS_ONLY_PLACEMENTS}
+            noCaret
+            originalPlacement="bottom-left"
+            offset={0}
+            anchorRef={anchorRef}
+            onClose={close}
+        >
+            <DropdownMenu>{dropdownMenuButtons}</DropdownMenu>
+        </Dropdown>
+    ) : null;
 
-        const drowpdown = menuItems.length ? (
-            <Dropdown
-                id={uid}
-                isOpen={isOpen}
-                originalPosition={position}
-                availablePlacements={CORNERS_ONLY_PLACEMENTS}
-                noCaret
-                originalPlacement="bottom-left"
-                offset={0}
-                anchorRef={ref as any}
-                onClose={close}
-            >
-                <DropdownMenu>{dropdownMenuButtons}</DropdownMenu>
-            </Dropdown>
-        ) : null;
-
-        return drowpdown;
-    }
-);
+    return drowpdown;
+};
 
 export default ContextMenu;
