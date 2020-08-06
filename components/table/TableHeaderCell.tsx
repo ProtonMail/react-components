0bfd33065cc00c689910d6ab6c6a0428b77f8d7c
Mantvidas M <mantvidasm@Mantvidass-MacBook-Pro.local>
2020-08-06 10:52:17 +0300

    direction check added.

--- components/table/TableHeaderCell.tsx
+++ components/table/TableHeaderCell.tsx
@@ -12,13 +12,13 @@ const TableHeaderCell = ({ children, direction, onSort, ...rest }: Props) => {
     const content = onSort ? (
         <div className="flex-nowrap inline-flex-vcenter" onClick={onSort}>
             <span className="mr0-25">{children}</span>
-            {
+            {direction && (
                 <Icon
                     name="caret"
                     size={12}
                     className={`flex-item-noshrink ${direction === SORT_DIRECTION.DESC ? '' : 'rotateX-180'}`}
                 />
-            }
+            )}
         </div>
     ) : (
         children
