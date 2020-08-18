7d582b1282ec46b267aaaf95cc230723121cc51c
Richard <richard@protonmail.com>
2020-08-18 12:11:43 +0200

    Review

--- containers/overview/index.ts
+++ containers/overview/index.ts
@@ -1,3 +1,2 @@
 export { default as IndexSection } from './IndexSection';
 export { default as SummarySection } from './SummarySection';
-export { default as PromoteSection } from './PromoteSection';
