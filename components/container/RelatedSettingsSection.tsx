ea7730a771653f5fda16d7749c9555ec96d37444
Jeremy Benaim <jeremybenaim@gmail.com>
2020-08-21 15:38:36 +0100

    Review

--- components/container/RelatedSettingsSection.tsx
+++ components/container/RelatedSettingsSection.tsx
@@ -7,40 +7,35 @@ interface BlockListItem {
     icon: string;
     text: string;
     to: string;
-    link: string;
+    link: string | React.ReactElement;
 }
 
 interface Props {
     list: BlockListItem[];
-    children?: React.ReactNode;
 }
 
-const RelatedSettingsSection = ({ list = [], children }: Props) => {
+const RelatedSettingsSection = ({ list = [] }: Props) => {
     if (list.length > 2) {
         throw new Error('You can only display 2 blocks in RelatedSettingsSection');
     }
     return (
         <div className="flex flex-spacebetween ontablet-flex-column">
-            {list.map(({ icon, text, to, link }: BlockListItem, index) => {
-                return (
-                    <div key={index.toString()} className="w45 flex ontablet-mb1">
-                        <Information icon={icon}>
-                            {children ? (
-                                children
+            {list.map(({ icon, text, to, link }: BlockListItem, index) => (
+                <div key={index.toString()} className="w45 flex ontablet-mb1">
+                    <Information icon={icon}>
+                        <Paragraph>{text}</Paragraph>
+                        <Paragraph className="aligncenter mtauto">
+                            {typeof link === 'string' ? (
+                                <Link className="pm-button pm-button--primary" to={to}>
+                                    {link}
+                                </Link>
                             ) : (
-                                <>
-                                    <Paragraph>{text}</Paragraph>
-                                    <Paragraph className="aligncenter mtauto">
-                                        <Link className="pm-button pm-button--primary" to={to}>
-                                            {link}
-                                        </Link>
-                                    </Paragraph>
-                                </>
+                                link
                             )}
-                        </Information>
-                    </div>
-                );
-            })}
+                        </Paragraph>
+                    </Information>
+                </div>
+            ))}
         </div>
     );
 };
