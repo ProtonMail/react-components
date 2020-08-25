1dd3dfbfccddb626a9c56bb355c7cc400d9f6648
Richard <richard@protonmail.com>
2020-08-25 17:29:53 +0200

    Add all sections

---
+++ containers/general/DateFormatSection.tsx
@@ -0,0 +1,44 @@
+import React, { ChangeEvent } from 'react';
+import { c } from 'ttag';
+import { SETTINGS_DATE_FORMAT } from 'proton-shared/lib/interfaces/calendar';
+import { updateDateFormat } from 'proton-shared/lib/api/settings';
+
+import { Row, Label, Field, Select } from '../../components';
+import { useApi, useEventManager, useNotifications, useLoading, useUserSettings } from '../../hooks';
+
+const DateFormatSection = () => {
+    const api = useApi();
+    const [userSettings] = useUserSettings();
+    const { call } = useEventManager();
+    const { createNotification } = useNotifications();
+    const [loading, withLoading] = useLoading();
+
+    const handleDateFormat = async (value: SETTINGS_DATE_FORMAT) => {
+        await api(updateDateFormat(value));
+        await call();
+        createNotification({ text: c('Success').t`Preference saved` });
+    };
+
+    return (
+        <Row>
+            <Label htmlFor="date-format-select">{c('Label').t`Date format`}</Label>
+            <Field>
+                <Select
+                    id="date-format-select"
+                    loading={loading}
+                    onChange={({ target }: ChangeEvent<HTMLSelectElement>) =>
+                        withLoading(handleDateFormat(+target.value))
+                    }
+                    value={userSettings.DateFormat}
+                    options={[
+                        { text: 'DD/MM/YYYY', value: SETTINGS_DATE_FORMAT.DDMMYYYY },
+                        { text: 'MM/DD/YYYY', value: SETTINGS_DATE_FORMAT.MMDDYYYY },
+                        { text: 'YYYY/MM/DD', value: SETTINGS_DATE_FORMAT.YYYYMMDD },
+                    ]}
+                />
+            </Field>
+        </Row>
+    );
+};
+
+export default DateFormatSection;
---
+++ containers/general/TimeFormatSection.tsx
@@ -0,0 +1,45 @@
+import React, { ChangeEvent } from 'react';
+import { c } from 'ttag';
+import { SETTINGS_TIME_FORMAT } from 'proton-shared/lib/interfaces/calendar';
+import updateLongLocale from 'proton-shared/lib/i18n/updateLongLocale';
+import { updateTimeFormat } from 'proton-shared/lib/api/settings';
+
+import { Row, Label, Field, Select } from '../../components';
+import { useApi, useEventManager, useNotifications, useLoading, useUserSettings } from '../../hooks';
+
+const TimeSection = () => {
+    const api = useApi();
+    const [userSettings] = useUserSettings();
+    const { call } = useEventManager();
+    const { createNotification } = useNotifications();
+    const [loading, withLoading] = useLoading();
+
+    const handleTimeFormat = async (value: SETTINGS_TIME_FORMAT) => {
+        await api(updateTimeFormat(value));
+        await call();
+        updateLongLocale({ displayAMPM: value === SETTINGS_TIME_FORMAT.H12 });
+        createNotification({ text: c('Success').t`Preference saved` });
+    };
+
+    return (
+        <Row>
+            <Label htmlFor="time-format-select">{c('Label').t`Time format`}</Label>
+            <Field>
+                <Select
+                    id="time-format-select"
+                    loading={loading}
+                    onChange={({ target }: ChangeEvent<HTMLSelectElement>) =>
+                        withLoading(handleTimeFormat(+target.value))
+                    }
+                    value={userSettings.TimeFormat}
+                    options={[
+                        { text: '12h', value: SETTINGS_TIME_FORMAT.H12 },
+                        { text: '24h', value: SETTINGS_TIME_FORMAT.H24 },
+                    ]}
+                />
+            </Field>
+        </Row>
+    );
+};
+
+export default TimeSection;
