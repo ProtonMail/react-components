6946347443760b89c4afb02098e3e15a27bfaf1a
Vincas Stonys <vincaslt@gmail.com>
2020-09-21 12:54:00 +0300

    Fix useEventManager types

--- hooks/useEventManager.ts
+++ hooks/useEventManager.ts
@@ -1,15 +1,15 @@
 import { useContext } from 'react';
-
+import { EventManager } from 'proton-shared/lib/eventManager/eventManager';
 import Context from '../containers/eventManager/context';
 
-const useEventManager = () => {
+function useEventManager<EventModel, ListenerResult = void>() {
     const eventManager = useContext(Context);
 
     if (!eventManager) {
         throw new Error('Trying to use uninitialized EventManagerContext');
     }
 
-    return eventManager;
-};
+    return eventManager as EventManager<EventModel, ListenerResult>;
+}
 
 export default useEventManager;
