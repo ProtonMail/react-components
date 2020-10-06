1398e9d8670d3632bf711234cfaa68518ffc9373
mmso <mmso@mmso.se>
2020-10-06 17:03:05 +0200

    Add app version display

--- components/version/AppVersion.tsx
+++ components/version/AppVersion.tsx
@@ -6,11 +6,12 @@ import ChangelogModal from './ChangelogModal';
 
 interface Props {
     appName?: string;
+    appVersion?: string;
     changelog?: string;
 }
 
-const AppVersion = ({ appName: maybeAppName, changelog }: Props) => {
-    const { APP_NAME, APP_VERSION, DATE_VERSION } = useConfig();
+const AppVersion = ({ appVersion: maybeAppVersion, appName: maybeAppName, changelog }: Props) => {
+    const { APP_NAME, APP_VERSION, APP_VERSION_DISPLAY, DATE_VERSION } = useConfig();
     const { createModal } = useModals();
 
     const handleModal = () => {
@@ -18,6 +19,7 @@ const AppVersion = ({ appName: maybeAppName, changelog }: Props) => {
     };
 
     const appName = maybeAppName || APPS_CONFIGURATION[APP_NAME]?.name;
+    const appVersion = maybeAppVersion || APP_VERSION_DISPLAY || APP_VERSION;
 
     return (
         <button
@@ -26,7 +28,7 @@ const AppVersion = ({ appName: maybeAppName, changelog }: Props) => {
             title={DATE_VERSION}
             className="smallest aligncenter opacity-50 mt0 mb0-5"
         >
-            {appName} {APP_VERSION}
+            {appName} {appVersion}
         </button>
     );
 };
