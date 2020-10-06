51c93be403c1e526a2e7da786e8ccd9083ad5271
Jeremy Benaim <jeremybenaim@gmail.com>
2020-10-06 08:11:31 +0200

    Fix translations

--- containers/import/modals/steps/ImportInstructionsStep.tsx
+++ containers/import/modals/steps/ImportInstructionsStep.tsx
@@ -1,8 +1,6 @@
 import React, { useState } from 'react';
 import { c } from 'ttag';
 
-import { randomHexString4 } from 'proton-shared/lib/helpers/uid';
-
 import gmailImapFolderImg from 'design-system/assets/img/import-instructions/gmail-folder-imap.jpg';
 import gmailImapImg from 'design-system/assets/img/import-instructions/gmail-imap.jpg';
 import gmailLessSecureAppsImg from 'design-system/assets/img/import-instructions/gmail-less-secure-apps.jpg';
@@ -21,22 +19,25 @@ interface Props {
 
 const ImportInstructionsStep = ({ changeProvider, provider, instructionsCurrentStep }: Props) => {
     const [tabIndex, setTabIndex] = useState(0);
+
+    /* The following consts are to be used in translations */
+    const googleAccountLink = (
+        <Href url="https://myaccount.google.com" key="googleAccountLink">
+            myaccount.google.com
+        </Href>
+    );
+    const boldSecurity = <strong key="boldSecurity">{c('Import instructions emphasis').t`Security`}</strong>;
+    const boldAccess = <strong key="boldAccess">{c('Import instructions emphasis').t`turn on access`}</strong>;
+    const boldAppPassword = <strong key="boldAppPassword">{c('Import instructions emphasis').t`app password`}</strong>;
+
     const gmailTabs = [
         {
             title: c('Import instructions tab title').t`2-step verification disabled (default settings)`,
             content: (
                 <>
                     <div className="mb1">
-                        {c('Import instructions').jt`Go to ${(
-                            <Href key={randomHexString4()} url="https://myaccount.google.com">
-                                myaccount.google.com
-                            </Href>
-                        )}. In the ${(
-                            <strong key={randomHexString4()}>{c('Import instructions emphasis').t`Security`}</strong>
-                        )} section, ${(
-                            <strong key={randomHexString4()}>{c('Import instructions emphasis')
-                                .t`turn on access`}</strong>
-                        )} for less secure apps. If you don't see this option, you might have 2-step verification enabled.`}
+                        {c('Import instructions')
+                            .jt`Go to ${googleAccountLink}. In the ${boldSecurity} section, ${boldAccess} for less secure apps. If you don't see this option, you might have 2-step verification enabled.`}
                     </div>
                     <img
                         className="border-currentColor"
@@ -52,16 +53,8 @@ const ImportInstructionsStep = ({ changeProvider, provider, instructionsCurrentS
             content: (
                 <>
                     <div className="mb1">
-                        {c('Import instructions').jt`Go to ${(
-                            <Href key={randomHexString4()} url="https://myaccount.google.com">
-                                myaccount.google.com
-                            </Href>
-                        )}. In the ${(
-                            <strong key={randomHexString4()}>{c('Import instructions emphasis').t`Security`}</strong>
-                        )} section, create an ${(
-                            <strong key={randomHexString4()}>{c('Import instructions emphasis')
-                                .t`app password`}</strong>
-                        )} if you don't have one already. You will need this password during the import.`}
+                        {c('Import instructions')
+                            .jt`Go to ${googleAccountLink}. In the ${boldSecurity} section, create an ${boldAppPassword} if you don't have one already. You will need this password during the import.`}
                     </div>
                     <img
                         className="border-currentColor"
@@ -76,21 +69,24 @@ const ImportInstructionsStep = ({ changeProvider, provider, instructionsCurrentS
     const handleChangeIndex = (index: number) => setTabIndex(index);
 
     const renderGmail = () => {
+        /* The following consts are to be used in translations */
+        const boldGmailSettings = (
+            <strong key="boldGmailSettings">{c('Import instructions emphasis').t`Gmail Settings`}</strong>
+        );
+        const boldForwarding = (
+            <strong key="boldForwarding">{c('Import instructions emphasis').t`Forwarding and POP/IMAP`}</strong>
+        );
+        const boldIMAPAccess = <strong key="boldIMAPAccess">{c('Import instructions emphasis').t`IMAP access`}</strong>;
+        const boldLabels = <strong key="boldLabels">{c('Import instructions emphasis').t`Labels`}</strong>;
+        const bold2Steps = <strong key="bold2Steps">{c('Import instructions emphasis').t`2-step verification`}</strong>;
+
         switch (instructionsCurrentStep) {
             case 0:
                 return (
                     <>
                         <div className="mb1">
-                            {c('Import instructions').jt`In your ${(
-                                <strong key={randomHexString4()}>{c('Import instructions emphasis')
-                                    .t`Gmail Settings`}</strong>
-                            )}, go to ${(
-                                <strong key={randomHexString4()}>{c('Import instructions emphasis')
-                                    .t`Forwarding and POP/IMAP`}</strong>
-                            )} and make sure that ${(
-                                <strong key={randomHexString4()}>{c('Import instructions emphasis')
-                                    .t`IMAP access`}</strong>
-                            )} is enabled.`}
+                            {c('Import instructions')
+                                .jt`In your ${boldGmailSettings}, go to ${boldForwarding} and make sure that ${boldIMAPAccess} is enabled.`}
                         </div>
                         <img
                             className="border-currentColor"
@@ -104,9 +100,8 @@ const ImportInstructionsStep = ({ changeProvider, provider, instructionsCurrentS
                 return (
                     <>
                         <div className="mb1">
-                            {c('Import instructions').jt`In the ${(
-                                <strong key={randomHexString4()}>{c('Import instructions emphasis').t`Labels`}</strong>
-                            )} section, choose the folders allowed for import into ProtonMail.`}
+                            {c('Import instructions')
+                                .jt`In the ${boldLabels} section, choose the folders allowed for import into ProtonMail.`}
                         </div>
                         <img
                             className="border-currentColor"
@@ -121,10 +116,7 @@ const ImportInstructionsStep = ({ changeProvider, provider, instructionsCurrentS
                     <>
                         <div className="mb1">
                             {c('Import instructions')
-                                .jt`Allow ProtonMail access into your Gmail account: choose whether ${(
-                                <strong key={randomHexString4()}>{c('Import instructions emphasis')
-                                    .t`2-step verification`}</strong>
-                            )} is enabled and follow the steps below.`}
+                                .jt`Allow ProtonMail access into your Gmail account: choose whether ${bold2Steps} is enabled and follow the steps below.`}
                         </div>
                         <Tabs tabs={gmailTabs} value={tabIndex} onChange={handleChangeIndex} />
                     </>
@@ -184,19 +176,22 @@ const ImportInstructionsStep = ({ changeProvider, provider, instructionsCurrentS
     };
 
     const renderYahoo = () => {
+        /* The following consts are to be used in translations */
+        const boldAccountInfo = (
+            <strong key="boldAccountInfo">{c('Import instructions emphasis').t`Account info`}</strong>
+        );
+        const boldAccountSec = (
+            <strong key="boldAccountSec">{c('Import instructions emphasis').t`Account security`}</strong>
+        );
+        const boldManageAppPass = (
+            <strong key="boldManageAppPass">{c('Import instructions emphasis').t`Manage app passwords`}</strong>
+        );
+
         return (
             <>
                 <div className="mb1">
                     {c('Import instructions')
-                        .jt`Log into Yahoo Mail and click on your name in the upper right corner to access ${(
-                        <strong key={randomHexString4()}>{c('Import instructions emphasis').t`Account info`}</strong>
-                    )}. In the ${(
-                        <strong key={randomHexString4()}>{c('Import instructions emphasis')
-                            .t`Account security`}</strong>
-                    )} section, go to  ${(
-                        <strong key={randomHexString4()}>{c('Import instructions emphasis')
-                            .t`Manage app passwords`}</strong>
-                    )} to generate a password. You will need this password during the import.`}
+                        .jt`Log into Yahoo Mail and click on your name in the upper right corner to access ${boldAccountInfo}. In the ${boldAccountSec} section, go to  ${boldManageAppPass} to generate a password. You will need this password during the import.`}
                 </div>
                 <img
                     className="border-currentColor"
