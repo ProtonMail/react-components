5a3307af018a02abb19312a8e11c65b28adcfd67
Richard <richard@protonmail.com>
2020-10-13 12:37:29 +0200

    Prepare new component for pagination in row

---
+++ components/pagination/PaginationRow.tsx
@@ -0,0 +1,60 @@
+import React from 'react';
+import { c } from 'ttag';
+import { range } from 'proton-shared/lib/helpers/array';
+
+import { Group, ButtonGroup } from '../button';
+import { Icon } from '../icon';
+import { classnames } from '../../helpers';
+
+interface Props {
+    onStart: () => void;
+    onEnd: () => void;
+    onPrevious: () => void;
+    onNext: () => void;
+    onPage: (pageNumber: number) => void;
+    page: number;
+    total: number;
+    disabled?: boolean;
+    step?: number;
+}
+
+const PaginationRow = ({ onStart, onEnd, onPrevious, onNext, onPage, page, disabled, total, step = 1 }: Props) => {
+    const pages = range(page - step, page + step).filter((pageNumber) => pageNumber > 0 && pageNumber <= total);
+    const goToPageTitle = c('Action').t`Go to page ${page}`;
+    return (
+        <Group>
+            <ButtonGroup disabled={disabled || page === 1} onClick={onStart} title={c('Action').t`Go to first page`}>
+                <Icon name="" />
+            </ButtonGroup>
+            <ButtonGroup
+                disabled={disabled || page === 1}
+                onClick={onPrevious}
+                title={c('Action').t`Go to previous page`}
+            >
+                <Icon name="" />
+            </ButtonGroup>
+            {pages.map((pageNumber) => {
+                const isActive = pageNumber === page;
+                return (
+                    <ButtonGroup
+                        disabled={disabled || isActive}
+                        className={classnames([isActive && 'is-active'])}
+                        key={pageNumber}
+                        title={goToPageTitle}
+                        onClick={() => onPage(pageNumber)}
+                    >
+                        {pageNumber}
+                    </ButtonGroup>
+                );
+            })}
+            <ButtonGroup disabled={disabled || page === total} onClick={onNext} title={c('Action').t`Go to next page`}>
+                <Icon name="" />
+            </ButtonGroup>
+            <ButtonGroup disabled={disabled || page === total} onClick={onEnd} title={c('Action').t`Go to last page`}>
+                <Icon name="" />
+            </ButtonGroup>
+        </Group>
+    );
+};
+
+export default PaginationRow;
