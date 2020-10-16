71ac65976277bd44ab94e2324358682c371d2269
Richard <richard@protonmail.com>
2020-10-16 15:05:26 +0200

    Review

--- hooks/useCookieState.ts
+++ hooks/useCookieState.ts
@@ -1,7 +1,12 @@
-import { useState, useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
 import { getCookie, setCookie } from 'proton-shared/lib/helpers/cookies';
 
-const useCookieState = (cookieValue: string, cookieName: string, expirationDate?: string, cookieDomain?: string) => {
+const useCookieState = (
+    cookieValue: string,
+    cookieName: string,
+    expirationDate?: string,
+    cookieDomain?: string
+): [string, React.Dispatch<React.SetStateAction<string>>] => {
     const [value, setValue] = useState(() => {
         const cookie = getCookie(cookieName, cookieValue);
         if (!cookie) {
