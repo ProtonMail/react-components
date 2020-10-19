79a7f2bd60d876d4f06a49eac225e2803dc996af
Richard <richard@protonmail.com>
2020-10-19 20:35:35 +0200

    Set cookie for all proton domains

--- hooks/usePaidCookie.ts
+++ hooks/usePaidCookie.ts
@@ -6,11 +6,14 @@ import { useUser } from './useUser';
 const COOKIE_NAME = 'is-paid-user';
 
 const usePaidCookie = () => {
+    const { hostname } = window.location;
+    const secondLevelDomain = hostname.substr(hostname.indexOf('.') + 1);
+    const cookieDomain = `.${secondLevelDomain}`;
     const [{ isPaid }] = useUser();
 
     useEffect(() => {
         if (isPaid && !checkCookie(COOKIE_NAME, 'true')) {
-            setCookie(COOKIE_NAME, 'true');
+            setCookie(COOKIE_NAME, 'true', undefined, cookieDomain);
         }
     }, []);
 };
