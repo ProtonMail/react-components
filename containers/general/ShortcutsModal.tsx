c2c9e32d79111ce20849c7658752e340deae9b51
Jeremy Benaim <jeremybenaim@gmail.com>
2020-11-19 14:35:45 +0100

    New shortcuts modal

--- containers/general/ShortcutsModal.tsx
+++ containers/general/ShortcutsModal.tsx
@@ -1,12 +1,12 @@
 import React from 'react';
 import { c } from 'ttag';
-import { APP_NAMES, APPS_CONFIGURATION } from 'proton-shared/lib/constants';
+import { APPS, APP_NAMES, APPS_CONFIGURATION } from 'proton-shared/lib/constants';
 import { noop } from 'proton-shared/lib/helpers/function';
 import { getShortcutsForApp } from 'proton-shared/lib/shortcuts/constants';
 
 import { classnames } from '../../helpers';
-import { FormModal } from '../../components';
-import { useActiveBreakpoint } from '../../hooks';
+import { Alert, FormModal, AppLink } from '../../components';
+import { useMailSettings } from '../../hooks';
 
 interface Shortcut {
     name: string;
@@ -16,6 +16,7 @@ interface Shortcut {
 interface ShortcutSection {
     name: string;
     shortcuts: Shortcut[];
+    alwaysActive?: boolean;
 }
 
 interface Props {
@@ -24,11 +25,15 @@ interface Props {
 }
 
 const ShortcutsModal = ({ app, onClose = noop, ...rest }: Props) => {
-    const { isNarrow } = useActiveBreakpoint();
-    const shortcuts = getShortcutsForApp(app);
+    const [{ Hotkeys } = { Hotkeys: 0 }] = useMailSettings();
+
+    const shortcutSections: ShortcutSection[] = getShortcutsForApp(app);
+
+    const alwaysOnSections = shortcutSections.filter((section) => section.alwaysActive);
+    const shortcutEnabledSections = shortcutSections.filter((section) => !section.alwaysActive);
 
     const sectionRenderer = ({ name, shortcuts }: ShortcutSection) => (
-        <div key={name} className="pr2 onmobile-pr0">
+        <div key={name} className="pr2 onmobile-pr0 mb2">
             <h2 className="h5 mb0-5">{name}</h2>
             {shortcuts.length > 0 && (
                 <ul className="unstyled mt1 onmobile-pr0">
@@ -53,6 +58,12 @@ const ShortcutsModal = ({ app, onClose = noop, ...rest }: Props) => {
         </div>
     );
 
+    const settingsLink = (
+        <AppLink to="/general#shortcuts" toApp={APPS.PROTONMAIL_SETTINGS} key="settings-link">
+            {c('Link').t`general settings.`}
+        </AppLink>
+    );
+
     return (
         <FormModal
             title={c('Title').t`${APPS_CONFIGURATION[app].name} Shortcuts`}
@@ -61,12 +72,26 @@ const ShortcutsModal = ({ app, onClose = noop, ...rest }: Props) => {
             onClose={onClose}
             {...rest}
         >
+            <Alert className="mb1">{c('Info')
+                .t`Basic navigation and actions remain available regardless of shortcuts being active or not in the settings.`}</Alert>
             <div
-                className="list-2columns onmobile-list-1column"
+                className="list-2columns onmobile-list-1column border-bottom mb1"
                 // to compensate the pr2 of the sections in the right column
-                style={isNarrow ? { marginRight: '-2em' } : undefined}
+                style={{ marginRight: '-2em' }}
             >
-                {shortcuts.map(sectionRenderer)}
+                {alwaysOnSections.map(sectionRenderer)}
+            </div>
+            <Alert className="mb1">
+                {Hotkeys
+                    ? c('Info').t`Your keyboard shortcuts are active`
+                    : c('Info').jt`To activate your keyboard shortcuts, go to ${settingsLink}`}
+            </Alert>
+            <div
+                className={classnames(['list-2columns onmobile-list-1column', !Hotkeys && 'opacity-50'])}
+                // to compensate the pr2 of the sections in the right column
+                style={{ marginRight: '-2em' }}
+            >
+                {shortcutEnabledSections.map(sectionRenderer)}
             </div>
         </FormModal>
     );
