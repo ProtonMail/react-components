916486862116be16d767c6673544dc8e37c73822
Jeremy Benaim <jeremybenaim@gmail.com>
2020-12-24 14:05:33 +0100

    [MAILWEB-1568] - Add welcome modal to Import/Export settings page

---
+++ containers/import/modals/ImportWelcomeModal.tsx
@@ -0,0 +1,60 @@
+import React from 'react';
+import { c } from 'ttag';
+
+import { noop } from 'proton-shared/lib/helpers/function';
+
+// import importStartedSvg from 'design-system/assets/img/shared/import-started.svg';
+
+import { FormModal, Href, PrimaryButton } from '../../../components';
+
+interface Props {
+    onClose?: () => void;
+}
+
+const ImportWelcomeModal = ({ onClose = noop, ...rest }: Props) => {
+    const gmailLink = (
+        <Href
+            url="https://protonmail.com/support/knowledge-base/transitioning-from-gmail-to-protonmail/"
+            key="gmailLink"
+        >
+            Gmail
+        </Href>
+    );
+    const yahooLink = (
+        <Href url="https://protonmail.com/support/knowledge-base/migrate-from-yahoo-to-protonmail/" key="yahooLink">
+            Yahoo
+        </Href>
+    );
+    const outlookLink = (
+        <Href url="https://protonmail.com/support/knowledge-base/migrate-from-outlook-to-protonmail/" key="outlookLink">
+            Outlook
+        </Href>
+    );
+
+    return (
+        <FormModal
+            title={
+                <h1 className="pm-modalTitle aligncenter">{c('Title').t`Your smooth transition to digital privacy`}</h1>
+            }
+            submit={
+                <PrimaryButton type="submit" data-focus-fallback={1}>{c('Action')
+                    .t`Try Import Assistant`}</PrimaryButton>
+            }
+            close={null}
+            onSubmit={onClose}
+            onClose={onClose}
+            {...rest}
+        >
+            <div className="aligncenter">
+                <div className="mt1">{c('Info')
+                    .t`Shelter your data behind Proton encryption with our email transfer feature Import Assistant.`}</div>
+                {/* @todo change image */}
+                {/* <img src={importStartedSvg} alt="" className="mb1" /> */}
+                <div className="mb1">{c('Info')
+                    .jt`Our guides for ${gmailLink}, ${yahooLink}, and ${outlookLink} show you the next steps as you switch to Proton.`}</div>
+            </div>
+        </FormModal>
+    );
+};
+
+export default ImportWelcomeModal;
