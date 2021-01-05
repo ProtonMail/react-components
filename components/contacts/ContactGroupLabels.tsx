02a497e2729a816225691da06132b204bb4248b8
Matthieu Lux <matthieu.lux@protonmail.com>
2021-01-05 16:21:40 +0100

    Review

--- components/contacts/ContactGroupLabels.tsx
+++ components/contacts/ContactGroupLabels.tsx
@@ -1,4 +1,5 @@
 import React, { MouseEvent } from 'react';
+import { APPS } from 'proton-shared/lib/constants';
 import { ContactGroup } from 'proton-shared/lib/interfaces/contacts/Contact';
 import LabelStack, { LabelDescription } from '../labelStack/LabelStack';
 import { useAppLink } from '../link';
@@ -19,7 +20,7 @@ const ContactGroupLabels = ({ contactGroups }: Props) => {
                       color: contactGroup.Color,
                       title: contactGroup.Name,
                       onClick: (event: MouseEvent) => {
-                          appLink(`/?contactGroupID=${contactGroup.ID}`, 'proton-contacts');
+                          appLink(`/?contactGroupID=${contactGroup.ID}`, APPS.PROTONCONTACTS);
                           event.stopPropagation();
                       },
                   },
