7e0a633773d587183d93bc56efab8b7ebdcb9666
Matthieu Bué <matthieu.bue@gmail.com>
2021-01-14 20:15:11 +0100

    circle-chart__* → circle-chart-*

--- containers/vpn/OpenVPNConfigurationSection/LoadIndicator.js
+++ containers/vpn/OpenVPNConfigurationSection/LoadIndicator.js
@@ -13,9 +13,9 @@ const LoadIndicator = ({ server: { Load = 0 } }) => {
             <Tooltip title={c('Info').t`Server load`}>
                 <div className="flex inline-flex-vcenter">
                     <CircularProgress progress={Load} size={22} className={className}>
-                        <g className="circle-chart__info">
-                            <rect x="17" y="14" width="1.55" height="9.1" className="circle-chart__percent" />
-                            <rect x="17" y="11" width="1.55" height="1.53" className="circle-chart__percent" />
+                        <g className="circle-chart-info">
+                            <rect x="17" y="14" width="1.55" height="9.1" className="circle-chart-percent" />
+                            <rect x="17" y="11" width="1.55" height="1.53" className="circle-chart-percent" />
                         </g>
                     </CircularProgress>
                     <div className="ml0-5">{Load}%</div>
