876f5d1117090b2a3f18345cefc54bd8fff3071c
mmso <mmso@mmso.se>
2021-01-14 12:11:25 +0100

    Convert shortcuts toggle to ts

--- containers/general/ShortcutsToggle.tsx
+++ containers/general/ShortcutsToggle.tsx
@@ -1,16 +1,22 @@
-import React from 'react';
-import PropTypes from 'prop-types';
+import React, { ChangeEvent } from 'react';
 import { c } from 'ttag';
 import { updateHotkeys } from 'proton-shared/lib/api/mailSettings';
 import { useToggle, useEventManager, useApiWithoutResult, useNotifications } from '../../hooks';
 import { Toggle } from '../../components';
 
-const ShortcutsToggle = ({ id, hotkeys, onChange, className }) => {
+interface Props {
+    id: string;
+    hotkeys: number;
+    className?: string;
+    onChange: (value: number) => void;
+}
+
+const ShortcutsToggle = ({ id, hotkeys, onChange, className }: Props) => {
     const { call } = useEventManager();
     const { createNotification } = useNotifications();
     const { request, loading } = useApiWithoutResult(updateHotkeys);
     const { state, toggle } = useToggle(!!hotkeys);
-    const handleChange = async ({ target }) => {
+    const handleChange = async ({ target }: ChangeEvent<HTMLInputElement>) => {
         await request(+target.checked);
         call();
         toggle();
@@ -18,13 +24,6 @@ const ShortcutsToggle = ({ id, hotkeys, onChange, className }) => {
         createNotification({ text: c('Success').t`Keyboard shortcuts preferences updated` });
     };
     return <Toggle id={id} className={className} checked={state} onChange={handleChange} loading={loading} />;
-};
-
-ShortcutsToggle.propTypes = {
-    id: PropTypes.string,
-    hotkeys: PropTypes.number.isRequired,
-    onChange: PropTypes.func.isRequired,
-    className: PropTypes.string,
 };
 
 export default ShortcutsToggle;
