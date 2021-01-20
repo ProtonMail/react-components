4ae39d215addd1be8364a4312bdfe0b6cd1d13c1
Richard <richard@protonmail.com>
2021-01-20 14:34:49 +0100

    Review

--- hooks/useContactEmailsSortedByName.ts
+++ hooks/useContactEmailsSortedByName.ts
@@ -1,22 +1,17 @@
+import { useMemo } from 'react';
 import { ContactEmail } from 'proton-shared/lib/interfaces/contacts/Contact';
 
 import useContactEmails from './useContactEmails';
 
 const compareContactEmailByName = (a: ContactEmail, b: ContactEmail) => {
-    if (a.Name > b.Name) {
-        return 1;
-    }
-
-    if (a.Name < b.Name) {
-        return -1;
-    }
-
-    return 0;
+    return a.Name.localeCompare(b.Name);
 };
 
 const useContactEmailsSortedByName = () => {
     const [contactEmails = [], loading] = useContactEmails();
-    return [[...contactEmails].sort(compareContactEmailByName), loading] as const;
+    return useMemo(() => {
+        return [[...(contactEmails || [])].sort(compareContactEmailByName), loading] as const;
+    }, [contactEmails]);
 };
 
 export default useContactEmailsSortedByName;
