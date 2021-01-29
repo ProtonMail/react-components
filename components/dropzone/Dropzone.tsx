a23d3b2b40177477295fcb4ff05c336d11eac919
Sergo Vainumäe <sergo.vainumae@protonmail.ch>
2021-01-29 13:21:18 +0100

    Animated hover, added content prop, spread rest
    CALWEB-1837

--- components/dropzone/Dropzone.tsx
+++ components/dropzone/Dropzone.tsx
@@ -1,28 +1,50 @@
-import React, { DragEvent } from 'react';
+import React from 'react';
 import { c } from 'ttag';
+import clsx from 'clsx';
 
 import './Dropzone.scss';
 
 interface Props extends React.HTMLAttributes<HTMLDivElement> {
-   isHovered: boolean;
+    /**
+     * When true, reveals the overlay and content
+     */
+    isHovered: boolean;
+    /**
+     * The content to show when dragging over the dropzone
+     */
+    content: React.ReactNode;
 }
 
-const Dropzone = ({ children, isHovered, onDrop, onDragEnter, onDragLeave, className }: Props) => {
+const Dropzone = ({
+    children,
+    isHovered,
+    onDrop,
+    onDragEnter,
+    onDragLeave,
+    className,
+    content = c('Info').t`Drop the file here to upload`,
+    ...rest
+}: Props) => {
     return (
         <div
             className={className}
             onDrop={onDrop}
             onDragEnter={onDragEnter}
             onDragOver={(event) => event.preventDefault()}
+            {...rest}
         >
-            {isHovered && (
-                <div
-                    className="dropzone covered-absolute flex flex-justify-center flex-items-center"
-                    onDragLeave={onDragLeave}
-                >
-                    <span className="dropzone-text no-pointer-events">{c('Info').t`Drop the file here to upload`}</span>
-                </div>
-            )}
+            <div
+                className={clsx([
+                    'dropzone covered-absolute flex flex-justify-center flex-items-center',
+                    {
+                        'is-hovered': isHovered,
+                    },
+                ])}
+                onDragLeave={onDragLeave}
+            >
+                <span className="dropzone-text no-pointer-events">{content}</span>
+            </div>
+
             <div className="dropzone-content flex flex-items-center flex-justify-center w100">{children}</div>
         </div>
     );
