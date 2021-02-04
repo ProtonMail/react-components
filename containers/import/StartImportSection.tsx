c6073db2fd07ddcc9d1e520643876d0f7d84cc83
Jeremy Benaim <jeremybenaim@gmail.com>
2021-02-04 10:37:44 +0100

    Error handling + quick rewriting using URLSearchParams + remove REDIRECT_URL constant

--- containers/import/StartImportSection.tsx
+++ containers/import/StartImportSection.tsx
@@ -3,16 +3,23 @@ import { c } from 'ttag';
 
 import { generateProtonWebUID } from 'proton-shared/lib/helpers/uid';
 
-import { useModals } from '../../hooks';
+import { useModals, useUser, useNotifications } from '../../hooks';
 import { PrimaryButton, Alert, Icon } from '../../components';
 
 import { OAUTH_PROVIDER } from './interfaces';
-import { GOOGLE_OAUTH_CLIENT_ID, GOOGLE_OAUTH_REDIRECT_URL, GOOGLE_OAUTH_SCOPE } from './constants';
+import { GOOGLE_OAUTH_CLIENT_ID, GOOGLE_OAUTH_SCOPE } from './constants';
 import ImportMailModal from './modals/ImportMailModal';
+
+const getCleanURL = (url?: URL) => {
+    const { protocol, host, pathname } = url || window.location;
+
+    return `${protocol}//${host}${pathname}`;
+};
 
 const getAuthorizationUrl = () => {
     const params = new URLSearchParams();
-    params.append('redirect_uri', GOOGLE_OAUTH_REDIRECT_URL);
+
+    params.append('redirect_uri', getCleanURL());
     params.append('response_type', 'code');
     params.append('access_type', 'offline');
     params.append('client_id', GOOGLE_OAUTH_CLIENT_ID);
@@ -25,8 +32,14 @@ const getAuthorizationUrl = () => {
 const WINDOW_WIDTH = 500;
 const WINDOW_HEIGHT = 600;
 
+/* @todo replace me */
+const TEST_ID = '8QpIN_zJxUXVQThOueLJQao-ZjetYRUiEpchaN-HOfu41o6eBz9wIV8FZWE7dlXTSmMcyKei9smisZ6mG5qDMg==';
+
 const StartImportSection = () => {
+    const [user] = useUser();
     const { createModal } = useModals();
+    const { createNotification } = useNotifications();
+
     const stateId = useRef<string>();
 
     const handleClick = () => createModal(<ImportMailModal />);
@@ -55,39 +68,54 @@ const StartImportSection = () => {
                 We can then move the following logic to a `onmessage` listener
             */
             interval = window.setInterval(() => {
-                try {
-                    const url = authWindow.document.URL;
+                const redirectURI = getCleanURL();
 
-                    if (url.includes(GOOGLE_OAUTH_REDIRECT_URL)) {
+                try {
+                    const url = new URL(authWindow.document.URL);
+                    const params = new URLSearchParams(url.search);
+
+                    if (getCleanURL(url) === redirectURI) {
                         authWindow.close();
                         window.clearInterval(interval);
 
-                        const query = decodeURIComponent(url.split('?').length > 0 ? url.split('?')[1] : '/');
+                        const error = params.get('error');
 
-                        if (query.includes('error')) {
-                            const error = query.split('error=')[1].split('&')[0];
-                            window.alert(error);
+                        if (error) {
+                            createNotification({
+                                text: (
+                                    <>
+                                        {c('Error').t`Authentication cancelled.`}
+                                        <br />
+                                        {c('Error').t`Your import will not be processed.`}
+                                    </>
+                                ),
+                            });
                             return;
                         }
 
-                        const state = query.split('state=')[1].split('&')[0];
+                        const state = params.get('state');
 
+                        // State passthrough mismatch error
                         if (state !== stateId.current) {
-                            window.alert('state passthru mismatch');
+                            createNotification({
+                                text: (
+                                    <>
+                                        {c('Error').t`Authentication error.`}
+                                        <br />
+                                        {c('Error').t`Your import will not be processed.`}
+                                    </>
+                                ),
+                            });
                             return;
                         }
 
-                        const code = query.split('code=')[1].split('&')[0];
+                        const code = params.get('code');
 
-                        createModal(
-                            <ImportMailModal
-                                oauthProps={{
-                                    code,
-                                    provider,
-                                    redirectURI: GOOGLE_OAUTH_REDIRECT_URL,
-                                }}
-                            />
-                        );
+                        if (!code) {
+                            return;
+                        }
+
+                        createModal(<ImportMailModal oauthProps={{ code, provider, redirectURI }} />);
                     }
                 } catch (err) {
                     // silent error
@@ -103,21 +131,21 @@ const StartImportSection = () => {
                     .t`Transfer your data safely to Proton. Import Assistant connects to your external email provider and imports your selected messages and folders.`}
             </Alert>
 
-            <div className="flex flex-align-items">
-                <PrimaryButton
-                    className="inline-flex flex-items-center flex-justify-center mt0-5 mr1"
-                    onClick={() => handleOauthClick(OAUTH_PROVIDER.GMAIL)}
-                >
-                    <Icon name="gmail" className="mr0-5" />
-                    {c('Action').t`Continue with Google`}
-                </PrimaryButton>
-                <PrimaryButton
-                    className="inline-flex flex-items-center flex-justify-center mt0-5"
-                    onClick={handleClick}
-                >
-                    <Icon name="imap-smtp" className="mr0-5" />
-                    {c('Action').t`Continue with IMAP`}
-                </PrimaryButton>
+            <div className="flex flex-flex-align-items-center">
+                {user.ID === TEST_ID ? (
+                    <PrimaryButton
+                        className="inline-flex flex-justify-center mt0-5 mr1"
+                        onClick={() => handleOauthClick(OAUTH_PROVIDER.GMAIL)}
+                    >
+                        <Icon name="gmail" className="mr0-5" />
+                        {c('Action').t`Continue with Google`}
+                    </PrimaryButton>
+                ) : (
+                    <PrimaryButton className="inline-flex flex-justify-center mt0-5" onClick={handleClick}>
+                        <Icon name="imap-smtp" className="mr0-5" />
+                        {c('Action').t`Continue with IMAP`}
+                    </PrimaryButton>
+                )}
             </div>
         </>
     );
