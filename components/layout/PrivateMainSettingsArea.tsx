430515aea092613559b23a30f68e4d459e366523
Matthias Margot <margotmatthias@protonmail.com>
2021-02-18 16:45:26 +0100

    Rework on Password & Recovery (wip)

--- components/layout/PrivateMainSettingsArea.tsx
+++ components/layout/PrivateMainSettingsArea.tsx
@@ -9,17 +9,14 @@ import { SubSectionConfig, SettingsPropsShared } from './interface';
 import useActiveSection from './useActiveSection';
 
 import createScrollIntoView from '../../helpers/createScrollIntoView';
-import { classnames } from '../../helpers';
-import { SettingsParagraph } from '../../containers';
 
 interface Props extends SettingsPropsShared {
     title: string;
     children: React.ReactNode;
     subsections: SubSectionConfig[];
-    description?: string;
 }
 
-const PrivateMainSettingsArea = ({ setActiveSection, location, title, children, description, subsections }: Props) => {
+const PrivateMainSettingsArea = ({ setActiveSection, location, title, children, subsections }: Props) => {
     const mainAreaRef = useRef<HTMLDivElement>(null);
     const useIntersectionSection = useRef(false);
 
@@ -115,10 +112,7 @@ const PrivateMainSettingsArea = ({ setActiveSection, location, title, children, 
     return (
         <PrivateMainArea ref={mainAreaRef}>
             <div className="container-section-sticky">
-                <SettingsPageTitle className={classnames(['mt1-5', !description && 'mb1-5'])}>
-                    {title}
-                </SettingsPageTitle>
-                {description && <SettingsParagraph className="mb1-5">{description}</SettingsParagraph>}
+                <SettingsPageTitle className="mt1-5 mb1-5">{title}</SettingsPageTitle>
                 <ErrorBoundary>{wrappedSections}</ErrorBoundary>
             </div>
         </PrivateMainArea>
