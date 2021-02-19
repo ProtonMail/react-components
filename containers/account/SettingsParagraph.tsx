f07ab87f1843a84fba687b31e84b4ee31eba34eb
Matthias Margot <margotmatthias@protonmail.com>
2021-02-19 12:00:48 +0100

    Enable SettingsParagraph to display a learn-more link

--- containers/account/SettingsParagraph.tsx
+++ containers/account/SettingsParagraph.tsx
@@ -1,7 +1,25 @@
 import React from 'react';
+import { LearnMore, LearnMoreProps } from '../../components';
 
-const SettingsParagraph = (props: any) => {
-    return <p style={{ maxWidth: 600 }} {...props} />;
+interface SettingsParagraphProps extends React.ComponentPropsWithoutRef<'p'> {
+    learnMoreUrl?: string;
+    learnMoreProps?: Omit<LearnMoreProps, 'url'>;
+}
+
+const SettingsParagraph = ({ learnMoreUrl, learnMoreProps, children, ...rest }: SettingsParagraphProps) => {
+    const learnMoreElement = learnMoreUrl ? (
+        <>
+            <br />
+            <LearnMore url={learnMoreUrl} {...learnMoreProps} />
+        </>
+    ) : null;
+
+    return (
+        <p style={{ maxWidth: 600 }} {...rest}>
+            {children}
+            {learnMoreElement}
+        </p>
+    );
 };
 
 export default SettingsParagraph;
