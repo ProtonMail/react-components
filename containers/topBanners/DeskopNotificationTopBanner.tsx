31620c5e340c5004ae968e3f5505efa4273184b3
Richard <richard@protonmail.com>
2021-02-22 10:34:39 +0100

    Review

--- containers/topBanners/DeskopNotificationTopBanner.tsx
+++ containers/topBanners/DeskopNotificationTopBanner.tsx
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 import { c } from 'ttag';
 import { getStatus, request, Status } from 'proton-shared/lib/helpers/desktopNotification';
 import { APP_NAMES, APPS } from 'proton-shared/lib/constants';
+import { getAppName } from 'proton-shared/lib/apps/helper';
 
 import TopBanner from './TopBanner';
 import { useLocalState, useConfig } from '../../hooks';
@@ -10,6 +11,7 @@ const DeskopNotificationTopBanner = () => {
     const [status, setStatus] = useState<Status>(getStatus());
     const [dontAsk, setDontAsk] = useLocalState(false, 'dont-ask-desktop-notification');
     const { APP_NAME } = useConfig();
+    const appName = getAppName(APP_NAME);
 
     if (!([APPS.PROTONMAIL, APPS.PROTONCALENDAR] as APP_NAMES[]).includes(APP_NAME)) {
         return null;
@@ -41,7 +43,7 @@ const DeskopNotificationTopBanner = () => {
 
     return (
         <TopBanner onClose={() => setDontAsk(true)} className="bg-pm-blue">{c('Info')
-            .jt`Proton needs your permission to ${enableDesktopNotifications}.`}</TopBanner>
+            .jt`${appName} needs your permission to ${enableDesktopNotifications}.`}</TopBanner>
     );
 };
 
