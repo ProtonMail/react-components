27d99ac00f32fc88a2f7ca40348eccbdeda05629
Richard <richard@protonmail.com>
2021-02-22 20:40:29 +0100

    Review

--- containers/labels/FolderIcon.tsx
+++ containers/labels/FolderIcon.tsx
@@ -3,7 +3,7 @@ import { Folder } from 'proton-shared/lib/interfaces/Folder';
 import { ROOT_FOLDER } from 'proton-shared/lib/constants';
 
 import { Icon } from '../../components';
-import { useMailSettings } from '../../hooks';
+import { useMailSettings, useFolders } from '../../hooks';
 
 interface Props {
     folder: Folder;
@@ -19,9 +19,24 @@ const getIconName = (isRoot?: boolean, color?: string) => {
 
 const FolderIcon = ({ folder, className }: Props) => {
     const [mailSettings] = useMailSettings();
+    const [folders] = useFolders();
     const isRoot = folder.ParentID === ROOT_FOLDER;
-    const folderColor = isRoot ? folder.Color : undefined;
-    const color = mailSettings?.EnableFolderColor ? folderColor : undefined;
+
+    const getColor = ({ ParentID, Color }: Folder): string => {
+        if (!mailSettings?.InheritParentFolderColor) {
+            return Color;
+        }
+        if (ParentID === ROOT_FOLDER) {
+            return Color;
+        }
+        const folder = folders?.find(({ ID }) => ID === ParentID);
+        if (folder) {
+            return getColor(folder);
+        }
+        return '';
+    };
+
+    const color = mailSettings?.EnableFolderColor ? getColor(folder) : undefined;
 
     return <Icon name={getIconName(isRoot, color)} color={color} className={className} alt={folder.Name} />;
 };
