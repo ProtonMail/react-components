2ddfbb468a687976768f4d268d0328f4806d7fab
Jeremy Benaim <jeremybenaim@gmail.com>
2021-02-22 15:22:46 +0100

    Identity section

--- containers/addresses/AddressesWithUser.tsx
+++ containers/addresses/AddressesWithUser.tsx
@@ -4,8 +4,11 @@ import { move } from 'proton-shared/lib/helpers/array';
 import { orderAddress } from 'proton-shared/lib/api/addresses';
 import { Address, CachedOrganizationKey, Member, UserModel } from 'proton-shared/lib/interfaces';
 import { ADDRESS_TYPE } from 'proton-shared/lib/constants';
+
 import { Alert, OrderableTable, OrderableTableHeader, OrderableTableBody, OrderableTableRow } from '../../components';
 import { useApi, useEventManager, useAddresses, useNotifications } from '../../hooks';
+
+import SettingsSectionWide from '../account/SettingsSectionWide';
 
 import AddressStatus from './AddressStatus';
 import AddressActions from './AddressActions';
@@ -52,7 +55,7 @@ const AddressesUser = ({ user, member, organizationKey }: Props) => {
 
                 setAddresses(newList);
                 await api(orderAddress(newList.map(({ ID }) => ID)));
-                call();
+                await call();
             } catch (e) {
                 setAddresses(formatAddresses(addresses));
             }
@@ -61,45 +64,43 @@ const AddressesUser = ({ user, member, organizationKey }: Props) => {
     );
 
     if (!loadingAddresses && !addresses.length) {
-        return (
-            <>
-                <Alert>{c('Info').t`No addresses exist`}</Alert>
-            </>
-        );
+        return <Alert>{c('Info').t`No addresses exist`}</Alert>;
     }
 
     return (
-        <OrderableTable onSortEnd={handleSortEnd} className="simple-table--has-actions">
-            <OrderableTableHeader
-                cells={[
-                    c('Header for addresses table').t`Address`,
-                    c('Header for addresses table').t`Status`,
-                    c('Header for addresses table').t`Actions`,
-                ]}
-            />
-            <OrderableTableBody colSpan={3} loading={loadingAddresses}>
-                {list &&
-                    list.map((address, i) => (
-                        <OrderableTableRow
-                            index={i}
-                            key={address.ID}
-                            cells={[
-                                <div key={0} className="text-ellipsis" title={address.Email}>
-                                    {address.Email}
-                                </div>,
-                                <AddressStatus key={1} {...getStatus(address, i)} />,
-                                <AddressActions
-                                    key={2}
-                                    address={address}
-                                    user={user}
-                                    member={member}
-                                    organizationKey={organizationKey}
-                                />,
-                            ]}
-                        />
-                    ))}
-            </OrderableTableBody>
-        </OrderableTable>
+        <SettingsSectionWide>
+            <OrderableTable onSortEnd={handleSortEnd} className="simple-table--has-actions">
+                <OrderableTableHeader
+                    cells={[
+                        c('Header for addresses table').t`Address`,
+                        c('Header for addresses table').t`Status`,
+                        c('Header for addresses table').t`Actions`,
+                    ]}
+                />
+                <OrderableTableBody colSpan={3} loading={loadingAddresses}>
+                    {list &&
+                        list.map((address, i) => (
+                            <OrderableTableRow
+                                index={i}
+                                key={address.ID}
+                                cells={[
+                                    <div key={0} className="text-ellipsis" title={address.Email}>
+                                        {address.Email}
+                                    </div>,
+                                    <AddressStatus key={1} {...getStatus(address, i)} />,
+                                    <AddressActions
+                                        key={2}
+                                        address={address}
+                                        user={user}
+                                        member={member}
+                                        organizationKey={organizationKey}
+                                    />,
+                                ]}
+                            />
+                        ))}
+                </OrderableTableBody>
+            </OrderableTable>
+        </SettingsSectionWide>
     );
 };
 
