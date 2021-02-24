2b5419b16757820dfa0dfdd815de01d19febc469
Nicolas Hoffmann <dante3333@gmail.com>
2021-02-24 17:10:07 +0100

    [DRVWEB-955] feat - shorten long filename with enhanced ellipsis

---
+++ components/fileNameDisplay/FileNameDisplay.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import { splitExtension } from 'proton-shared/lib/helpers/file';
+import { MiddleEllipsis } from '../ellipsis';
+
+const CHARACTERS_BEFORE_EXTENSION = 3;
+
+interface Props {
+    text: string;
+}
+
+const FileNameDisplay = ({ text }: Props) => {
+    const fileDetails = splitExtension(text);
+    const extensionOffset = fileDetails[1].length + CHARACTERS_BEFORE_EXTENSION;
+
+    return <MiddleEllipsis charsToDisplayEnd={extensionOffset} className="center" text={text} />;
+};
+
+export default FileNameDisplay;
