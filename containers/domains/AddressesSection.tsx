db3e962722904886f10625250df61859dca3cf8a
Jeremy Benaim <jeremybenaim@gmail.com>
2021-02-25 09:07:55 +0100

    Filters page + remove Blocks + a couple of new Buttons

--- containers/domains/AddressesSection.tsx
+++ containers/domains/AddressesSection.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { c } from 'ttag';
 import { APPS } from 'proton-shared/lib/constants';
 
-import { Alert, Block, AppLink } from '../../components';
+import { Alert, AppLink } from '../../components';
 import { useOrganization } from '../../hooks';
 
 interface Props {
@@ -17,36 +17,36 @@ const AddressesSection = ({ onClose }: Props) => {
             <>
                 <Alert>{c('Info for domain modal')
                     .t`Add a new user to your organization and create an address for it.`}</Alert>
-                <Block>
+                <div className="mb1">
                     <AppLink
                         className="button button--primary"
                         onClick={() => onClose?.()}
                         to="/organization#members"
                         toApp={APPS.PROTONACCOUNT}
                     >{c('Action').t`Add user`}</AppLink>
-                </Block>
+                </div>
                 <Alert>{c('Info for domain modal').t`Add a new address for any user of your organization.`}</Alert>
-                <Block>
+                <div className="mb1">
                     <AppLink
                         className="button button--primary"
                         onClick={() => onClose?.()}
                         to="/organization#addresses"
                         toApp={APPS.PROTONACCOUNT}
                     >{c('Action').t`Add address`}</AppLink>
-                </Block>
+                </div>
             </>
         );
     }
 
     return (
-        <Block>
+        <div className="mb1">
             <AppLink
                 className="button button--primary"
                 onClick={() => onClose?.()}
                 to="/organization#addresses"
                 toApp={APPS.PROTONACCOUNT}
             >{c('Action').t`Add address`}</AppLink>
-        </Block>
+        </div>
     );
 };
 
