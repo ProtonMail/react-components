0377eae818afc44bdfe51aa28bc52939a5bcc1a5
Jeremy Benaim <jeremybenaim@gmail.com>
2021-03-02 14:03:23 +0100

    Use new Button API

--- containers/contacts/settings/ContactGroupsSection.tsx
+++ containers/contacts/settings/ContactGroupsSection.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { c } from 'ttag';
 
-import { PrimaryButton } from '../../../components';
+import { Button } from '../../../components';
 import { useModals } from '../../../hooks';
 
 import ContactGroupModal from '../modals/ContactGroupModal';
@@ -19,7 +19,7 @@ const ContactGroupsSection = () => {
                     .t`A group can contain multiple email addresses from the same contact. Please note that a sending limit may apply and prevent you from sending emails to excessively large groups.`}
             </SettingsParagraph>
             <div className="mb1">
-                <PrimaryButton onClick={handleCreate}>{c('Action').t`Add group`}</PrimaryButton>
+                <Button color="norm" onClick={handleCreate}>{c('Action').t`Add group`}</Button>
             </div>
             <ContactGroupsTable />
         </SettingsSection>
