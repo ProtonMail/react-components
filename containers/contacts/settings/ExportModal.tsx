f8a5f95f5b535af8822318a73ab982a7021e8d68
Jeremy Benaim <jeremybenaim@gmail.com>
2021-03-09 09:03:38 +0100

    Report contact widget changes to moved file

--- containers/contacts/settings/ExportModal.tsx
+++ containers/contacts/settings/ExportModal.tsx
@@ -2,16 +2,11 @@ import React, { useState, useEffect } from 'react';
 import { c } from 'ttag';
 import { format } from 'date-fns';
 
-import { queryContactExport } from 'proton-shared/lib/api/contacts';
 import downloadFile from 'proton-shared/lib/helpers/downloadFile';
-import { wait } from 'proton-shared/lib/helpers/promise';
 import { noop } from 'proton-shared/lib/helpers/function';
-import { splitKeys } from 'proton-shared/lib/keys/keys';
-import { prepareContact } from 'proton-shared/lib/contacts/decrypt';
-import { toICAL } from 'proton-shared/lib/contacts/vcard';
 import { DecryptedKey } from 'proton-shared/lib/interfaces';
 import { Contact } from 'proton-shared/lib/interfaces/contacts';
-import { QUERY_EXPORT_MAX_PAGESIZE, API_SAFE_INTERVAL } from 'proton-shared/lib/contacts/constants';
+import { exportContactsFromLabel } from 'proton-shared/lib/contacts/export';
 
 import { FormModal, ResetButton, PrimaryButton, Alert, DynamicProgress } from '../../../components';
 import { useContacts, useApi } from '../../../hooks';
@@ -50,7 +45,6 @@ const ExportModal = ({ contactGroupID: LabelID, userKeysList, onSave = noop, ...
     const countContacts = LabelID
         ? contacts.filter(({ LabelIDs = [] }) => LabelIDs.includes(LabelID)).length
         : contacts.length;
-    const apiCalls = Math.ceil(countContacts / QUERY_EXPORT_MAX_PAGESIZE);
 
     const handleSave = (vcards: string[]) => {
         const allVcards = vcards.join('\n');
@@ -66,52 +60,16 @@ const ExportModal = ({ contactGroupID: LabelID, userKeysList, onSave = noop, ...
         }
 
         const abortController = new AbortController();
-        const apiWithAbort = (config: any) => api({ ...config, signal: abortController.signal });
 
-        const { publicKeys, privateKeys } = splitKeys(userKeysList);
-
-        const exportBatch = async (i: number, { signal }: AbortController) => {
-            const { Contacts: contacts } = (await apiWithAbort(
-                queryContactExport({ LabelID, Page: i, PageSize: QUERY_EXPORT_MAX_PAGESIZE })
-            )) as { Contacts: Contact[] };
-            for (const { Cards, ID } of contacts) {
-                if (signal.aborted) {
-                    return;
-                }
-                try {
-                    const { properties: contactDecrypted = [], errors = [] } = await prepareContact(
-                        { Cards } as Contact,
-                        { publicKeys, privateKeys }
-                    );
-
-                    if (errors.length) {
-                        throw new Error('Error decrypting contact');
-                    }
-
-                    const contactExported = toICAL(contactDecrypted).toString();
-                    // need to check again for signal.aborted because the abort
-                    // may have taken place during await prepareContact
-                    if (!signal.aborted) {
-                        addSuccess((contactsExported) => [...contactsExported, contactExported]);
-                    }
-                } catch (error) {
-                    // need to check again for signal.aborted because the abort
-                    // may have taken place during await prepareContact
-                    if (!signal.aborted) {
-                        addError((contactsNotExported) => [...contactsNotExported, ID]);
-                    }
-                }
-            }
-        };
-
-        const exportContacts = async (abortController: AbortController) => {
-            for (let i = 0; i < apiCalls; i++) {
-                // avoid overloading API in the unlikely case exportBatch is too fast
-                await Promise.all([exportBatch(i, abortController), wait(API_SAFE_INTERVAL)]);
-            }
-        };
-
-        exportContacts(abortController).catch((error) => {
+        exportContactsFromLabel(
+            LabelID,
+            countContacts,
+            userKeysList,
+            abortController.signal,
+            api,
+            (contactExported) => addSuccess((contactsExported) => [...contactsExported, contactExported]),
+            (ID) => addError((contactsNotExported) => [...contactsNotExported, ID])
+        ).catch((error) => {
             if (error.name !== 'AbortError') {
                 rest.onClose?.(); // close the modal; otherwise it is left hanging in there
                 throw error;
