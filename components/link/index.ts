46fbef9b3150a8e04d4ac485a4c6a073c90782e5
Jeremy Benaim <jeremybenaim@gmail.com>
2021-03-11 12:00:06 +0100

    SettingsLink component + update appLinks

--- components/link/index.ts
+++ components/link/index.ts
@@ -2,6 +2,7 @@ export { default as Info } from './Info';
 export { default as Href } from './Href';
 export { default as useAppLink } from './useAppLink';
 export { default as AppLink } from './AppLink';
+export { default as SettingsLink } from './SettingsLink';
 export { default as LearnMore } from './LearnMore';
 export * from './LearnMore';
 export { default as TopNavbarLink } from './TopNavbarLink';
