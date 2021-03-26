9e27a1cbdae66fb64db83a8801414bb97fa653c3
Richard <richard@protonmail.com>
2021-03-26 08:28:22 +0000

    Apply 1 suggestion(s) to 1 file(s)

--- containers/topBanners/TopBanner.tsx
+++ containers/topBanners/TopBanner.tsx
@@ -15,7 +15,7 @@ const TopBanner = ({ children, className, onClose }: Props) => {
         <div className={classnames(['flex flex-nowrap text-center relative text-bold', className])}>
             <div className="flex-item-fluid p0-5">{children}</div>
             {onClose ? (
-                <Button icon shape="ghost" className="flex-item-noshrinkd" onClick={onClose}>
+                <Button icon shape="ghost" className="flex-item-noshrink" onClick={onClose}>
                     <Icon name="off" />
                 </Button>
             ) : null}
