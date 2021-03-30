35f3b45f3fcceda62e2211b137a9d570189b2d0a
Nicolas Hoffmann <dante3333@gmail.com>
2021-03-30 22:27:35 +0200

    Review

--- components/wizard/Wizard.tsx
+++ components/wizard/Wizard.tsx
@@ -13,23 +13,40 @@ interface Props {
 
 const Wizard = ({ step = 0, steps = [], hideText = false }: Props) => {
     return (
-        <div className={classnames(['wizard-container', hideText && 'wizard-container--no-text-displayed'])}>
-            <ul className="wizard unstyled flex flex-nowrap flex-justify-space-between">
+        <div
+            className={classnames(['wizard-container pt0 pb1 mb1', hideText && 'wizard-container--no-text-displayed'])}
+        >
+            <ul className="wizard mt0 unstyled flex flex-nowrap flex-justify-space-between">
                 {steps.map((text = '', index) => {
                     return (
                         <li
                             key={index.toString()}
                             className={classnames(['wizard-item', index < step && 'is-complete'])}
                             aria-current={index === step ? 'step' : undefined}
+                            title={text}
                         >
                             <span className="wizard-marker flex">
                                 {index < step && <Icon name="on" size={12} className="z10 mauto wizard-marker-icon" />}
                             </span>
-                            <span className="wizard-item-inner">{text}</span>
                         </li>
                     );
                 })}
             </ul>
+            {hideText === false && (
+                <ul className="unstyled flex flex-nowrap m0">
+                    {steps.map((text = '', index) => {
+                        return (
+                            <li
+                                key={`wizard-label-${index.toString()}`}
+                                className={classnames(['wizard-label', index < step && 'is-complete'])}
+                                aria-current={index === step ? 'step' : undefined}
+                            >
+                                <span>{text}</span>
+                            </li>
+                        );
+                    })}
+                </ul>
+            )}
         </div>
     );
 };
