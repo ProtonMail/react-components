6f2b11d951e1219e47d7af388965f023acce0c2c
Bastien Miclo <kylekatarnls@protonmail.com>
2021-04-01 13:51:09 +0200

    Add button for EC key

--- containers/vpn/AuthenticationCertificateSection/AuthenticationCertificateSection.js
+++ containers/vpn/AuthenticationCertificateSection/AuthenticationCertificateSection.js
@@ -3,7 +3,13 @@ import { c } from 'ttag';
 import { queryAuthenticationCertificate, queryAuthenticationCertificateKey } from 'proton-shared/lib/api/vpn';
 // import { ENCRYPTION_CONFIGS, ENCRYPTION_TYPES } from 'proton-shared/lib/constants';
 // import { randomBytes } from 'crypto';
-import { exportPrivateKey, exportPublicKey, generateKeyPair, isCryptoSupported } from 'proton-shared/lib/keys/keyPair';
+import {
+    exportPrivateKey,
+    exportPublicKey,
+    generateEcKeyPair,
+    generateRsaKeyPair,
+    isCryptoSupported,
+} from 'proton-shared/lib/keys/keyPair';
 import { Button } from '../../../components';
 import useApi from '../../../hooks/useApi';
 import { useLoading } from '../../../hooks';
@@ -12,27 +18,26 @@ const AuthenticationCertificateSection = () => {
     const [loading, withLoading] = useLoading();
     const api = useApi();
 
-    const getKeyPair = async () => {
+    const getKeyPair = async (mode) => {
         if (!isCryptoSupported()) {
-            return api(queryAuthenticationCertificateKey());
+            return api(queryAuthenticationCertificateKey(mode));
         }
 
-        const key = await generateKeyPair();
+        const key = await (mode === 'rsa' ? generateRsaKeyPair() : generateEcKeyPair());
 
         return {
-            PrivateKey: exportPrivateKey(key),
-            PublicKey: exportPublicKey(key),
-            Mode: 'RSA',
+            PrivateKey: await exportPrivateKey(key),
+            PublicKey: await exportPublicKey(key),
         };
     };
 
-    const download = async () => {
+    const download = async (mode) => {
         // const passphrase = randomBytes(32).toString();
-        const { PrivateKey, PublicKey, Mode } = await getKeyPair();
+        const { PrivateKey, PublicKey } = await getKeyPair(mode);
         const { SerialNumber, Certificate, ExpirationTime } = await api(
             queryAuthenticationCertificate({
                 ClientPublicKey: PublicKey,
-                ClientPublicKeyMode: Mode || 'EC',
+                ClientPublicKeyMode: mode,
             })
         );
 
@@ -52,7 +57,10 @@ const AuthenticationCertificateSection = () => {
         <>
             <h3 className="mt2">{c('Title').t`Download authentication certificate`}</h3>
             <div className="mb1">
-                <Button onClick={() => withLoading(download())} loading={loading}>{c('Button').t`Download`}</Button>
+                <Button onClick={() => withLoading(download('ec'))} loading={loading}>{c('Button')
+                    .t`Download EC`}</Button>
+                <Button onClick={() => withLoading(download('rsa'))} loading={loading}>{c('Button')
+                    .t`Download RSA`}</Button>
             </div>
         </>
     );
