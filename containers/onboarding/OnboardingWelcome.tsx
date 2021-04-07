2d2762c745412940ed31f05527cddbda00dffddd
Richard <richard@protonmail.com>
2021-04-07 17:40:11 +0200

    Review

--- containers/onboarding/OnboardingWelcome.tsx
+++ containers/onboarding/OnboardingWelcome.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { c } from 'ttag';
+import welcomeImage from 'design-system/assets/img/illustrations/proton-welcome.svg';
 
 import OnboardingContent, { Props as OnboardingContentProps } from './OnboardingContent';
 
@@ -9,7 +10,7 @@ const OnboardingWelcome = (props: Omit<OnboardingContentProps, 'decription' | 'i
             title={c('Onboarding Proton').t`Welcome to Proton`}
             description={c('Onboarding Proton')
                 .t`Our mission is to build an internet where you are in control of your data and your privacy. We have recently updated Proton - welcome to the secure internet.`}
-            img={<img src="TODO" alt={c('Onboarding Proton').t`Welcome to Proton`} />}
+            img={<img src={welcomeImage} alt={c('Onboarding Proton').t`Welcome to Proton`} />}
             {...props}
         />
     );
