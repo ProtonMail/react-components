fc37f246fd7a26e18773678159c80e78640b1c44
mmso <mmso@mmso.se>
2021-04-07 17:11:59 +0200

    Update human verification path

--- containers/api/humanVerification/Captcha.tsx
+++ containers/api/humanVerification/Captcha.tsx
@@ -1,22 +1,36 @@
 import React, { useState, useEffect, useRef } from 'react';
-import { getHost } from 'proton-shared/lib/helpers/url';
-import { createUrl } from 'proton-shared/lib/fetch/helpers';
-import { isURL } from 'proton-shared/lib/helpers/validators';
 import { useConfig } from '../../../hooks';
+
+export const getRelativeApiHostname = (hostname: string) => {
+    const idx = hostname.indexOf('.');
+    const first = hostname.substr(0, idx);
+    const second = hostname.substr(idx + 1);
+    return `${first}-api.${second}`;
+};
+
+const getIframeUrl = (apiUrl: string, token: string) => {
+    const url = new URL(apiUrl, window.location.origin);
+    url.hostname = getRelativeApiHostname(url.hostname);
+    url.pathname = '/core/v4/captcha';
+    url.searchParams.set('Token', token);
+    url.searchParams.set('Client', 'web');
+    return url;
+};
 
 interface Props {
     token: string;
     onSubmit: (token: string) => void;
 }
+
 const Captcha = ({ token, onSubmit }: Props) => {
     const [style, setStyle] = useState<any>();
     const iframeRef = useRef<HTMLIFrameElement>(null);
     const { API_URL } = useConfig();
-    const client = 'web';
-    const host = isURL(API_URL) ? getHost(API_URL) : window.location.host;
-    const url = createUrl('https://secure.protonmail.com/captcha/captcha.html', { token, client, host });
-    const src = url.toString();
-    const targetOrigin = url.origin;
+
+    const iframeUrl = getIframeUrl(API_URL, token);
+
+    const src = iframeUrl.toString();
+    const targetOrigin = iframeUrl.origin;
 
     useEffect(() => {
         const handleMessage = (event: MessageEvent) => {
