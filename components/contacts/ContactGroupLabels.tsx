9a3a927f03470f54c179bef824278e8b7f8662eb
Richard <richard@protonmail.com>
2021-04-07 16:38:29 +0200

    Remove contact links

--- components/contacts/ContactGroupLabels.tsx
+++ components/contacts/ContactGroupLabels.tsx
@@ -1,8 +1,7 @@
-import React, { MouseEvent } from 'react';
-import { APPS } from 'proton-shared/lib/constants';
+import React from 'react';
 import { ContactGroup } from 'proton-shared/lib/interfaces/contacts/Contact';
+
 import LabelStack, { LabelDescription } from '../labelStack/LabelStack';
-import { useAppLink } from '../link';
 
 interface Props {
     contactGroups: ContactGroup[];
@@ -11,8 +10,6 @@ interface Props {
 }
 
 const ContactGroupLabels = ({ contactGroups, isStacked = true, className }: Props) => {
-    const appLink = useAppLink();
-
     const labels = contactGroups.reduce((acc: LabelDescription[], contactGroup: ContactGroup) => {
         return contactGroup
             ? [
@@ -21,10 +18,6 @@ const ContactGroupLabels = ({ contactGroups, isStacked = true, className }: Prop
                       name: contactGroup.Name,
                       color: contactGroup.Color,
                       title: contactGroup.Name,
-                      onClick: (event: MouseEvent) => {
-                          appLink(`/?contactGroupID=${contactGroup.ID}`, APPS.PROTONCONTACTS);
-                          event.stopPropagation();
-                      },
                   },
               ]
             : acc;
