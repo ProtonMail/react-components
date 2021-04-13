1058b37389f40f9d6487f6e588845cc7bdcfe092
Jeremy Benaim <jeremybenaim@gmail.com>
2021-04-13 14:57:34 +0200

    OAuth scope change

--- containers/mail/import/constants.ts
+++ containers/mail/import/constants.ts
@@ -15,5 +15,10 @@ export const IMAPS = {
 
 /* The following constants are use to forge OAuth URL in the import assistant */
 export const G_OAUTH_CLIENT_ID = '923746734024-4rggv7tvusv9c0fi9tvh5elnuj5o067b.apps.googleusercontent.com';
-export const G_OAUTH_SCOPE = ['email', 'openid', 'https://mail.google.com/'].join(' ');
+export const G_OAUTH_SCOPE = [
+    'email',
+    'openid',
+    'https://www.googleapis.com/auth/gmail.readonly',
+    'https://www.googleapis.com/auth/drive.metadata.readonly',
+].join(' ');
 export const G_OAUTH_REDIRECT_PATH = '/oauth/callback';
