e00085dbcac52cde99db885630b26558471fb462
mmso <mmso@mmso.se>
2021-04-16 15:36:41 +0200

    Normalize

--- containers/login/AbuseModal.tsx
+++ containers/login/AbuseModal.tsx
@@ -7,14 +7,6 @@ interface Props {
 }
 
 const AbuseModal = ({ message, ...rest }: Props) => {
-    const title = c('Title').t`Account disabled`;
-
-    const abuseOrFraudLink = (
-        <Href url="https://protonmail.com/support/knowledge-base/account-disabled/" key={0}>
-            {c('Info').t`abuse or fraud`}
-        </Href>
-    );
-
     const contactLink = (
         <Href url="https://protonmail.com/abuse" key={1}>
             {c('Info').t`here`}
@@ -22,11 +14,19 @@ const AbuseModal = ({ message, ...rest }: Props) => {
     );
 
     return (
-        <FormModal hasClose={false} hasSubmit={false} title={title} small close={c('Action').t`Close`} {...rest}>
+        <FormModal
+            hasClose={false}
+            hasSubmit={false}
+            title={c('Title').t`Account suspended`}
+            small
+            close={c('Action').t`Close`}
+            {...rest}
+        >
             {message || (
                 <>
-                    <div className="mb1">{c('Info').jt`Account disabled due to ${abuseOrFraudLink}.`}</div>
-                    <div>{c('Info').jt`You can find more information and contact us ${contactLink}.`}</div>
+                    <div className="mb1">{c('Info')
+                        .t`This account has been suspended due to a potential policy violation.`}</div>
+                    <div>{c('Info').jt`If you believe this is in error, please contact us ${contactLink}.`}</div>
                 </>
             )}
         </FormModal>
