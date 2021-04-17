8cad5d73a72eabb4506332bc541087cfc7af7182
Sergo Vainumäe <sergo.vainumae@protonmail.ch>
2021-04-17 07:59:22 +0200

    Pass already unique tzids to getVTimezones

--- containers/calendar/useExtractUniqueVTimezonesFromVevents.ts
+++ containers/calendar/useExtractUniqueVTimezonesFromVevents.ts
@@ -1,5 +1,6 @@
 import { getPropertyTzid } from 'proton-shared/lib/calendar/vcalHelper';
 import { VcalVeventComponent } from 'proton-shared/lib/interfaces/calendar';
+import { unique } from 'proton-shared/lib/helpers/array';
 
 import { useGetVTimezones } from '../../hooks/useGetVtimezones';
 
@@ -9,11 +10,13 @@ const useExtractUniqueVTimezonesFromVevents = () => {
     return async (vevents: VcalVeventComponent[]) => {
         return Object.values(
             await getVTimezones(
-                vevents.flatMap(({ dtstart, dtend }) =>
-                    [dtstart, dtend]
-                        .flatMap((date) => (date ? [date] : []))
-                        .map(getPropertyTzid)
-                        .flatMap((tzid) => (tzid ? [tzid] : []))
+                unique(
+                    vevents.flatMap(({ dtstart, dtend }) =>
+                        [dtstart, dtend]
+                            .flatMap((date) => (date ? [date] : []))
+                            .map(getPropertyTzid)
+                            .flatMap((tzid) => (tzid ? [tzid] : []))
+                    )
                 )
             )
         ).flatMap((vtimezoneObj) => (vtimezoneObj ? [vtimezoneObj.vtimezone] : []));
