337f5e4830573b405c71bfd9e5ca38dfa6215cd1
LeGrosSancho <monsieurk74@gmail.com>
2021-04-19 13:10:12 +0200

    [MAILWEB-2118] Fix hidden contacts when mergeContactBanner is displayed

--- containers/contacts/widget/MergeContactBanner.tsx
+++ containers/contacts/widget/MergeContactBanner.tsx
@@ -6,12 +6,17 @@ import { Icon, InlineLinkButton } from '../../../components';
 interface Props {
     countMergeableContacts: number;
     onMerge: () => void;
+    mergeContactBannerRef: React.RefObject<HTMLDivElement>;
 }
 
-const MergeContactBanner = ({ countMergeableContacts, onMerge }: Props) => {
-    const mergeAction = <InlineLinkButton onClick={onMerge}>{c('Action').t`Merge`}</InlineLinkButton>;
+const MergeContactBanner = ({ countMergeableContacts, onMerge, mergeContactBannerRef }: Props) => {
+    const mergeAction = <InlineLinkButton onClick={onMerge} key="mergeAction">{c('Action').t`Merge`}</InlineLinkButton>;
+
     return (
-        <div className="pl2 pr2 pt1 pb1 bg-weak flex flex-items-align-center flex-nowrap border-bottom">
+        <div
+            ref={mergeContactBannerRef}
+            className="pl2 pr2 pt1 pb1 bg-weak flex flex-items-align-center flex-nowrap border-bottom"
+        >
             <Icon name="merge" className="mr0-5" />
             <span>{c('Info, no need for singular')
                 .jt`${countMergeableContacts} contacts look identical. ${mergeAction}`}</span>
