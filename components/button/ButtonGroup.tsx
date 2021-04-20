b5c13221d0eee1298e06d50719e13d20ab89dd0f
mmso <mmso@mmso.se>
2021-04-20 17:35:03 +0200

    Add automatic vr

--- components/button/ButtonGroup.tsx
+++ components/button/ButtonGroup.tsx
@@ -1,17 +1,34 @@
 import React from 'react';
 import { classnames } from '../../helpers';
+import { Vr } from '../vr';
 
 type Shape = 'outline' | 'ghost';
+
 interface Props {
     children: React.ReactNode;
     shape?: Shape;
     className?: string;
 }
 
-const ButtonGroup = ({ children, shape = 'outline', className = '' }: Props, ref: React.Ref<HTMLDivElement>) => (
-    <div className={classnames(['button-group', `button-group-${shape}`, className])} ref={ref}>
-        {children}
-    </div>
-);
+const ButtonGroup = ({ children, shape = 'outline', className = '' }: Props, ref: React.Ref<HTMLDivElement>) => {
+    const childrenWithSeparators = React.Children.toArray(children)
+        .filter((x) => x !== null && React.isValidElement(x))
+        .map((child, index, array) => {
+            if (index === array.length - 1) {
+                return child;
+            }
+            return (
+                <>
+                    {child}
+                    <Vr />
+                </>
+            );
+        });
+    return (
+        <div className={classnames(['button-group', `button-group-${shape}`, className])} ref={ref}>
+            {childrenWithSeparators}
+        </div>
+    );
+};
 
 export default React.forwardRef<HTMLDivElement, Props>(ButtonGroup);
