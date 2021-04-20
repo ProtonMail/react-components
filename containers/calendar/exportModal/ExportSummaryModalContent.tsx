e1a3cf9c921e2ef3a08fa3e430628a52c80ba74f
econdepe <econdepe@protonmail.com>
2021-04-20 16:47:58 +0200

    Fix typo in plural text
    CALWEB-2337

--- containers/calendar/exportModal/ExportSummaryModalContent.tsx
+++ containers/calendar/exportModal/ExportSummaryModalContent.tsx
@@ -38,7 +38,7 @@ const ExportSummaryModalContent = ({ model }: Props) => {
     };
     const displayMessage = c('Export calendar').ngettext(
         msgid`${totalProcessed.length}/${totalToProcess} event exported`,
-        `${totalProcessed.length}/${totalToProcess} event exported`,
+        `${totalProcessed.length}/${totalToProcess} events exported`,
         totalToProcess
     );
 
