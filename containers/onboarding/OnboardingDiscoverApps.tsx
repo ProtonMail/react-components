f18db5b77e85394508f69af2d40caf8f7ebd08a2
Richard <richard@protonmail.com>
2021-04-29 11:17:56 +0200

    [MAILWEB-2170] Remove unnecessary comma

--- containers/onboarding/OnboardingDiscoverApps.tsx
+++ containers/onboarding/OnboardingDiscoverApps.tsx
@@ -9,7 +9,7 @@ const OnboardingDiscoverApps = (props: Omit<OnboardingContentProps, 'decription'
         <OnboardingContent
             title={c('Onboarding Proton').t`Discover all Proton services`}
             description={c('Onboarding Proton')
-                .t`Use the app selector in the top left to access other Proton services, including VPN, Calendar, and Drive.`}
+                .t`Use the app selector in the top left to access other Proton services, including VPN, Calendar and Drive.`}
             img={<img src={onboardingDiscover} alt={c('Onboarding Proton').t`Discover all Proton services`} />}
             {...props}
         />
