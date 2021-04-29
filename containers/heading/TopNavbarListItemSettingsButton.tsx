d92f8645994aa8b7ed235e6f0fb8683f95acab80
Matthias Margot <margotmatthias@protonmail.com>
2021-04-29 14:09:34 +0200

    Connect real data to the SettingsDropdown

--- containers/heading/TopNavbarListItemSettingsButton.tsx
+++ containers/heading/TopNavbarListItemSettingsButton.tsx
@@ -1,15 +1,17 @@
 import React from 'react';
 import { c } from 'ttag';
+import { PROTON_THEMES } from 'proton-shared/lib/themes/themes';
 
 import { DropdownMenu, DropdownMenuButton, DropdownMenuLink, Icon } from '../../components';
 import SimpleDropdown from '../../components/dropdown/SimpleDropdown';
 import TopNavbarListItemButton, {
     TopNavbarListItemButtonProps,
 } from '../../components/topnavbar/TopNavbarListItemButton';
-import { useModals } from '../../hooks';
+import { useEarlyAccess, useMailSettings, useModals } from '../../hooks';
 import { MailShortcutsModal } from '../mail';
 import ThemesModal from '../themes/ThemesModal';
 import EarlyAccessModal from '../earlyAccess/EarlyAccessModal';
+import { useTheme } from '../themes';
 
 const TopNavbarListItemSettingsButton = React.forwardRef(
     (props: Omit<TopNavbarListItemButtonProps<'button'>, 'icon' | 'text' | 'as'>, ref: typeof props.ref) => {
@@ -29,6 +31,9 @@ const TopNavbarListItemSettingsButton = React.forwardRef(
 
 const TopNavbarListItemSettingsDropdown = (props: any) => {
     const { createModal } = useModals();
+    const earlyAccess = useEarlyAccess();
+    const [theme] = useTheme();
+    const [{ Shortcuts } = { Shortcuts: 0 }] = useMailSettings();
 
     const handleEarlyAccessClick = () => {
         createModal(<EarlyAccessModal />);
@@ -57,23 +62,23 @@ const TopNavbarListItemSettingsDropdown = (props: any) => {
                     className="flex flex-nowrap flex-justify-space-between flex-align-items-center"
                 >
                     {c('Action').t`Early access`}
-                    <span>On</span>
+                    <span className="color-primary">
+                        {earlyAccess.value ? c('Enabled').t`On` : c('Disabled').t`Off`}
+                    </span>
                 </DropdownMenuButton>
-
                 <DropdownMenuButton
                     onClick={handleThemeClick}
                     className="flex flex-nowrap flex-justify-space-between flex-align-items-center"
                 >
                     {c('Action').t`Theme`}
-                    <span>Default</span>
+                    <span className="color-primary">{PROTON_THEMES[theme].getI18NLabel()}</span>
                 </DropdownMenuButton>
-
                 <DropdownMenuButton
                     onClick={handleKeyboardShortcutsClick}
                     className="flex flex-nowrap flex-justify-space-between flex-align-items-center"
                 >
                     {c('Action').t`Keyboard shortcuts`}
-                    <span>On</span>
+                    <span className="color-primary">{Shortcuts ? c('Enabled').t`On` : c('Disabled').t`Off`}</span>
                 </DropdownMenuButton>
             </DropdownMenu>
         </SimpleDropdown>
