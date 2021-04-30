90d7bd0700285b53bd592791538fe80587659e7b
Matthias Margot <margotmatthias@protonmail.com>
2021-04-30 12:16:31 +0200

    Display app-name instead of placeholder

--- containers/topBanners/EarlyAccessDesynchronizedBanner.tsx
+++ containers/topBanners/EarlyAccessDesynchronizedBanner.tsx
@@ -1,11 +1,16 @@
 import React from 'react';
 import { c } from 'ttag';
+import { getAppFromPathnameSafe } from 'proton-shared/lib/apps/slugHelper';
+import { getAppName } from 'proton-shared/lib/apps/helper';
+import { APPS } from 'proton-shared/lib/constants';
 
 import { InlineLinkButton } from '../../components';
+import { useConfig } from '../../hooks';
 import useEarlyAccess from '../../hooks/useEarlyAccess';
 import TopBanner from './TopBanner';
 
 const EarlyAccessDesynchronizedBanner = () => {
+    const { APP_NAME } = useConfig();
     const earlyAccess = useEarlyAccess();
 
     const refreshButton = (
@@ -21,13 +26,17 @@ const EarlyAccessDesynchronizedBanner = () => {
         return null;
     }
 
+    const appName = getAppName(
+        (APP_NAME === APPS.PROTONACCOUNT && getAppFromPathnameSafe(window.location.pathname)) || APP_NAME
+    );
+
     return (
         <TopBanner className="bg-info">
             {earlyAccess.value
                 ? c('Info')
-                      .jt`You have enabled Early Access. ${refreshButton} the page to use the latest version of [insert name of product].`
+                      .jt`You have enabled Early Access. ${refreshButton} the page to use the latest version of ${appName}.`
                 : c('Info')
-                      .jt`You have disabled Early Access. ${refreshButton} the page to use the stable version of [insert name of product].`}
+                      .jt`You have disabled Early Access. ${refreshButton} the page to use the stable version of ${appName}.`}
         </TopBanner>
     );
 };
