ad35b9096a8a014edacad48966afd34cffbacbc4
Sergo Vainumäe <sergo.vainumae@protonmail.ch>
2021-06-03 08:41:08 +0200

    TruncatedText changes

--- components/truncatedText/TruncatedText.tsx
+++ components/truncatedText/TruncatedText.tsx
@@ -3,7 +3,7 @@ import { c } from 'ttag';
 
 import { truncate } from 'proton-shared/lib/helpers/string';
 
-import { Button } from '../button';
+import { InlineLinkButton } from '../button';
 
 interface Props {
     children: string;
@@ -17,16 +17,15 @@ const TruncatedText = ({ children, maxChars }: Props) => {
 
     return (
         <>
-            {truncate(children, lengthToShow)}{' '}
+            {isShowingMore ? children : truncate(children, lengthToShow)}{' '}
             {childLength > maxChars && (
-                <Button
-                    shape="link"
+                <InlineLinkButton
                     color="norm"
                     onClick={() => setIsShowingMore((prevState) => !prevState)}
-                    style={{ verticalAlign: 'baseline' }}
+                    className="align-baseline"
                 >
                     {isShowingMore ? c('Action').t`Show less` : c('Action').t`Show more`}
-                </Button>
+                </InlineLinkButton>
             )}
         </>
     );
