5ffc92eea202e1bc30671523cd342cba22587c47
Jeremy Benaim <jeremybenaim@gmail.com>
2021-06-11 18:15:36 +0200

    Add scopes for calendar + contacts

--- containers/mail/import/constants.ts
+++ containers/mail/import/constants.ts
@@ -16,5 +16,15 @@ export const IMAPS = {
 /* The following constants are use to forge OAuth URL in the import assistant */
 // export const G_OAUTH_CLIENT_ID = '923746734024-4rggv7tvusv9c0fi9tvh5elnuj5o067b.apps.googleusercontent.com';
 export const G_OAUTH_CLIENT_ID = '192543898962-v1mvc6s9jlfn71tms865ercsun7crnk4.apps.googleusercontent.com';
-export const G_OAUTH_SCOPE = ['email', 'openid', 'https://mail.google.com/'].join(' ');
+export const G_OAUTH_SCOPE = [
+    'email',
+    'openid',
+    'https://mail.google.com/',
+    'https://www.googleapis.com/auth/calendar',
+    'https://www.googleapis.com/auth/calendar.readonly',
+    'https://www.googleapis.com/auth/calendar.events',
+    'https://www.googleapis.com/auth/calendar.events.readonly',
+    'https://www.googleapis.com/auth/contacts.readonly',
+    'https://www.googleapis.com/auth/contacts.other.readonly',
+].join(' ');
 export const G_OAUTH_REDIRECT_PATH = '/oauth/callback';
