fa0195faa0d1b8f39a0b22b4fc6f793353605609
Jeremy Benaim <jeremybenaim@gmail.com>
2021-06-17 10:53:30 +0200

    Update to prod client_id

--- containers/importAssistant/constants.ts
+++ containers/importAssistant/constants.ts
@@ -1,5 +1,4 @@
-// export const G_OAUTH_CLIENT_ID = '923746734024-4rggv7tvusv9c0fi9tvh5elnuj5o067b.apps.googleusercontent.com';
-export const G_OAUTH_CLIENT_ID = '192543898962-v1mvc6s9jlfn71tms865ercsun7crnk4.apps.googleusercontent.com';
+export const G_OAUTH_CLIENT_ID = '923746734024-4rggv7tvusv9c0fi9tvh5elnuj5o067b.apps.googleusercontent.com';
 
 export const G_OAUTH_REDIRECT_PATH = '/oauth/callback';
 
@@ -21,6 +20,4 @@ export const OAUTH_TEST_IDS = [
     'cxinT4HnEQpRz7FHRiGu7CjH9pFULfMwqHc9mv65yycL99EohZgfRP7eMbBUMlEZG4Ks_yszjrcMzDeKD2No6w==',
     'ddjZNL8VtjZIOVR6tenP3u1Yj9s-hRLPFHuK-iDPnJunIano7ExK27dZGG41Z7t-4NQ_JJB1W2pK1N6dgEuVTA==',
     'hFe07LzzAjBB4HxpAZnIiK7nUIja1qXkdOGPAlPeToHDKd7KlFvovGzZD13Ylp1DrJ00wJkqifz58YeYlVmxFg==',
-    // @todo remove me: adding `pro` id on proton.black for testing purpose
-    'rSUCW_Qlh8dCCsxWKPXvkUsoDNL5eW9FJUM7WX8jTPrDE3ftOMIfWt-BSuKaw5PZ7EQ6Zsp8HL9Y9qMv4Y5XJQ==',
 ];
