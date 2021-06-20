89e5a6d9721fd5fc2262d590b0f4be2ae2403300
econdepe <econdepe@protonmail.com>
2021-06-20 12:10:28 +0200

    Pass imported event data to the widget

--- containers/calendar/AddToCalendarButton.tsx
+++ containers/calendar/AddToCalendarButton.tsx
@@ -1,5 +1,12 @@
+import { CALENDAR_APP_NAME } from 'proton-shared/lib/calendar/constants';
+import { API_CODES } from 'proton-shared/lib/constants';
 import { noop } from 'proton-shared/lib/helpers/function';
-import { CalendarWidgetData, VcalVeventComponent } from 'proton-shared/lib/interfaces/calendar';
+import {
+    CalendarWidgetData,
+    ImportErrorData,
+    SavedImportData,
+    VcalVeventComponent,
+} from 'proton-shared/lib/interfaces/calendar';
 import React, { useCallback } from 'react';
 import { c } from 'ttag';
 import { processInBatches } from 'proton-shared/lib/calendar/import/encryptAndSubmit';
@@ -9,11 +16,12 @@ import { useApi, useLoading } from '../../hooks';
 interface Props {
     events: VcalVeventComponent[];
     calendarData?: CalendarWidgetData;
-    onSuccess: () => void;
+    onSuccess: (data: SavedImportData[]) => void;
+    onError: (data: ImportErrorData[]) => void;
     className?: string;
 }
 
-const AddToCalendarButton = ({ events, calendarData, onSuccess, className }: Props) => {
+const AddToCalendarButton = ({ events, calendarData, onSuccess, onError, className }: Props) => {
     const api = useApi();
     const [loading, withLoading] = useLoading();
 
@@ -24,7 +32,7 @@ const AddToCalendarButton = ({ events, calendarData, onSuccess, className }: Pro
         if (!calendar || isCalendarDisabled || !calendarKeys || !memberID || !addressKeys) {
             return noop();
         }
-        await processInBatches({
+        const encryptedEvents = await processInBatches({
             events,
             api,
             memberID,
@@ -32,10 +40,32 @@ const AddToCalendarButton = ({ events, calendarData, onSuccess, className }: Pro
             calendarID: calendar.ID,
             calendarKeys,
         });
-        onSuccess();
+        const { successData, errorData } = encryptedEvents.reduce<{
+            successData: SavedImportData[];
+            errorData: ImportErrorData[];
+        }>(
+            (acc, { component, response }) => {
+                const {
+                    Response: { Code, Event, Error },
+                } = response;
+                if (Code === API_CODES.SINGLE_SUCCESS && Event) {
+                    acc.successData.push({ savedVevent: component, savedEvent: Event });
+                } else {
+                    acc.errorData.push({ vevent: component, error: Error || 'Unknown error' });
+                }
+                return acc;
+            },
+            { successData: [], errorData: [] }
+        );
+        if (successData.length) {
+            onSuccess(successData);
+        }
+        if (errorData.length) {
+            onError(errorData);
+        }
     }, [events, api, calendar, memberID, addressKeys, calendar]);
 
-    const importText = c('Action').t`Add to ProtonCalendar`;
+    const importText = c('Action').t`Add to ${CALENDAR_APP_NAME}`;
 
     return (
         <Button
